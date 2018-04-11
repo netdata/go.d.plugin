e5f73e906a589abc976e45a52b7fd274ef1559e8
lgz <ilyamaschenko@gmail.com>
2018-04-11 11:08:55 +0900

    move TestValidChartType to chart_test

--- charts/raw/variable_test.go
+++ charts/raw/variable_test.go
@@ -84,19 +84,3 @@ func TestVariable_SetIDSetValue(t *testing.T) {
 	}
 
 }
-
-func TestValidChartType(t *testing.T) {
-
-	for _, v := range []string{Line, Area, Stacked} {
-		if !ValidChartType(v) {
-			t.Fatalf("function returned false for correct chart type")
-		}
-	}
-
-	for _, v := range []string{"", "this", "is", "wrong"} {
-		if ValidChartType(v) {
-			t.Fatalf("function returned true for incorrect chart type")
-		}
-	}
-
-}
