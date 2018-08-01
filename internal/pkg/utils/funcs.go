e70008220288d0451d0149b86ad1c61367eae298
Wing924 <weihe924stephen@gmail.com>
2018-08-01 10:42:32 +0900

    fix

--- internal/pkg/utils/funcs.go
+++ internal/pkg/utils/funcs.go
@@ -7,15 +7,8 @@ const fieldTagName = "stm"
 // StrToMap Convert struct to map[string]int64 based on stm tag
 func StrToMap(s interface{}) map[string]int64 {
 	rv := make(map[string]int64)
-	t := reflect.TypeOf(s)
-
-	if t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-	v := reflect.ValueOf(s)
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
+	v := reflect.Indirect(reflect.ValueOf(s))
+	t := v.Type()
 	for i := 0; i < t.NumField(); i++ {
 		ft := t.Field(i)
 		if tag := ft.Tag.Get(fieldTagName); tag != "" {
