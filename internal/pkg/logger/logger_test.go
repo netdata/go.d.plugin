8c86ae7a11d07d047e5c92b92cafe4bc5725cc8c
Ilya Mashchenko <ilyamaschenko@gmail.com>
2018-08-07 22:24:25 +0900

    add msg per interval limit to logger (#23)
    * add msg per interval limit to logger
    
    * logger limit bugfixes
    
    * log limiter fix
    
    * log limit fix
    
    * rename msgPerInterval
    
    * add some tests to logger
    
    * logger test minor
    
    * msgPerSecond var => constant
    
    * register Logger.count instead of the whole instance

---
+++ internal/pkg/logger/logger_test.go
@@ -0,0 +1,106 @@
+package logger
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetLevel(t *testing.T) {
+	SetLevel(WARNING)
+	assert.Equal(t, sevLevel, WARNING)
+}
+
+func TestSetModName(t *testing.T) {
+	l := New("", "")
+	SetModName(l, "name")
+	assert.Equal(t, l.modName, "name")
+}
+
+func TestSetLimit(t *testing.T) {
+	l := New("", "")
+	SetLimit(l)
+	assert.Len(t, globalTicker.counters, 1)
+
+}
+
+func TestNew(t *testing.T) {
+	assert.IsType(t, (*Logger)(nil), New("", ""))
+}
+
+func TestLogger_Log(t *testing.T) {
+	logger := New("", "")
+	buf := new(bytes.Buffer)
+	reader := bufio.NewReader(buf)
+	logger.log.SetOutput(buf)
+	SetLevel(DEBUG)
+
+	check := func(sev Severity) {
+		s, err := reader.ReadString('\n')
+		assert.Nil(t, err)
+		assert.NotEmpty(t, s)
+		assert.True(t, strings.Contains(s, sev.String()))
+	}
+
+	logger.Error("")
+	check(ERROR)
+
+	logger.Warning("")
+	check(WARNING)
+
+	logger.Info("")
+	check(INFO)
+
+	logger.Debug("")
+	check(DEBUG)
+
+	logger.Errorf("")
+	check(ERROR)
+
+	logger.Warningf("")
+	check(WARNING)
+
+	logger.Infof("")
+	check(INFO)
+
+	logger.Debugf("")
+	check(DEBUG)
+}
+
+func TestLogger_Limit(t *testing.T) {
+	logger := New("", "")
+	buf := new(bytes.Buffer)
+	scan := bufio.NewScanner(buf)
+
+	logger.log.SetOutput(buf)
+	SetLimit(logger)
+	SetLevel(DEBUG)
+
+	num := 500
+
+	for i := 0; i < num; i++ {
+		logger.Error("")
+	}
+
+	var c int
+	for scan.Scan() {
+		c++
+	}
+
+	assert.True(t, num == c)
+
+	SetLevel(INFO)
+
+	for i := 0; i < num; i++ {
+		logger.Error("")
+	}
+
+	scan = bufio.NewScanner(buf)
+	for scan.Scan() {
+		c++
+	}
+	assert.True(t, num+msgPerSecond == c)
+}
---
+++ internal/pkg/logger/ticker.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+type ticker struct {
+	ticker  <-chan time.Time
+	// collection of &Logger.count
+	counters []*int64
+}
+
+func (t *ticker) register(counter *int64) {
+	t.counters = append(t.counters, counter)
+}
+
+func newTicker() *ticker {
+	t := &ticker{
+		ticker: time.Tick(time.Second),
+	}
+	go func() {
+		for {
+			<-t.ticker
+			for _, v := range t.counters {
+				atomic.StoreInt64(v, 0)
+			}
+		}
+	}()
+	return t
+}
+
+var globalTicker = newTicker()
