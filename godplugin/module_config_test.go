d8b1fb3dceb1ee999b41ddff94c3246fc5712cd3
lgz <ilyamaschenko@gmail.com>
2018-11-21 17:00:28 +0300

    module_config_test

--- godplugin/module_config_test.go
+++ godplugin/module_config_test.go
@@ -1,1 +1,126 @@
 package godplugin
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+var (
+	moduleConfNotExist = "tests/module.conf-not-exist.yml"
+	moduleConf         = "tests/module.conf.yml"
+)
+
+func TestModuleConfigNone(t *testing.T) {
+	config := moduleConfig{}
+	assert.Error(t, config.load(moduleConfNotExist))
+
+}
+
+func TestModuleConfigDefault(t *testing.T) {
+	config := moduleConfig{}
+
+	require.NoError(t, config.load(moduleConf))
+	require.Len(t, config.Jobs, 2)
+
+	config.updateJobs(0, 1)
+
+	assert.Equal(
+		t, config.Jobs,
+		[]rawConfig{
+			{
+				"name":                "job1",
+				"update_every":        1,  // default
+				"autodetection_retry": 10, // global
+				"param":               10, // global
+			},
+			{
+				"name":                "job2",
+				"update_every":        22, // job specific
+				"autodetection_retry": 10, // global
+				"param":               22, // job specific
+			},
+		},
+	)
+}
+
+func TestModuleConfigWithModUpdateEvery(t *testing.T) {
+	config := moduleConfig{}
+
+	require.NoError(t, config.load(moduleConf))
+	require.Len(t, config.Jobs, 2)
+
+	config.updateJobs(99, 1)
+
+	assert.Equal(
+		t, config.Jobs,
+		[]rawConfig{
+			{
+				"name":                "job1",
+				"update_every":        99, // default
+				"autodetection_retry": 10, // global
+				"param":               10, // global
+			},
+			{
+				"name":                "job2",
+				"update_every":        22, // job specific
+				"autodetection_retry": 10, // global
+				"param":               22, // job specific
+			},
+		},
+	)
+}
+
+func TestModuleConfigWithGlobalUpdateEvery(t *testing.T) {
+	config := moduleConfig{}
+
+	require.NoError(t, config.load(moduleConf))
+	require.Len(t, config.Jobs, 2)
+
+	config.updateJobs(0, 15)
+
+	assert.Equal(
+		t, config.Jobs,
+		[]rawConfig{
+			{
+				"name":                "job1",
+				"update_every":        15, // global overridden
+				"autodetection_retry": 10, // global
+				"param":               10, // global
+			},
+			{
+				"name":                "job2",
+				"update_every":        22, // job specific
+				"autodetection_retry": 10, // global
+				"param":               22, // job specific
+			},
+		},
+	)
+}
+
+func TestModuleConfigWithGlobalAndModuleUpdateEvery(t *testing.T) {
+	config := moduleConfig{}
+
+	require.NoError(t, config.load(moduleConf))
+	require.Len(t, config.Jobs, 2)
+
+	config.updateJobs(5, 15)
+
+	assert.Equal(
+		t, config.Jobs,
+		[]rawConfig{
+			{
+				"name":                "job1",
+				"update_every":        15, // global overridden
+				"autodetection_retry": 10, // global
+				"param":               10, // global
+			},
+			{
+				"name":                "job2",
+				"update_every":        22, // job specific
+				"autodetection_retry": 10, // global
+				"param":               22, // job specific
+			},
+		},
+	)
+}
