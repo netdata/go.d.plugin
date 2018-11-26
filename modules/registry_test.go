969d1598459edefc470a0942796de9350b7a9ec7
lgz <ilyamaschenko@gmail.com>
2018-11-26 17:20:38 +0300

    registry test

--- modules/registry_test.go
+++ modules/registry_test.go
@@ -1,1 +1,25 @@
 package modules
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegister(t *testing.T) {
+	modName := "modName"
+	registry := make(Registry)
+
+	assert.NotPanics(t, func() {
+		register(registry, modName, Creator{})
+	})
+
+	_, ok := registry[modName]
+
+	assert.True(t, ok)
+
+	assert.Panics(t, func() {
+		register(registry, modName, Creator{})
+	})
+
+}
