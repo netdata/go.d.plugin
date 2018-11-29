7b7d4c53e600865fcaa41283a11581d8a474a5e4
lgz <ilyamaschenko@gmail.com>
2018-11-29 10:47:22 +0300

    wip

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"gopkg.in/yaml.v2"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -49,4 +51,36 @@ func TestToMap(t *testing.T) {
 
 	assert.EqualValuesf(t, expected, ToMap(s), "value test")
 	assert.EqualValuesf(t, expected, ToMap(&s), "ptr test")
+}
+
+func TestDuration_UnmarshalYAML(t *testing.T) {
+	var d Duration
+	values := [][]byte{
+		[]byte("100ms"),   // duration
+		[]byte("3s300ms"), // duration
+		[]byte("3"),       // int
+		[]byte("3.3"),     // float
+	}
+
+	for _, v := range values {
+		assert.NoError(t, yaml.Unmarshal(v, &d))
+	}
+}
+
+func TestDuration_ConvertTo(t *testing.T) {
+	d := Duration{time.Second}
+
+	assert.Equal(
+		t,
+		1000,
+		d.ConvertTo(time.Millisecond),
+	)
+
+	d.Duration = time.Second * 3
+
+	assert.Equal(
+		t,
+		1000*1000*3,
+		d.ConvertTo(time.Microsecond),
+	)
 }
