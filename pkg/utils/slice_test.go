b14e5d83b809ebd6564642047e086d6f681f071f
lgz <ilyamaschenko@gmail.com>
2018-11-29 11:44:24 +0300

    wip

--- pkg/utils/slice_test.go
+++ pkg/utils/slice_test.go
@@ -1,10 +1,10 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestStringSlice_Append(t *testing.T) {
