ae2d3efcdcc3f7b772d06e1dbfe2a32346a6ffac
lgz <ilyamaschenko@gmail.com>
2018-11-30 11:25:55 +0300

    plugin tests init

---
+++ godplugin/plugin_test.go
@@ -0,0 +1,39 @@
+package godplugin
+
+import "testing"
+
+func TestPlugin_RemoveFromQueue(t *testing.T) {
+
+}
+
+func TestPlugin_Serve(t *testing.T) {
+
+}
+
+func TestPlugin_Setup(t *testing.T) {
+
+}
+
+func TestPlugin_JobInitOK(t *testing.T) {
+
+}
+
+func TestPlugin_JobInitNG(t *testing.T) {
+
+}
+
+func TestPlugin_JobCheckOK(t *testing.T) {
+
+}
+
+func TestPlugin_JobCheckNG(t *testing.T) {
+
+}
+
+func TestPlugin_JobCheckPanic(t *testing.T) {
+
+}
+
+func TestPlugin_JobCheckNGAutoDetectionRetry(t *testing.T) {
+
+}
