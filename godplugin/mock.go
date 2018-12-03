186d43f477f22770c035597c7f9cc6dd2c00e9a0
lgz <ilyamaschenko@gmail.com>
2018-12-03 10:00:35 +0300

    plugin tests wip

--- godplugin/mock.go
+++ godplugin/mock.go
@@ -19,46 +19,57 @@ type mockJob struct {
 	stop  func()
 }
 
+// FullName invokes fullName
 func (m mockJob) FullName() string {
 	return m.fullName()
 }
 
+// ModuleName invokes moduleName
 func (m mockJob) ModuleName() string {
 	return m.moduleName()
 }
 
+// Name invokes name
 func (m mockJob) Name() string {
 	return m.name()
 }
 
+// AutoDetectionRetry invokes autoDetectionRetry
 func (m mockJob) AutoDetectionRetry() int {
 	return m.autoDetectionRetry()
 }
 
+// Panicked invokes panicked
 func (m mockJob) Panicked() bool {
 	return m.panicked()
 }
 
+// Init invokes init
 func (m mockJob) Init() bool {
 	return m.init()
 }
 
+// Check invokes check
 func (m mockJob) Check() bool {
 	return m.check()
 }
 
+// PostCheck invokes postCheck
 func (m mockJob) PostCheck() bool {
 	return m.postCheck()
 }
 
+// Tick invokes tick
 func (m mockJob) Tick(clock int) {
 	m.tick(clock)
 }
 
+// Start invokes start
 func (m mockJob) Start() {
 	m.start()
 }
 
+// Stop invokes stop
 func (m mockJob) Stop() {
 	m.stop()
 }
