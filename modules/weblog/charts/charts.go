c01df0ad8e6f8171df5f606be5ec3c3f8ce4a8c3
lgz <ilyamaschenko@gmail.com>
2018-12-10 18:39:58 +0300

    web_log charts package fix2

--- modules/weblog/charts/charts.go
+++ modules/weblog/charts/charts.go
@@ -11,113 +11,166 @@ type (
 	Dims = modules.Dims
 )
 
-var (
-	stacked     = modules.Stacked
-	area        = modules.Area
-	incremental = modules.Incremental
-)
-
+// NOTE: inconsistency between contexts with python web_log
 var (
 	ResponseStatuses = Chart{
 		ID:    "response_statuses",
-		Title: "Response Statuses", Units: "requests/s", Fam: "responses", Type: stacked,
+		Title: "Response Statuses",
+		Units: "requests/s",
+		Fam:   "responses",
+		Ctx:   "web_log.response_statuses",
+		Type:  modules.Stacked,
 		Dims: Dims{
-			{ID: "successful_requests", Name: "success", Algo: incremental},
-			{ID: "server_errors", Name: "error", Algo: incremental},
-			{ID: "redirects", Name: "redirect", Algo: incremental},
-			{ID: "bad_requests", Name: "bad", Algo: incremental},
-			{ID: "other_requests", Name: "other", Algo: incremental},
+			{ID: "successful_requests", Name: "success", Algo: modules.Incremental},
+			{ID: "server_errors", Name: "error", Algo: modules.Incremental},
+			{ID: "redirects", Name: "redirect", Algo: modules.Incremental},
+			{ID: "bad_requests", Name: "bad", Algo: modules.Incremental},
+			{ID: "other_requests", Name: "other", Algo: modules.Incremental},
 		},
 	}
 	ResponseCodes = Chart{
 		ID:    "response_codes",
-		Title: "Response Codes", Units: "requests/s", Fam: "responses", Type: stacked,
+		Title: "Response Codes",
+		Units: "requests/s",
+		Fam:   "responses",
+		Ctx:   "web_log.response_codes",
+		Type:  modules.Stacked,
 		Dims: Dims{
-			{ID: "2xx", Algo: incremental},
-			{ID: "5xx", Algo: incremental},
-			{ID: "3xx", Algo: incremental},
-			{ID: "4xx", Algo: incremental},
-			{ID: "1xx", Algo: incremental},
-			{ID: "0xx", Algo: incremental},
-			{ID: "unmatched", Algo: incremental},
+			{ID: "2xx", Algo: modules.Incremental},
+			{ID: "5xx", Algo: modules.Incremental},
+			{ID: "3xx", Algo: modules.Incremental},
+			{ID: "4xx", Algo: modules.Incremental},
+			{ID: "1xx", Algo: modules.Incremental},
+			{ID: "0xx", Algo: modules.Incremental},
+			{ID: "unmatched", Algo: modules.Incremental},
 		},
 	}
 	ResponseCodesDetailed = Chart{
 		ID:    "detailed_response_codes",
-		Title: "Detailed Response Codes", Units: "requests/s", Fam: "responses", Type: stacked,
+		Title: "Detailed Response Codes",
+		Units: "requests/s",
+		Fam:   "responses",
+		Ctx:   "web_log.response_codes_detailed",
+		Type:  modules.Stacked,
 	}
 	Bandwidth = Chart{
 		ID:    "bandwidth",
-		Title: "Bandwidth", Units: "kilobits/s", Fam: "bandwidth", Type: area,
+		Title: "Bandwidth",
+		Units: "kilobits/s",
+		Fam:   "bandwidth",
+		Ctx:   "web_log.bandwidth",
+		Type:  modules.Area,
 		Dims: Dims{
-			{ID: "resp_length", Name: "received", Algo: incremental, Mul: 8, Div: 1000},
-			{ID: "bytes_sent", Name: "sent", Algo: incremental, Mul: -8, Div: 1000},
+			{ID: "resp_length", Name: "received", Algo: modules.Incremental, Mul: 8, Div: 1000},
+			{ID: "bytes_sent", Name: "sent", Algo: modules.Incremental, Mul: -8, Div: 1000},
 		},
 	}
 	ResponseTime = Chart{
 		ID:    "response_time",
-		Title: "Processing Time", Units: "milliseconds", Fam: "timings", Type: area,
+		Title: "Processing Time",
+		Units: "milliseconds",
+		Fam:   "timings",
+		Ctx:   "web_log.response_time",
+		Type:  modules.Area,
 		Dims: Dims{
-			{ID: "resp_time_min", Name: "min", Algo: incremental, Div: 1000},
-			{ID: "resp_time_max", Name: "max", Algo: incremental, Div: 1000},
-			{ID: "resp_time_avg", Name: "avg", Algo: incremental, Div: 1000},
+			{ID: "resp_time_min", Name: "min", Algo: modules.Incremental, Div: 1000},
+			{ID: "resp_time_max", Name: "max", Algo: modules.Incremental, Div: 1000},
+			{ID: "resp_time_avg", Name: "avg", Algo: modules.Incremental, Div: 1000},
 		},
 	}
 	ResponseTimeHistogram = Chart{
 		ID:    "response_time_histogram",
-		Title: "Processing Time Histogram", Units: "requests/s", Fam: "timings",
+		Title: "Processing Time Histogram",
+		Units: "requests/s",
+		Fam:   "timings",
+		Ctx:   "web_log.response_time_histogram",
 	}
 	ResponseTimeUpstream = Chart{
 		ID:    "response_time_upstream",
-		Title: "Processing Time Upstream", Units: "milliseconds", Fam: "timings", Type: area,
+		Title: "Processing Time Upstream",
+		Units: "milliseconds",
+		Fam:   "timings",
+		Ctx:   "web_log.response_time_upstream",
+		Type:  modules.Area,
 		Dims: Dims{
-			{ID: "resp_time_upstream_min", Name: "min", Algo: incremental, Div: 1000},
-			{ID: "resp_time_upstream_max", Name: "max", Algo: incremental, Div: 1000},
-			{ID: "resp_time_upstream_avg", Name: "avg", Algo: incremental, Div: 1000},
+			{ID: "resp_time_upstream_min", Name: "min", Algo: modules.Incremental, Div: 1000},
+			{ID: "resp_time_upstream_max", Name: "max", Algo: modules.Incremental, Div: 1000},
+			{ID: "resp_time_upstream_avg", Name: "avg", Algo: modules.Incremental, Div: 1000},
 		},
 	}
 	ResponseTimeUpstreamHistogram = Chart{
 		ID:    "response_time_upstream_histogram",
-		Title: "Processing Time Upstream Histogram", Units: "requests/s", Fam: "timings",
+		Title: "Processing Time Upstream Histogram",
+		Units: "requests/s",
+		Fam:   "timings",
+		Ctx:   "web_log.response_time_upstream_histogram",
 	}
 	RequestsPerURL = Chart{
 		ID:    "requests_per_url",
-		Title: "Requests Per Url", Units: "requests/s", Fam: "urls", Type: stacked,
+		Title: "Requests Per Url",
+		Units: "requests/s",
+		Fam:   "urls",
+		Ctx:   "web_log.requests_per_url",
+		Type:  modules.Stacked,
 	}
 	RequestsPerUserDefined = Chart{
 		ID:    "requests_per_user_defined",
-		Title: "Requests Per User Defined Pattern", Units: "requests/s", Fam: "user defined", Type: stacked,
+		Title: "Requests Per User Defined Pattern",
+		Units: "requests/s",
+		Fam:   "user defined",
+		Ctx:   "web_log.requests_per_user_defined",
+		Type:  modules.Stacked,
 	}
 	RequestsPerHTTPMethod = Chart{
 		ID:    "requests_per_http_method",
-		Title: "Requests Per HTTP Method", Units: "requests/s", Fam: "http methods", Type: stacked,
+		Title: "Requests Per HTTP Method",
+		Units: "requests/s",
+		Fam:   "http methods",
+		Ctx:   "web_log.requests_per_http_method",
+		Type:  modules.Stacked,
 		Dims: Dims{
-			{ID: "GET", Algo: incremental},
+			{ID: "GET", Algo: modules.Incremental},
 		},
 	}
 	RequestsPerHTTPVersion = Chart{
 		ID:    "requests_per_http_version",
-		Title: "Requests Per HTTP Version", Units: "requests/s", Fam: "http versions", Type: stacked,
+		Title: "Requests Per HTTP Version",
+		Units: "requests/s",
+		Fam:   "http versions",
+		Ctx:   "web_log.requests_per_http_version",
+		Type:  modules.Stacked,
 	}
 	RequestsPerIPProto = Chart{
 		ID:    "requests_per_ip_proto",
-		Title: "Requests Per IP Protocol", Units: "requests/s", Fam: "ip protocols", Type: stacked,
+		Title: "Requests Per IP Protocol",
+		Units: "requests/s",
+		Fam:   "ip protocols",
+		Ctx:   "web_log.requests_per_ip_proto",
+		Type:  modules.Stacked,
 		Dims: Dims{
-			{ID: "req_ipv4", Name: "ipv4", Algo: incremental},
-			{ID: "req_ipv6", Name: "ipv6", Algo: incremental},
+			{ID: "req_ipv4", Name: "ipv4", Algo: modules.Incremental},
+			{ID: "req_ipv6", Name: "ipv6", Algo: modules.Incremental},
 		},
 	}
 	CurrentPollIPs = Chart{
 		ID:    "clients_current",
-		Title: "Current Poll Unique Client IPs", Units: "unique ips", Fam: "clients", Type: stacked,
+		Title: "Current Poll Unique Client IPs",
+		Units: "unique ips",
+		Fam:   "clients",
+		Ctx:   "web_log.current_poll_ips",
+		Type:  modules.Stacked,
 		Dims: Dims{
-			{ID: "unique_cur_ipv4", Name: "ipv4", Algo: incremental},
-			{ID: "unique_cur_ipv6", Name: "ipv6", Algo: incremental},
+			{ID: "unique_cur_ipv4", Name: "ipv4", Algo: modules.Incremental},
+			{ID: "unique_cur_ipv6", Name: "ipv6", Algo: modules.Incremental},
 		},
 	}
 	AllTimeIPs = Chart{
 		ID:    "clients_all_time",
-		Title: "All Time Unique Client IPs", Units: "unique ips", Fam: "clients", Type: stacked,
+		Title: "All Time Unique Client IPs",
+		Units: "unique ips",
+		Fam:   "clients",
+		Ctx:   "web_log.all_time_ips",
+		Type:  modules.Stacked,
 		Dims: Dims{
 			{ID: "unique_all_ipv4", Name: "ipv4"},
 			{ID: "unique_all_ipv6", Name: "ipv6"},
@@ -129,27 +182,51 @@ func ResponseCodesDetailedPerFamily() []Chart {
 	return []Chart{
 		{
 			ID:    ResponseCodesDetailed.ID + "_1xx",
-			Title: "Detailed Response Codes 1xx", Units: "requests/s", Fam: "responses", Type: stacked,
+			Title: "Detailed Response Codes 1xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_1xx",
+			Type:  modules.Stacked,
 		},
 		{
 			ID:    ResponseCodesDetailed.ID + "_2xx",
-			Title: "Detailed Response Codes 2xx", Units: "requests/s", Fam: "responses", Type: stacked,
+			Title: "Detailed Response Codes 2xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_2xx",
+			Type:  modules.Stacked,
 		},
 		{
 			ID:    ResponseCodesDetailed.ID + "_3xx",
-			Title: "Detailed Response Codes 3xx", Units: "requests/s", Fam: "responses", Type: stacked,
+			Title: "Detailed Response Codes 3xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_3xx",
+			Type:  modules.Stacked,
 		},
 		{
 			ID:    ResponseCodesDetailed.ID + "_4xx",
-			Title: "Detailed Response Codes 4xx", Units: "requests/s", Fam: "responses", Type: stacked,
+			Title: "Detailed Response Codes 4xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_4xx",
+			Type:  modules.Stacked,
 		},
 		{
 			ID:    ResponseCodesDetailed.ID + "_5xx",
-			Title: "Detailed Response Codes 5xx", Units: "requests/s", Fam: "responses", Type: stacked,
+			Title: "Detailed Response Codes 5xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_5xx",
+			Type:  modules.Stacked,
 		},
 		{
 			ID:    ResponseCodesDetailed.ID + "_other",
-			Title: "Detailed Response Codes Other", Units: "requests/s", Fam: "responses", Type: stacked,
+			Title: "Detailed Response Codes Other",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_other",
+			Type:  modules.Stacked,
 		},
 	}
 }
@@ -158,26 +235,35 @@ func PerCategoryStats(id string) []Chart {
 	return []Chart{
 		{
 			ID:    ResponseCodesDetailed.ID + "_" + id,
-			Title: "Detailed Response Codes", Units: "requests/s", Fam: id, Ctx: "web_log.url_detailed_response_codes",
-			Type: stacked,
+			Title: "Detailed Response Codes",
+			Units: "requests/s",
+			Fam:   id,
+			Ctx:   "web_log.response_codes_detailed_per_url",
+			Type:  modules.Stacked,
 		},
 		{
 			ID:    Bandwidth.ID + "_" + id,
-			Title: "Bandwidth", Units: "kilobits/s", Fam: id, Ctx: "web_log.url_bandwidth",
-			Type: area,
+			Title: "Bandwidth",
+			Units: "kilobits/s",
+			Fam:   id,
+			Ctx:   "web_log.bandwidth_per_url",
+			Type:  modules.Area,
 			Dims: Dims{
-				{ID: id + "_resp_length", Name: "received", Algo: incremental, Mul: 8, Div: 1000},
-				{ID: id + "_bytes_sent", Name: "sent", Algo: incremental, Mul: -8, Div: 1000},
+				{ID: id + "_resp_length", Name: "received", Algo: modules.Incremental, Mul: 8, Div: 1000},
+				{ID: id + "_bytes_sent", Name: "sent", Algo: modules.Incremental, Mul: -8, Div: 1000},
 			},
 		},
 		{
 			ID:    ResponseTime.ID + "_" + id,
-			Title: "Processing Time", Units: "milliseconds", Fam: id, Ctx: "web_log.url_response_time",
-			Type: area,
+			Title: "Processing Time",
+			Units: "milliseconds",
+			Fam:   id,
+			Ctx:   "web_log.response_time_per_url",
+			Type:  modules.Area,
 			Dims: Dims{
-				{ID: id + "_resp_time_min", Name: "min", Algo: incremental, Div: 1000},
-				{ID: id + "_resp_time_max", Name: "max", Algo: incremental, Div: 1000},
-				{ID: id + "_resp_time_avg", Name: "avg", Algo: incremental, Div: 1000},
+				{ID: id + "_resp_time_min", Name: "min", Algo: modules.Incremental, Div: 1000},
+				{ID: id + "_resp_time_max", Name: "max", Algo: modules.Incremental, Div: 1000},
+				{ID: id + "_resp_time_avg", Name: "avg", Algo: modules.Incremental, Div: 1000},
 			},
 		},
 	}
