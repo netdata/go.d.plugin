7ec12751d910166101ef31f0734a4e77a936d0d4
lgz <ilyamaschenko@gmail.com>
2018-12-11 21:51:11 +0300

    weblog filter

--- modules/weblog/filter.go
+++ modules/weblog/filter.go
@@ -1,54 +1,56 @@
 package weblog
 
-//
-//import "github.com/netdata/go.d.plugin/collectors/weblogog/matcher"
-//
-//type filter struct {
-//	include matcher.Matcher
-//	exclude matcher.Matcher
-//}
-//
-//func (f *filter) exist() bool {
-//	return f.include != nil || f.exclude != nil
-//}
-//
-//func (f *filter) filter(s string) bool {
-//	i, e := true, true
-//	if f.include != nil {
-//		i = f.include.Match(s)
-//	}
-//	if f.exclude != nil {
-//		e = !f.exclude.Match(s)
-//	}
-//	return i && e
-//}
-//
-//type rawFilter struct {
-//	Include string `yaml:"include"`
-//	Exclude string `yaml:"exclude"`
-//}
-//
-//func getFilter(r rawFilter) (filter, error) {
-//	var f filter
-//	if r.Include == "" && r.Exclude == "" {
-//		return f, nil
-//	}
-//
-//	if r.Include != "" {
-//		m, err := matcher.New(r.Include)
-//		if err != nil {
-//			return f, err
-//		}
-//		f.include = m
-//	}
-//
-//	if r.Exclude != "" {
-//		m, err := matcher.New(r.Exclude)
-//		if err != nil {
-//			return f, err
-//		}
-//		f.exclude = m
-//	}
-//
-//	return f, nil
-//}
+import "github.com/netdata/go.d.plugin/modules/weblog/matcher"
+
+type RawFilter struct {
+	Include string
+	Exclude string
+}
+
+type Filter struct {
+	include matcher.Matcher
+	exclude matcher.Matcher
+}
+
+func (f *Filter) Match(s string) bool {
+	includeOK := true
+	excludeOK := true
+
+	if f.include != nil {
+		includeOK = f.include.Match(s)
+	}
+
+	if f.exclude != nil {
+		excludeOK = f.exclude.Match(s)
+	}
+
+	return includeOK && excludeOK
+}
+
+func createFilter(raw RawFilter) (matcher.Matcher, error) {
+	var f Filter
+
+	if raw.Include == "" && raw.Exclude == "" {
+		return &f, nil
+	}
+
+	if raw.Include != "" {
+		m, err := matcher.New(raw.Include)
+		if err != nil {
+			return nil, err
+		}
+		f.include = m
+
+	}
+
+	if raw.Exclude != "" {
+		m, err := matcher.New(raw.Exclude)
+		if err != nil {
+			return nil, err
+		}
+		f.exclude = m
+	}
+
+	return &f, nil
+
+}
