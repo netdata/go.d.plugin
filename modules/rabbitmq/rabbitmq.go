70c2017bab4cdce6213bc84a68eefbf2afa6aa47
lgz <ilyamaschenko@gmail.com>
2018-12-13 15:29:18 +0300

    add stm tags

--- modules/rabbitmq/rabbitmq.go
+++ modules/rabbitmq/rabbitmq.go
@@ -15,49 +15,49 @@ func init() {
 // https://www.rabbitmq.com/monitoring.html
 type (
 	apiOverview struct {
-		objectTotals `json:"object_totals"`
-		queueTotals  `json:"queue_totals"`
-		messageStats `json:"message_stats"`
+		objectTotals `json:"object_totals" stm:"object_totals"`
+		queueTotals  `json:"queue_totals" stm:"queue_totals"`
+		messageStats `json:"message_stats" stm:"message_stats"`
 	}
 	apiNode []node
 )
 
 type objectTotals struct {
-	Consumers   int
-	Queues      int
-	Exchanges   int
-	Connections int
-	Channels    int
+	Consumers   int `stm:"consumers"`
+	Queues      int `stm:"queues"`
+	Exchanges   int `stm:"exchanges"`
+	Connections int `stm:"connections"`
+	Channels    int `stm:"channels"`
 }
 
 type queueTotals struct {
-	MessagesReady          int `json:"messages_ready"`
-	MessagesUnacknowledged int `json:"messages_unacknowledged"`
+	MessagesReady          int `json:"messages_ready" stm:"messages_ready"`
+	MessagesUnacknowledged int `json:"messages_unacknowledged" stm:"messages_unacknowledged"`
 }
 
 // https://rawcdn.githack.com/rabbitmq/rabbitmq-management/master/priv/www/doc/stats.html
 type messageStats struct {
-	Ack              int
-	Publish          int
-	PublishIn        int `json:"publish_in"`
-	PublishOut       int `json:"publish_out"`
-	Confirm          int
-	Deliver          int
-	DeliverNoAck     int `json:"deliver_no_ack"`
-	Get              int
-	GetNoAck         int `json:"get_no_ack"`
-	DeliverGet       int `json:"deliver_get"`
-	Redeliver        int
-	ReturnUnroutable int `json:"return_unroutable"`
+	Ack              int `stm:"ack"`
+	Publish          int `stm:"publish"`
+	PublishIn        int `json:"publish_in" stm:"publish_in"`
+	PublishOut       int `json:"publish_out" stm:"publish_out"`
+	Confirm          int `stm:"confirm"`
+	Deliver          int `stm:"deliver"`
+	DeliverNoAck     int `json:"deliver_no_ack" stm:"deliver_no_ack"`
+	Get              int `stm:"get"`
+	GetNoAck         int `json:"get_no_ack" stm:"get_no_ack"`
+	DeliverGet       int `json:"deliver_get" stm:"deliver_get"`
+	Redeliver        int `stm:"redeliver"`
+	ReturnUnroutable int `json:"return_unroutable" stm:"return_unroutable"`
 }
 
 type node struct {
-	FDUsed      int `json:"fd_used"`
-	MemUsed     int `json:"mem_used"`
-	SocketsUsed int `json:"sockets_used"`
-	ProcUsed    int `json:"proc_used"`
-	DiskFree    int `json:"disk_free"`
-	RunQueue    int `json:"run_queue"`
+	FDUsed      int `json:"fd_used" stm:"fd_used"`
+	MemUsed     int `json:"mem_used" stm:"mem_used"`
+	SocketsUsed int `json:"sockets_used" stm:"sockets_used"`
+	ProcUsed    int `json:"proc_used" stm:"proc_used"`
+	DiskFree    int `json:"disk_free" stm:"disk_free"`
+	RunQueue    int `json:"run_queue" stm:"run_queue"`
 }
 
 // New creates Rabbitmq with default values
