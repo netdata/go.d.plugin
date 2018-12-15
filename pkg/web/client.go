d7ee2481f701af8eaccd07161012a012bdc2aa76
lgz <ilyamaschenko@gmail.com>
2018-12-16 01:32:54 +0300

    web package cleanup

--- pkg/web/client.go
+++ pkg/web/client.go
@@ -7,12 +7,10 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-
-	"github.com/netdata/go.d.plugin/pkg/utils"
 )
 
-// Client is the interface that wraps the Do method.
-type Client interface {
+// HTTPClient is the interface that wraps the Do method.
+type HTTPClient interface {
 	Do(r *http.Request) (*http.Response, error)
 }
 
@@ -23,57 +21,63 @@ func (f clientFunc) Do(r *http.Request) (*http.Response, error) {
 	return f(r)
 }
 
-// RawClient is a struct that contains the fields that are needed to create Client.
-type RawClient struct {
-	Timeout           utils.Duration `yaml:"timeout"`              // default is zero (no timeout) must be tuned by modules
-	NotFollowRedirect bool           `yaml:"not_follow_redirects"` // default is follow
-	SkipVerify        bool           `yaml:"skip_tls_verify"`      // default is verify
-	ProxyURL          string         `yaml:"proxy_url"`
-	ProxyUsername     string         `yaml:"proxy_username"`
-	ProxyPassword     string         `yaml:"proxy_password"`
+// Client is a struct that contains the fields that are needed fore creating HTTPClient.
+type Client struct {
+	Timeout           Duration `yaml:"timeout"`              // default is zero (no timeout) must be tuned by modules
+	NotFollowRedirect bool     `yaml:"not_follow_redirects"` // default is follow
+	SkipVerify        bool     `yaml:"skip_tls_verify"`      // default is verify
+	ProxyURL          string   `yaml:"proxy_url"`
+	ProxyUsername     string   `yaml:"proxy_username"`
+	ProxyPassword     string   `yaml:"proxy_password"`
 }
 
-// CreateHTTPClient returns new Client.
-func (r RawClient) CreateHTTPClient() Client {
-	if r.ProxyUsername == "" || r.ProxyPassword == "" {
-		return r.create()
+// NewHTTPClient creates new HTTPClient.
+func NewHTTPClient(client Client) HTTPClient {
+	if client.ProxyUsername == "" || client.ProxyPassword == "" {
+		return newHTTPClient(client)
 	}
-	return r.createWithProxyAuth()
+	return newHTTPClientProxyAuth(client)
 }
 
 // TODO: TLSClientConfig
-func (r RawClient) create() Client {
-	client := &http.Client{
-		Timeout: r.Timeout.Duration,
+func newHTTPClient(client Client) HTTPClient {
+	httpClient := &http.Client{
+		Timeout: client.Timeout.Duration,
 		Transport: &http.Transport{
-			Proxy:           getProxyFunc(r.ProxyURL),
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: r.SkipVerify},
-		}}
-
-	if r.NotFollowRedirect {
-		client.CheckRedirect = func(req *http.Request, via []*http.Request) error { return errors.New("redirect") }
+			Proxy:           proxyFunc(client.ProxyURL),
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: client.SkipVerify},
+		},
 	}
-	return client
+
+	if client.NotFollowRedirect {
+		httpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error { return errors.New("redirect") }
+	}
+
+	return httpClient
 }
 
-func (r RawClient) createWithProxyAuth() Client {
-	auth := base64.StdEncoding.EncodeToString([]byte(r.ProxyUsername + ":" + r.ProxyPassword))
-	client := r.create()
+func newHTTPClientProxyAuth(client Client) HTTPClient {
+	auth := base64.StdEncoding.EncodeToString([]byte(client.ProxyUsername + ":" + client.ProxyPassword))
+	httpClient := newHTTPClient(client)
 
-	return clientFunc(
-		func(req *http.Request) (*http.Response, error) {
-			req.Header.Set("Proxy-Authorization", "Basic "+auth)
-			return client.Do(req)
-		})
+	f := func(req *http.Request) (*http.Response, error) {
+		req.Header.Set("Proxy-Authorization", "Basic "+auth)
+
+		return httpClient.Do(req)
+	}
+
+	return clientFunc(f)
 }
 
-func getProxyFunc(u string) func(r *http.Request) (*url.URL, error) {
-	if u == "" {
+func proxyFunc(rawProxyURL string) func(r *http.Request) (*url.URL, error) {
+	if rawProxyURL == "" {
 		return http.ProxyFromEnvironment
 	}
-	proxyURL, err := url.Parse(u)
+
+	proxyURL, err := url.Parse(rawProxyURL)
 	if err != nil {
 		return func(r *http.Request) (*url.URL, error) { return nil, fmt.Errorf("invalid proxy: %s", err) }
 	}
+
 	return func(r *http.Request) (*url.URL, error) { return proxyURL, nil }
 }
