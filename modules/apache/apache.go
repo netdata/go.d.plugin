d06557f5c01e56ff6318e8b03a16a2004c103017
lgz <ilyamaschenko@gmail.com>
2018-12-16 01:33:20 +0300

    apache cleanup

--- modules/apache/apache.go
+++ modules/apache/apache.go
@@ -23,15 +23,17 @@ func init() {
 }
 
 const (
-	totalAccesses       = "Total Accesses" // extended stats
-	totalkBytes         = "Total kBytes"   // extended stats
-	cpuLoad             = "CPULoad"        // extended stats
-	uptime              = "Uptime"         // extended stats
-	reqPerSec           = "ReqPerSec"      // extended stats
-	bytesPerSec         = "BytesPerSec"    // extended stats
-	bytesPerReq         = "BytesPerReq"    // extended stats
-	busyWorkers         = "BusyWorkers"
-	idleWorkers         = "IdleWorkers"
+	// The lines marked "(*)" are only available if ExtendedStatus is On.
+	// In version 2.3.6, loading mod_status will toggle ExtendedStatus On by default.
+	totalAccesses       = "Total Accesses" // * a total number of accesses
+	totalkBytes         = "Total kBytes"   // * a byte count served extended stats
+	cpuLoad             = "CPULoad"        // * the current percentage CPU used in total by all workers combined
+	uptime              = "Uptime"         // * the time server has been running for
+	reqPerSec           = "ReqPerSec"      // * the average number of requests per second
+	bytesPerSec         = "BytesPerSec"    // * the average number of bytes served per second
+	bytesPerReq         = "BytesPerReq"    // * the average number of bytes per request
+	busyWorkers         = "BusyWorkers"    //   the number of worker serving requests
+	idleWorkers         = "IdleWorkers"    //   the number of idle worker
 	connsTotal          = "ConnsTotal"
 	connsAsyncKeepAlive = "ConnsAsyncKeepAlive"
 	connsAsyncClosing   = "ConnsAsyncClosing"
@@ -39,23 +41,15 @@ const (
 	scoreBoard          = "Scoreboard"
 )
 
-var assignment = map[string]string{
-	totalAccesses:       "requests",
-	totalkBytes:         "sent",
-	reqPerSec:           "requests_sec",
-	bytesPerSec:         "size_sec",
-	bytesPerReq:         "size_req",
-	busyWorkers:         "busy",
-	idleWorkers:         "idle",
-	connsTotal:          "connections",
-	connsAsyncKeepAlive: "keepalive",
-	connsAsyncClosing:   "closing",
-	connsAsyncWriting:   "writing",
-}
-
 // New creates Apache with default values
 func New() *Apache {
-	return &Apache{}
+	return &Apache{
+		HTTP: web.HTTP{
+			Request: web.Request{URL: "http://localhost/server-status?auto"},
+			Client:  web.Client{Timeout: web.Duration{Duration: time.Second}},
+		},
+		metrics: make(map[string]int64),
+	}
 }
 
 // Apache apache module
@@ -65,7 +59,7 @@ type Apache struct {
 	web.HTTP `yaml:",inline"`
 
 	request *http.Request
-	client  web.Client
+	client  web.HTTPClient
 
 	extendedStats bool
 
@@ -77,19 +71,23 @@ func (Apache) Cleanup() {}
 
 // Init makes initialization
 func (a *Apache) Init() bool {
-	req, err := a.CreateHTTPRequest()
-
-	if err != nil {
-		a.Error(err)
+	if !strings.HasSuffix(a.URL, "?auto") {
+		a.Errorf("invalid status page URL %s, must end with '?auto'", a.URL)
 		return false
 	}
 
-	if a.Timeout.Duration == 0 {
-		a.Timeout.Duration = time.Second
+	req, err := web.NewHTTPRequest(a.Request)
+
+	if err != nil {
+		a.Errorf("error on creating request : %s", err)
+		return false
 	}
 
 	a.request = req
-	a.client = a.CreateHTTPClient()
+
+	a.client = web.NewHTTPClient(a.Client)
+
+	a.Debugf("using timeout: %s", a.Timeout.Duration)
 
 	return true
 }
@@ -143,9 +141,6 @@ func (a *Apache) Collect() map[string]int64 {
 		return nil
 	}
 
-	// Extended On -> Extended Off
-	a.metrics = make(map[string]int64)
-
 	if err := a.parseResponse(resp); err != nil {
 		a.Errorf("error on parse response : %s", err)
 		return nil
@@ -174,7 +169,7 @@ func parseLine(line string, metrics map[string]int64) error {
 	parts := strings.SplitN(line, ":", 2)
 
 	if len(parts) != 2 {
-		return fmt.Errorf("bad format : %s", line)
+		return fmt.Errorf("invalid format : %s", line)
 	}
 
 	key := strings.TrimSpace(parts[0])
@@ -216,7 +211,7 @@ func parseScoreboard(scoreboard string, metrics map[string]int64) {
 	// “L” Logging
 	// “G” Gracefully finishing
 	// “I” Idle cleanup of worker
-	//“.” Open slot with no current process
+	// “.” Open slot with no current process
 
 	var waiting, open, S, R, W, K, D, C, L, G, I int64
 
@@ -259,6 +254,20 @@ func parseScoreboard(scoreboard string, metrics map[string]int64) {
 	metrics["scoreboard_finishing"] = G
 	metrics["scoreboard_idle_cleanup"] = I
 	metrics["scoreboard_open"] = open
+}
+
+var assignment = map[string]string{
+	totalAccesses:       "requests",
+	totalkBytes:         "sent",
+	reqPerSec:           "requests_sec",
+	bytesPerSec:         "size_sec",
+	bytesPerReq:         "size_req",
+	busyWorkers:         "busy",
+	idleWorkers:         "idle",
+	connsTotal:          "connections",
+	connsAsyncKeepAlive: "keepalive",
+	connsAsyncClosing:   "closing",
+	connsAsyncWriting:   "writing",
 }
 
 func assign(key string) string {
