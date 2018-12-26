d2951932f89bd8c2220a5dc70c6b5a62a00e6166
lgz <ilyamaschenko@gmail.com>
2018-12-27 00:09:59 +0300

    wip

--- modules/freeradius/freeradius.go
+++ modules/freeradius/freeradius.go
@@ -1,1 +1,46 @@
 package freeradius
+
+import (
+	"github.com/netdata/go.d.plugin/modules"
+)
+
+func init() {
+	creator := modules.Creator{
+		Create: func() modules.Module { return New() },
+	}
+
+	modules.Register("freeradius", creator)
+}
+
+// New creates Example with default values
+func New() *Freeradius {
+	return &Freeradius{}
+}
+
+// Freeradius freeradius module
+type Freeradius struct {
+	modules.Base
+}
+
+// Cleanup makes cleanup
+func (Freeradius) Cleanup() {}
+
+// Init makes initialization
+func (Freeradius) Init() bool {
+	return false
+}
+
+// Check makes check
+func (Freeradius) Check() bool {
+	return false
+}
+
+// Charts creates Charts
+func (Freeradius) Charts() *Charts {
+	return charts.Copy()
+}
+
+// Collect collects metrics
+func (Freeradius) Collect() map[string]int64 {
+	return nil
+}
