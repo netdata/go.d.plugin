e92adc25860c7b52b783b72e408e487578ac4b8e
lgz <ilyamaschenko@gmail.com>
2018-12-27 11:16:28 +0300

    wip

--- modules/freeradius/freeradius.go
+++ modules/freeradius/freeradius.go
@@ -2,11 +2,15 @@ package freeradius
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/md5"
 	"time"
 
 	"github.com/netdata/go.d.plugin/modules"
+	"github.com/netdata/go.d.plugin/pkg/web"
 
 	"layeh.com/radius"
+	"layeh.com/radius/rfc2869"
 )
 
 func init() {
@@ -17,12 +21,19 @@ func init() {
 	modules.Register("freeradius", creator)
 }
 
+const (
+	defAddress = "127.0.0.1"
+	defPort    = 18121
+	defSecret  = "adminsecret"
+)
+
 // New creates Freeradius with default values
 func New() *Freeradius {
 	return &Freeradius{
-		Address: "127.0.0.1",
-		Port:    18121,
-		Secret:  "adminsecret",
+		Address: defAddress,
+		Port:    defPort,
+		Secret:  defSecret,
+		Timeout: web.Duration{Duration: time.Second},
 
 		exchanger: &radius.Client{
 			Retry:           time.Second,
@@ -42,6 +53,7 @@ type Freeradius struct {
 	Address string
 	Port    int
 	Secret  string
+	Timeout web.Duration
 
 	exchanger exchanger
 }
@@ -50,8 +62,18 @@ type Freeradius struct {
 func (Freeradius) Cleanup() {}
 
 // Init makes initialization
-func (Freeradius) Init() bool {
-	return false
+func (f *Freeradius) Init() bool {
+	if f.Address == "" {
+		f.Address = defAddress
+	}
+	if f.Port == 0 {
+		f.Port = defPort
+	}
+	if f.Secret == "" {
+		f.Secret = defSecret
+	}
+
+	return true
 }
 
 // Check makes check
@@ -67,4 +89,35 @@ func (Freeradius) Charts() *Charts {
 // Collect collects metrics
 func (Freeradius) Collect() map[string]int64 {
 	return nil
+}
+
+func newStatusServerPacket(secret string) (*radius.Packet, error) {
+	packet := radius.New(radius.CodeStatusServer, []byte(secret))
+
+	if err := PacketType_Set(packet, PacketType_Value_AccessAccept); err != nil {
+		return nil, err
+	}
+	if err := FreeRADIUSStatisticsType_Set(packet, FreeRADIUSStatisticsType_Value_All); err != nil {
+		return nil, err
+	}
+
+	if err := rfc2869.MessageAuthenticator_Set(packet, make([]byte, 16)); err != nil {
+		return nil, err
+	}
+
+	hash := hmac.New(md5.New, packet.Secret)
+
+	encode, err := packet.Encode()
+
+	if err != nil {
+		return nil, err
+	}
+
+	hash.Write(encode)
+
+	if err := rfc2869.MessageAuthenticator_Set(packet, hash.Sum(nil)); err != nil {
+		return nil, err
+	}
+
+	return packet, nil
 }
