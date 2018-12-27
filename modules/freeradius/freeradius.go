e9e9f2b63b5ade0a813ba6e029c82eeffb068ad3
lgz <ilyamaschenko@gmail.com>
2018-12-27 16:07:36 +0300

    wip

--- modules/freeradius/freeradius.go
+++ modules/freeradius/freeradius.go
@@ -107,19 +107,46 @@ func (f *Freeradius) Collect() map[string]int64 {
 		return nil
 	}
 
-	metrics, err := decodeServerResponse(resp)
-
-	if err != nil {
-		f.Errorf("error on decoding response from %s : %s", net.JoinHostPort(f.Address, strconv.Itoa(f.Port)), err)
-		return nil
-	}
-
-	return metrics
+	return decodeServerResponse(resp)
 }
 
-func decodeServerResponse(resp *radius.Packet) (map[string]int64, error) {
-	return nil, nil
-
+func decodeServerResponse(resp *radius.Packet) map[string]int64 {
+	return map[string]int64{
+		"access-requests":               int64(FreeRADIUSTotalAccessRequests_Get(resp)),
+		"access-accepts":                int64(FreeRADIUSTotalAccessAccepts_Get(resp)),
+		"access-rejects":                int64(FreeRADIUSTotalAccessRequests_Get(resp)),
+		"access-challenges":             int64(FreeRADIUSTotalAccessChallenges_Get(resp)),
+		"auth-responses":                int64(FreeRADIUSTotalAuthResponses_Get(resp)),
+		"auth-duplicate-requests":       int64(FreeRADIUSTotalAuthDuplicateRequests_Get(resp)),
+		"auth-malformed-requests":       int64(FreeRADIUSTotalAuthMalformedRequests_Get(resp)),
+		"auth-invalid-requests":         int64(FreeRADIUSTotalAuthInvalidRequests_Get(resp)),
+		"auth-dropped-requests":         int64(FreeRADIUSTotalAuthDroppedRequests_Get(resp)),
+		"auth-unknown-types":            int64(FreeRADIUSTotalAuthUnknownTypes_Get(resp)),
+		"proxy-access-requests":         int64(FreeRADIUSTotalProxyAccessRequests_Get(resp)),
+		"proxy-access-accepts":          int64(FreeRADIUSTotalProxyAccessAccepts_Get(resp)),
+		"proxy-access-rejects":          int64(FreeRADIUSTotalProxyAccessRequests_Get(resp)),
+		"proxy-access-challenges":       int64(FreeRADIUSTotalProxyAccessChallenges_Get(resp)),
+		"proxy-auth-responses":          int64(FreeRADIUSTotalProxyAuthResponses_Get(resp)),
+		"proxy-auth-duplicate-requests": int64(FreeRADIUSTotalProxyAuthDuplicateRequests_Get(resp)),
+		"proxy-auth-malformed-requests": int64(FreeRADIUSTotalProxyAuthMalformedRequests_Get(resp)),
+		"proxy-auth-invalid-requests":   int64(FreeRADIUSTotalProxyAuthInvalidRequests_Get(resp)),
+		"proxy-auth-dropped-requests":   int64(FreeRADIUSTotalProxyAuthDroppedRequests_Get(resp)),
+		"proxy-auth-unknown-types":      int64(FreeRADIUSTotalProxyAuthUnknownTypes_Get(resp)),
+		"accounting-requests":           int64(FreeRADIUSTotalAccountingRequests_Get(resp)),
+		"accounting-responses":          int64(FreeRADIUSTotalAccountingResponses_Get(resp)),
+		"acct-duplicate-requests":       int64(FreeRADIUSTotalAcctDuplicateRequests_Get(resp)),
+		"acct-malformed-requests":       int64(FreeRADIUSTotalAcctMalformedRequests_Get(resp)),
+		"acct-invalid-requests":         int64(FreeRADIUSTotalAcctInvalidRequests_Get(resp)),
+		"acct-dropped-requests":         int64(FreeRADIUSTotalAcctDroppedRequests_Get(resp)),
+		"acct-unknown-types":            int64(FreeRADIUSTotalAcctUnknownTypes_Get(resp)),
+		"proxy-accounting-requests":     int64(FreeRADIUSTotalProxyAccountingRequests_Get(resp)),
+		"proxy-accounting-responses":    int64(FreeRADIUSTotalProxyAccountingResponses_Get(resp)),
+		"proxy-acct-duplicate-requests": int64(FreeRADIUSTotalProxyAcctDuplicateRequests_Get(resp)),
+		"proxy-acct-malformed-requests": int64(FreeRADIUSTotalProxyAcctMalformedRequests_Get(resp)),
+		"proxy-acct-invalid-requests":   int64(FreeRADIUSTotalProxyAcctInvalidRequests_Get(resp)),
+		"proxy-acct-dropped-requests":   int64(FreeRADIUSTotalProxyAcctDroppedRequests_Get(resp)),
+		"proxy-acct-unknown-types":      int64(FreeRADIUSTotalProxyAcctUnknownTypes_Get(resp)),
+	}
 }
 
 func newStatusServerPacket(secret string) (*radius.Packet, error) {
