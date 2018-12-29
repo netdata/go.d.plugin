d1189b64a28f2af8a8717506608e595ad091241f
lgz <ilyamaschenko@gmail.com>
2018-12-29 22:37:39 +0300

    update requests charts

--- modules/solr/charts.go
+++ modules/solr/charts.go
@@ -13,7 +13,7 @@ type (
 	Dims = modules.Dims
 )
 
-var charts = []*Chart{
+var charts = Charts{
 	{
 		ID:    "search_requests",
 		Title: "Search Requests",
@@ -74,6 +74,68 @@ var charts = []*Chart{
 			{ID: "query_requestTimes_p95_ms", Name: "p95", Div: 1000000},
 			{ID: "query_requestTimes_p99_ms", Name: "p99", Div: 1000000},
 			{ID: "query_requestTimes_p999_ms", Name: "p999", Div: 1000000},
+		},
+	},
+	{
+		ID:    "update_requests",
+		Title: "Update Requests",
+		Units: "requests/s",
+		Ctx:   "solr.update_requests",
+		Dims: Dims{
+			{ID: "update_requests_count", Name: "requests", Algo: modules.Incremental},
+		},
+	},
+	{
+		ID:    "update_errors",
+		Title: "Update Errors",
+		Units: "errors/s",
+		Ctx:   "solr.update_errors",
+		Dims: Dims{
+			{ID: "update_errors_count", Name: "errors", Algo: modules.Incremental},
+		},
+	},
+	{
+		ID:    "update_errors_by_type",
+		Title: "Update Errors By Types",
+		Units: "errors/s",
+		Ctx:   "solr.update_errors_by_type",
+		Dims: Dims{
+			{ID: "update_clientErrors_count", Name: "client", Algo: modules.Incremental},
+			{ID: "update_serverErrors_count", Name: "server", Algo: modules.Incremental},
+			{ID: "update_timeouts_count", Name: "timeouts", Algo: modules.Incremental},
+		},
+	},
+	{
+		ID:    "update_requests_processing_time",
+		Title: "Update Requests Processing Time",
+		Units: "milliseconds",
+		Ctx:   "solr.update_requests_processing_time",
+		Dims: Dims{
+			{ID: "update_totalTime_count", Name: "time", Algo: modules.Incremental},
+		},
+	},
+	{
+		ID:    "update_requests_timings",
+		Title: "Update Requests Timings",
+		Units: "milliseconds",
+		Ctx:   "solr.update_requests_timings",
+		Dims: Dims{
+			{ID: "update_requestTimes_min_ms", Name: "min", Div: 1000000},
+			{ID: "update_requestTimes_median_ms", Name: "median", Div: 1000000},
+			{ID: "update_requestTimes_mean_ms", Name: "mean", Div: 1000000},
+			{ID: "update_requestTimes_max_ms", Name: "max", Div: 1000000},
+		},
+	},
+	{
+		ID:    "update_requests_duration",
+		Title: "Update Requests Duration",
+		Units: "milliseconds",
+		Ctx:   "solr.update_requests_duration",
+		Dims: Dims{
+			{ID: "update_requestTimes_p75_ms", Name: "p75", Div: 1000000},
+			{ID: "update_requestTimes_p95_ms", Name: "p95", Div: 1000000},
+			{ID: "update_requestTimes_p99_ms", Name: "p99", Div: 1000000},
+			{ID: "update_requestTimes_p999_ms", Name: "p999", Div: 1000000},
 		},
 	},
 }
