80a554a815488c2fa7837ed0e9f9cb9074068859
lgz <ilyamaschenko@gmail.com>
2018-12-29 18:46:02 +0300

    v7 parser

--- modules/solr/parser.go
+++ modules/solr/parser.go
@@ -85,7 +85,7 @@ func (v *v6Parser) parseCore(core string, data map[string]json.RawMessage) error
 			//	if err := json.Unmarshal(stats, &v.value); err != nil {
 			//		return err
 			//	}
-			//default:
+			//case "commits", "cumulativeAdds", "cumulativeDeletesById", "cumulativeDeletesByQuery", "cumulativeErrors", "expungeDeletes", "merges", "optimizes", "rollbacks", "splits":
 			//	if err := json.Unmarshal(stats, &v.common); err != nil {
 			//		return err
 			//	}
@@ -104,6 +104,82 @@ func (v *v6Parser) parseCore(core string, data map[string]json.RawMessage) error
 				return err
 			}
 			v.parsed[fmt.Sprintf("%s_%s_%s_count", core, typ, stat)] += v.count.Count
+		case "requestTimes":
+			if err := json.Unmarshal(stats, &v.requestTimes); err != nil {
+				return err
+			}
+			v.parsed[fmt.Sprintf("%s_%s_%s_count", core, typ, stat)] += v.requestTimes.Count
+			v.parsed[fmt.Sprintf("%s_%s_%s_mean_ms", core, typ, stat)] += v.requestTimes.MeanMS
+			v.parsed[fmt.Sprintf("%s_%s_%s_median_ms", core, typ, stat)] += v.requestTimes.MedianMS
+			v.parsed[fmt.Sprintf("%s_%s_%s_p75_ms", core, typ, stat)] += v.requestTimes.P75MS
+			v.parsed[fmt.Sprintf("%s_%s_%s_p95_ms", core, typ, stat)] += v.requestTimes.P95MS
+			v.parsed[fmt.Sprintf("%s_%s_%s_p99_ms", core, typ, stat)] += v.requestTimes.P99MS
+			v.parsed[fmt.Sprintf("%s_%s_%s_p999_ms", core, typ, stat)] += v.requestTimes.P999MS
+		}
+	}
+
+	return nil
+}
+
+type v7Parser struct {
+	count        int64
+	common       common
+	requestTimes requestTimes
+
+	parsed map[string]int64
+}
+
+func (v *v7Parser) parse(resp *http.Response) (map[string]int64, error) {
+	var m coresMetrics
+
+	if err := json.NewDecoder(resp.Body).Decode(&m); err != nil {
+		return nil, err
+	}
+
+	for core, data := range m.Metrics {
+		if err := v.parseCore(core[10:], data); err != nil {
+			return nil, err
+		}
+	}
+
+	return v.parsed, nil
+}
+
+func (v *v7Parser) parseCore(core string, data map[string]json.RawMessage) error {
+	for metric, stats := range data {
+		parts := strings.Split(metric, ".")
+
+		if len(parts) != 3 {
+			continue
+		}
+
+		typ, handler, stat := strings.ToLower(parts[0]), parts[1], parts[2]
+
+		if handler == "updateHandler" {
+			//switch stat {
+			//case "adds", "autoCommits", "deletesById", "deletesByQuery", "docsPending", "errors", "softAutoCommits":
+			//	if err := json.Unmarshal(stats, &v.count); err != nil {
+			//		return err
+			//	}
+			//case "commits", "cumulativeAdds", "cumulativeDeletesById", "cumulativeDeletesByQuery", "cumulativeErrors", "expungeDeletes", "merges", "optimizes", "rollbacks", "splits":
+			//	if err := json.Unmarshal(stats, &v.common); err != nil {
+			//		return err
+			//	}
+			//}
+			continue
+		}
+
+		switch stat {
+		case "clientErrors", "errors", "serverErrors", "timeouts":
+			if err := json.Unmarshal(stats, &v.common); err != nil {
+				return err
+			}
+			v.parsed[fmt.Sprintf("%s_%s_%s_count", core, typ, stat)] += v.common.Count
+		case "requests", "totalTime":
+			if err := json.Unmarshal(stats, &v.count); err != nil {
+				return err
+			}
+			v.parsed[fmt.Sprintf("%s_%s_%s_count", core, typ, stat)] += v.count
 		case "requestTimes":
 			if err := json.Unmarshal(stats, &v.requestTimes); err != nil {
 				return err
