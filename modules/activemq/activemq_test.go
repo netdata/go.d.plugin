68b5cd56f9bb3b19b38c0024c4dd632f83758e29
lgz <ilyamaschenko@gmail.com>
2018-12-31 20:55:29 +0300

    wip

--- modules/activemq/activemq_test.go
+++ modules/activemq/activemq_test.go
@@ -12,6 +12,43 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var (
+	testQueues = `<queues>
+<queue name="sandra">
+<stats size="0" consumerCount="0" enqueueCount="0" dequeueCount="0"/>
+<feed>
+<atom>queueBrowse/sandra?view=rss&amp;feedType=atom_1.0</atom>
+<rss>queueBrowse/sandra?view=rss&amp;feedType=rss_2.0</rss>
+</feed>
+</queue>
+<queue name="Test">
+<stats size="0" consumerCount="0" enqueueCount="0" dequeueCount="0"/>
+<feed>
+<atom>queueBrowse/Test?view=rss&amp;feedType=atom_1.0</atom>
+<rss>queueBrowse/Test?view=rss&amp;feedType=rss_2.0</rss>
+</feed>
+</queue>
+</queues>`
+
+	testTopics = `<topics>
+<topic name="ActiveMQ.Advisory.MasterBroker ">
+<stats size="0" consumerCount="0" enqueueCount="1" dequeueCount="0"/>
+</topic>
+<topic name="AAA ">
+<stats size="0" consumerCount="1" enqueueCount="0" dequeueCount="0"/>
+</topic>
+<topic name="ActiveMQ.Advisory.Topic ">
+<stats size="0" consumerCount="0" enqueueCount="1" dequeueCount="0"/>
+</topic>
+<topic name="ActiveMQ.Advisory.Queue ">
+<stats size="0" consumerCount="0" enqueueCount="2" dequeueCount="0"/>
+</topic>
+<topic name="AAAA ">
+<stats size="0" consumerCount="0" enqueueCount="0" dequeueCount="0"/>
+</topic>
+</topics>`
+)
+
 func TestNew(t *testing.T) {
 	mod := New()
 
@@ -38,7 +75,24 @@ func TestActivemq_Init(t *testing.T) {
 }
 
 func TestActivemq_Check(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				switch r.URL.Path {
+				case "/webadmin/xml/queues.jsp":
+					_, _ = w.Write([]byte(testQueues))
+				case "/webadmin/xml/topics.jsp":
+					_, _ = w.Write([]byte(testTopics))
+				}
+			}))
+	defer ts.Close()
 
+	mod := New()
+	mod.HTTP.Request = web.Request{URL: ts.URL}
+	mod.Webadmin = "webadmin"
+
+	require.True(t, mod.Init())
+	require.True(t, mod.Check())
 }
 
 func TestActivemq_Charts(t *testing.T) {
@@ -46,7 +100,7 @@ func TestActivemq_Charts(t *testing.T) {
 }
 
 func TestActivemq_Cleanup(t *testing.T) {
-
+	New().Cleanup()
 }
 
 func TestActivemq_Collect(t *testing.T) {
