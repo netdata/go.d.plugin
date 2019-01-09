57528e57b786ca8718b41c4d6e9aca6fa5a18373
lgz <ilyamaschenko@gmail.com>
2019-01-10 01:46:03 +0300

    wip

--- pkg/matcher/matcher_test.go
+++ pkg/matcher/matcher_test.go
@@ -1,7 +1,67 @@
 package matcher
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCreateMatcher(t *testing.T) {
+	cases := []struct {
+		valid        bool
+		line         string
+		expectedType Matcher
+	}{
+		{
+			valid:        true,
+			line:         "string=hello",
+			expectedType: &StringContains{},
+		},
+		{
+			valid:        true,
+			line:         "string=^hello",
+			expectedType: &StringPrefix{},
+		},
+		{
+			valid:        true,
+			line:         "string=hello$",
+			expectedType: &StringSuffix{},
+		},
+		{
+			valid:        true,
+			line:         "regexp=[0-9]+",
+			expectedType: &RegexpMatch{},
+		},
+		{
+			valid:        true,
+			line:         "simplepattern=*foo !bar* *",
+			expectedType: &SimplePatterns{},
+		},
+		{
+			valid:        false,
+			line:         "unknown=*foo !bar* *",
+			expectedType: nil,
+		},
+		{
+			valid:        false,
+			line:         "no method",
+			expectedType: nil,
+		},
+		{
+			valid:        false,
+			line:         "=empty",
+			expectedType: nil,
+		},
+	}
+
+	for _, c := range cases {
+		m, err := CreateMatcher(c.line)
+		assert.IsType(t, c.expectedType, m)
+		if c.valid {
+			assert.NoError(t, err)
+		} else {
+			assert.Error(t, err)
+		}
+	}
 
 }
