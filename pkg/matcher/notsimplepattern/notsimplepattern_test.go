c9394a69f55e5be9e55348ef31411dbea235dffd
lgz <ilyamaschenko@gmail.com>
2019-01-10 23:31:35 +0300

    wip

--- pkg/matcher/notsimplepattern/notsimplepattern_test.go
+++ pkg/matcher/notsimplepattern/notsimplepattern_test.go
@@ -1,96 +1,58 @@
 package notsimplepattern
 
-//func TestShellMatch_Match(t *testing.T) {
-//	m := ShellMatch{"*bar*"}
-//
-//	cases := []struct {
-//		expected bool
-//		line     string
-//	}{
-//		{
-//			expected: true,
-//			line:     "Would you come into the bar and have a drink with me?",
-//		},
-//		{
-//			expected: true,
-//			line:     "The hotel has a licensed bar.",
-//		},
-//		{
-//			expected: false,
-//			line:     "This will never fail!",
-//		},
-//	}
-//
-//	for _, c := range cases {
-//		assert.Equal(t, c.expected, m.Match(c.line))
-//	}
-//}
-//
-//func TestSimplePatterns_Match(t *testing.T) {
-//	m, err := CreateSimplePatterns("*foobar* !foo* *Bar*")
-//
-//	require.NoError(t, err)
-//
-//	cases := []struct {
-//		expected bool
-//		line     string
-//	}{
-//		{
-//			expected: false,
-//			line:     "Would you come into the bar and have a drink with me?",
-//		},
-//		{
-//			expected: true,
-//			line:     "His parents destined him for a career at the Bar.",
-//		},
-//		{
-//			expected: false,
-//			line:     "This will never fail!",
-//		},
-//	}
-//
-//	for _, c := range cases {
-//		assert.Equal(t, c.expected, m.Match(c.line))
-//	}
-//}
-//
-//func TestSimplePatterns_Add(t *testing.T) {
-//	sps := &SimplePatterns{}
-//
-//	cases := []struct {
-//		error   error
-//		pattern string
-//	}{
-//		{
-//			error:   nil,
-//			pattern: "Totally valid [pattern]",
-//		},
-//		{
-//			error:   nil,
-//			pattern: "*ally valid? [pattern]",
-//		},
-//		{
-//			error:   filepath.ErrBadPattern,
-//			pattern: "[]",
-//		},
-//	}
-//
-//	for _, c := range cases {
-//		assert.Equal(t, c.error, sps.Add(c.pattern))
-//	}
-//
-//	assert.Len(t, sps.Patterns, 2)
-//}
-//
-//func TestCreateSimplePatterns(t *testing.T) {
-//	line := "*foobar* !foo* !*bar *"
-//
-//	sps, err := CreateSimplePatterns(line)
-//
-//	require.NoError(t, err)
-//	assert.Len(t, sps.Patterns, 4)
-//	assert.False(t, sps.Patterns[0].Exclude)
-//	assert.True(t, sps.Patterns[1].Exclude)
-//	assert.True(t, sps.Patterns[2].Exclude)
-//	assert.False(t, sps.Patterns[3].Exclude)
-//}
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	sps := New()
+
+	assert.False(t, sps.UseCache)
+	assert.NotNil(t, sps.Cache)
+}
+
+func TestCreate(t *testing.T) {
+	expr := "*foobar* !foo* !*bar *"
+
+	sps, err := Create(expr)
+
+	require.NoError(t, err)
+	assert.Len(t, sps.patterns, 4)
+	assert.False(t, sps.patterns[0].exclude)
+	assert.True(t, sps.patterns[1].exclude)
+	assert.True(t, sps.patterns[2].exclude)
+	assert.False(t, sps.patterns[3].exclude)
+
+}
+
+func TestPatterns_Match(t *testing.T) {
+	m, err := Create("*foobar* !foo* *Bar*")
+
+	require.NoError(t, err)
+
+	cases := []struct {
+		expected bool
+		line     string
+	}{
+		{
+			expected: false,
+			line:     "Would you come into the bar and have a drink with me?",
+		},
+		{
+			expected: true,
+			line:     "His parents destined him for a career at the Bar.",
+		},
+		{
+			expected: false,
+			line:     "This will never fail!",
+		},
+	}
+
+	for _, c := range cases {
+		assert.Equal(t, c.expected, m.Match(c.line))
+	}
+
+}
--- pkg/matcher/regexp_test.go
+++ pkg/matcher/regexp_test.go
@@ -32,30 +32,3 @@ func TestRegExpMatch_Match(t *testing.T) {
 		assert.Equal(t, c.expected, m.Match(c.line))
 	}
 }
-
-//func TestGlobMatch_Match(t *testing.T) {
-//	m := RegExpMatch{regexp.MustCompile("[0-9]+")}
-//
-//	cases := []struct {
-//		expected bool
-//		line     string
-//	}{
-//		{
-//			expected: true,
-//			line:     "2019",
-//		},
-//		{
-//			expected: true,
-//			line:     "It's over 9000!",
-//		},
-//		{
-//			expected: false,
-//			line:     "This will never fail!",
-//		},
-//	}
-//
-//	for _, c := range cases {
-//		assert.Equal(t, c.expected, m.Match(c.line))
-//	}
-//
-//}
