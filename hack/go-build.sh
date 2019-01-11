ba417b5a3b6e31b5e7841a0b75dfbdbb07cff7fa
Paweł Krupa <pawel@krupa.net.pl>
2019-01-11 15:31:44 +0100

    Add release mechanism and code ownership (#106)
    * * Change configuration directory name
    * Add github publisher script
    * Add cross-arch cross-os builder script
    * Add help target to Makefile
    * Better Makefile
    * Multi-stage travis configuration
    * Add github CODEOWNERS file
    
    * simplify .gitignore
    
    * revert naming of configuration directory

---
+++ hack/go-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+WHICH="$1"
+PLATFORMS=(darwin/386 darwin/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips linux/mipsle linux/mips64 linux/mips64le)
+
+GOFLAGS=${GOFLAGS:-}
+GLDFLAGS=${GLDFLAGS:-}
+VERSION="${TRAVIS_TAG}"
+: "${VERSION:=undefined}"
+
+echo "Building binaries for version: $VERSION"
+if [ "${WHICH}" != "all" ]; then
+	eval $(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")
+	: "${GOOS:=${GOHOSTOS}}"
+	: "${GOARCH:=${GOHOSTARCH}}"
+	CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/godplugin github.com/netdata/go.d.plugin/cmd/godplugin
+else
+	for PLATFORM in "${PLATFORMS[@]}"; do
+		PLTFRM_SPLT=(${PLATFORM//\// })
+		GOOS=${PLTFRM_SPLT[0]}
+		GOARCH=${PLTFRM_SPLT[1]}
+		FILE="bin/go.d.plugin-${VERSION}.${GOOS}-${GOARCH}"
+		CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o "${FILE}" github.com/netdata/go.d.plugin/cmd/godplugin
+	done
+fi
+
