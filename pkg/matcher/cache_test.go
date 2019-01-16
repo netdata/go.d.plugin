1444a20f88fa17d3fe0e3e1d9ada1fefb3e4abb3
Wing924 <weihe924stephen@gmail.com>
2019-01-16 14:47:15 +0900

    improve matcher benchmark

--- pkg/matcher/cache_test.go
+++ pkg/matcher/cache_test.go
@@ -20,20 +20,32 @@ func TestWithCache_specialCase(t *testing.T) {
 	assert.Equal(t, TRUE(), WithCache(TRUE()))
 	assert.Equal(t, FALSE(), WithCache(FALSE()))
 }
-
-func BenchmarkCachedMatcher_Match(b *testing.B) {
-	b.Run("raw", func(b *testing.B) {
-		m := globMatcher("abc*def*ghi")
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			m.MatchString("abc123def456ghi")
-		}
-	})
-	b.Run("cached", func(b *testing.B) {
-		m := WithCache(globMatcher("abc*def*ghi"))
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			m.MatchString("abc123def456ghi")
-		}
-	})
+func BenchmarkCachedMatcher_MatchString_cache_hit(b *testing.B) {
+	benchmarks := []struct {
+		name   string
+		expr   string
+		target string
+	}{
+		{"stringFullMatcher", "= abc123", "abc123"},
+		{"stringPrefixMatcher", "~ ^abc123", "abc123456"},
+		{"stringSuffixMatcher", "~ abc123$", "hello abc123"},
+		{"stringSuffixMatcher", "~ abc123", "hello abc123 world"},
+		{"globMatcher", "* abc*def", "abc12345678def"},
+		{"regexp", "~ [0-9]+", "1234567890"},
+	}
+	for _, bm := range benchmarks {
+		m := Must(Parse(bm.expr))
+		b.Run(bm.name+"_raw", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				m.MatchString(bm.target)
+			}
+		})
+		b.Run(bm.name+"_cache", func(b *testing.B) {
+			cached := WithCache(m)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				cached.MatchString(bm.target)
+			}
+		})
+	}
 }
