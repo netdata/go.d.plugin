9abedfd91a706f208daad3c3e7062a11ecb72641
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2019-01-16 10:13:30 +0200

    add mysql stats collector, same as netdata python's module (TODO: charts and README)

--- modules/mysql/README.md
+++ modules/mysql/README.md
@@ -11,16 +11,6 @@ _TODO:_
 ```yaml
 jobs:
   - name: local
-    host: ::1
-    port: 3306
-    user: user
-    pass: pass
-    # socket: /var/run/mysqld/mysql.sock
-```
-
-```yaml
-jobs:
-  - name: local
     dsn: [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
     # username:password@protocol(address)/dbname?param=value
     # user:password@/dbname
--- modules/mysql/mysql.go
+++ modules/mysql/mysql.go
@@ -2,8 +2,10 @@ package mysql
 
 import (
 	"database/sql"
+	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/netdata/go.d.plugin/modules"
@@ -25,14 +27,6 @@ type MySQL struct {
 	db *sql.DB
 
 	DSN string `yaml:"dsn"` // i.e user:password@/dbname
-	// // or
-	// User string `yaml:"user"`
-	// Pass string `yaml:"pass"`
-
-	// Host string `yaml:"host"`
-	// Port int    `yaml:"port"`
-	// // or
-	// Socket string `yaml:"socket"`
 }
 
 // New creates and returns a new empty MySQL module.
@@ -123,5 +117,389 @@ func (m *MySQL) Charts() *Charts {
 
 // Collect collects health checks and metrics for MySQL.
 func (m *MySQL) Collect() map[string]int64 {
+	if err := m.openConnection(); err != nil {
+		return nil
+	}
+
+	metrics := make(map[string]int64)
+
+	if err := m.collectGlobalStats(metrics); err != nil {
+		m.Errorf("error on collecting global stats: %v", err)
+		return nil
+	}
+	// m.Debugf("total metrics length after stats: %d\n", len(metrics))
+
+	if err := m.collectSlaveStatus(metrics); err != nil {
+		m.Errorf("error on collecting slave status: %v", err)
+		return nil
+	}
+	// m.Debugf("total metrics length after slave status: %d\n", len(metrics))
+
+	if err := m.collectMaxConnections(metrics); err != nil {
+		m.Errorf("error on determinating max connections: %v", err)
+		return nil
+	}
+	// m.Debugf("total metrics length after max connections determination: %d\n", len(metrics))
+
+	return metrics
+}
+
+var globalStats = [...]string{
+	"Bytes_received",
+	"Bytes_sent",
+	"Queries",
+	"Questions",
+	"Slow_queries",
+	"Handler_commit",
+	"Handler_delete",
+	"Handler_prepare",
+	"Handler_read_first",
+	"Handler_read_key",
+	"Handler_read_next",
+	"Handler_read_prev",
+	"Handler_read_rnd",
+	"Handler_read_rnd_next",
+	"Handler_rollback",
+	"Handler_savepoint",
+	"Handler_savepoint_rollback",
+	"Handler_update",
+	"Handler_write",
+	"Table_locks_immediate",
+	"Table_locks_waited",
+	"Select_full_join",
+	"Select_full_range_join",
+	"Select_range",
+	"Select_range_check",
+	"Select_scan",
+	"Sort_merge_passes",
+	"Sort_range",
+	"Sort_scan",
+	"Created_tmp_disk_tables",
+	"Created_tmp_files",
+	"Created_tmp_tables",
+	"Connections",
+	"Aborted_connects",
+	"Max_used_connections",
+	"Binlog_cache_disk_use",
+	"Binlog_cache_use",
+	"Threads_connected",
+	"Threads_created",
+	"Threads_cached",
+	"Threads_running",
+	"Thread_cache_misses",
+	"Innodb_data_read",
+	"Innodb_data_written",
+	"Innodb_data_reads",
+	"Innodb_data_writes",
+	"Innodb_data_fsyncs",
+	"Innodb_data_pending_reads",
+	"Innodb_data_pending_writes",
+	"Innodb_data_pending_fsyncs",
+	"Innodb_log_waits",
+	"Innodb_log_write_requests",
+	"Innodb_log_writes",
+	"Innodb_os_log_fsyncs",
+	"Innodb_os_log_pending_fsyncs",
+	"Innodb_os_log_pending_writes",
+	"Innodb_os_log_written",
+	"Innodb_row_lock_current_waits",
+	"Innodb_rows_inserted",
+	"Innodb_rows_read",
+	"Innodb_rows_updated",
+	"Innodb_rows_deleted",
+	"Innodb_buffer_pool_pages_data",
+	"Innodb_buffer_pool_pages_dirty",
+	"Innodb_buffer_pool_pages_free",
+	"Innodb_buffer_pool_pages_flushed",
+	"Innodb_buffer_pool_pages_misc",
+	"Innodb_buffer_pool_pages_total",
+	"Innodb_buffer_pool_bytes_data",
+	"Innodb_buffer_pool_bytes_dirty",
+	"Innodb_buffer_pool_read_ahead",
+	"Innodb_buffer_pool_read_ahead_evicted",
+	"Innodb_buffer_pool_read_ahead_rnd",
+	"Innodb_buffer_pool_read_requests",
+	"Innodb_buffer_pool_write_requests",
+	"Innodb_buffer_pool_reads",
+	"Innodb_buffer_pool_wait_free",
+	"Qcache_hits",
+	"Qcache_lowmem_prunes",
+	"Qcache_inserts",
+	"Qcache_not_cached",
+	"Qcache_queries_in_cache",
+	"Qcache_free_memory",
+	"Qcache_free_blocks",
+	"Qcache_total_blocks",
+	"Key_blocks_unused",
+	"Key_blocks_used",
+	"Key_blocks_not_flushed",
+	"Key_read_requests",
+	"Key_write_requests",
+	"Key_reads",
+	"Key_writes",
+	"Open_files",
+	"Opened_files",
+	"Binlog_stmt_cache_disk_use",
+	"Binlog_stmt_cache_use",
+	"Connection_errors_accept",
+	"Connection_errors_internal",
+	"Connection_errors_max_connections",
+	"Connection_errors_peer_address",
+	"Connection_errors_select",
+	"Connection_errors_tcpwrap",
+	"wsrep_local_recv_queue",
+	"wsrep_local_send_queue",
+	"wsrep_received",
+	"wsrep_replicated",
+	"wsrep_received_bytes",
+	"wsrep_replicated_bytes",
+	"wsrep_local_bf_aborts",
+	"wsrep_local_cert_failures",
+	"wsrep_flow_control_paused_ns",
+	"Com_delete",
+	"Com_insert",
+	"Com_select",
+	"Com_update",
+	"Com_replace",
+}
+
+func extractStatKey(s string) (string, bool) {
+	for _, varName := range globalStats {
+		if s == varName {
+			return toKey(s), true
+		}
+	}
+
+	return s, false
+}
+
+func toKey(s string) string {
+	return strings.ToLower(s)
+}
+
+func (m *MySQL) collectGlobalStats(metrics map[string]int64) error {
+	rows, err := m.db.Query("SHOW GLOBAL STATUS")
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			varName string
+			// it does not always returns int64, i.e public key []uint8(bytes) we can't catch it before Scan, so interface{}.
+			value interface{}
+		)
+
+		// Variable_name
+		// Value
+		err = rows.Scan(&varName, &value)
+		if err != nil {
+			return err
+		}
+
+		if key, ok := extractStatKey(varName); ok {
+			metrics[key], err = strconv.ParseInt(fmt.Sprintf("%s", value), 10, 64)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
+
+	/*
+	   [sort_merge_passes] = [0]
+	   [com_delete] = [0]
+	   [innodb_buffer_pool_read_ahead_rnd] = [0]
+	   [innodb_data_pending_fsyncs] = [0]
+	   [key_blocks_unused] = [6698]
+	   [key_read_requests] = [0]
+	   [key_reads] = [0]
+	   [select_full_range_join] = [0]
+	   [bytes_received] = [4014]
+	   [connections] = [22]
+	   [handler_read_prev] = [0]
+	   [innodb_buffer_pool_wait_free] = [0]
+	   [innodb_data_writes] = [362]
+	   [innodb_data_written] = [5153792]
+	   [table_locks_waited] = [0]
+	   [created_tmp_files] = [5]
+	   [innodb_data_read] = [15864832]
+	   [open_files] = [7]
+	   [com_select] = [18]
+	   [innodb_log_waits] = [0]
+	   [select_range] = [0]
+	   [handler_read_next] = [6081]
+	   [innodb_buffer_pool_bytes_data] = [4210688]
+	   [innodb_buffer_pool_pages_misc] = [6]
+	   [sort_scan] = [4]
+	   [handler_savepoint] = [0]
+	   [handler_update] = [346]
+	   [innodb_buffer_pool_pages_total] = [512]
+	   [innodb_os_log_fsyncs] = [71]
+	   [queries] = [61]
+	   [threads_cached] = [0]
+	   [innodb_buffer_pool_read_ahead] = [14]
+	   [bytes_sent] = [156380]
+	   [connection_errors_peer_address] = [0]
+	   [created_tmp_disk_tables] = [0]
+	   [handler_rollback] = [0]
+	   [handler_savepoint_rollback] = [0]
+	   [handler_write] = [5222]
+	   [innodb_buffer_pool_pages_free] = [249]
+	   [innodb_buffer_pool_write_requests] = [1945]
+	   [sort_range] = [0]
+	   [threads_connected] = [3]
+	   [created_tmp_tables] = [20]
+	   [handler_read_first] = [39]
+	   [innodb_buffer_pool_read_requests] = [21518]
+	   [key_blocks_used] = [0]
+	   [max_used_connections] = [3]
+	   [select_full_join] = [2]
+	   [innodb_buffer_pool_pages_flushed] = [215]
+	   [binlog_cache_use] = [0]
+	   [com_insert] = [0]
+	   [com_replace] = [0]
+	   [connection_errors_internal] = [0]
+	   [connection_errors_tcpwrap] = [0]
+	   [innodb_buffer_pool_pages_data] = [257]
+	   [innodb_buffer_pool_pages_dirty] = [0]
+	   [innodb_os_log_written] = [89600]
+	   [threads_created] = [3]
+	   [innodb_data_fsyncs] = [105]
+	   [innodb_data_reads] = [987]
+	   [innodb_rows_read] = [7208]
+	   [key_write_requests] = [0]
+	   [key_writes] = [0]
+	   [slow_queries] = [0]
+	   [innodb_data_pending_writes] = [0]
+	   [innodb_os_log_pending_fsyncs] = [0]
+	   [aborted_connects] = [1]
+	   [handler_prepare] = [0]
+	   [innodb_log_writes] = [98]
+	   [innodb_rows_deleted] = [0]
+	   [innodb_rows_updated] = [346]
+	   [key_blocks_not_flushed] = [0]
+	   [binlog_cache_disk_use] = [0]
+	   [connection_errors_accept] = [0]
+	   [connection_errors_max_connections] = [0]
+	   [connection_errors_select] = [0]
+	   [innodb_buffer_pool_read_ahead_evicted] = [0]
+	   [innodb_os_log_pending_writes] = [0]
+	   [binlog_stmt_cache_use] = [0]
+	   [com_update] = [0]
+	   [handler_delete] = [0]
+	   [handler_read_key] = [2363]
+	   [handler_read_rnd] = [360]
+	   [handler_read_rnd_next] = [10166]
+	   [innodb_data_pending_reads] = [0]
+	   [innodb_rows_inserted] = [0]
+	   [handler_commit] = [678]
+	   [innodb_log_write_requests] = [765]
+	   [opened_files] = [28]
+	   [questions] = [37]
+	   [select_range_check] = [0]
+	   [binlog_stmt_cache_disk_use] = [0]
+	   [innodb_buffer_pool_bytes_dirty] = [0]
+	   [innodb_buffer_pool_reads] = [950]
+	   [innodb_row_lock_current_waits] = [0]
+	   [select_scan] = [35]
+	   [table_locks_immediate] = [12]
+	   [threads_running] = [2]
+	*/
+}
+
+func slaveSeconds(value interface{}) int64 {
+	v, err := strconv.ParseInt(fmt.Sprintf("%s", value), 10, 64)
+	if err != nil {
+		return -1
+	}
+	return v
+}
+
+func slaveRunning(value interface{}) int64 {
+	if v, ok := value.(string); ok {
+		if v == "Yes" {
+			return 1
+		}
+
+		return -1
+	}
+
+	if v := fmt.Sprintf("%s", value); v == "Yes" {
+		return 1
+	}
+
+	return -1
+}
+
+var slaveStats = map[string]func(value interface{}) int64{
+	"Seconds_Behind_Master": slaveSeconds,
+	"Slave_SQL_Running":     slaveRunning,
+	"Slave_IO_Running":      slaveRunning,
+}
+
+// https://dev.mysql.com/doc/refman/8.0/en/show-slave-status.html
+func (m *MySQL) collectSlaveStatus(metrics map[string]int64) error {
+	rows, err := m.db.Query("SHOW SLAVE STATUS")
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			varName string
+			value   interface{}
+		)
+
+		err = rows.Scan(&varName, &value)
+		if err != nil {
+			return err
+		}
+
+		// fmt.Printf("var name: %s | value: %s\n", varName, value)
+
+		if valFn, ok := slaveStats[varName]; ok { // hmmm...
+			value := valFn(value)
+			metrics[toKey(varName)] = value
+		}
+	}
+
+	return nil
+}
+
+// https://dev.mysql.com/doc/refman/8.0/en/show-variables.html
+func (m *MySQL) collectMaxConnections(metrics map[string]int64) error {
+	rows, err := m.db.Query("SHOW GLOBAL VARIABLES LIKE 'max_connections'") // only one result, i.e "max_conections" = 151
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	if !rows.Next() {
+		return nil
+	}
+
+	var (
+		varName string
+		value   int64
+	)
+
+	err = rows.Scan(&varName, &value)
+	if err != nil {
+		return err
+	}
+
+	metrics["max_connections"] = value
+
+	return nil
+
+	/*
+		[max_connections] = [151]
+	*/
 }
