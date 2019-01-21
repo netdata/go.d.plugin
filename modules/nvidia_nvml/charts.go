931dddf14272a5295186338c90a1c63ff4163f8c
lgz <ilyamaschenko@gmail.com>
2019-01-21 22:42:43 +0300

    init

--- modules/nvidia_nvml/charts.go
+++ modules/nvidia_nvml/charts.go
@@ -1,1 +1,80 @@
 package nvidia_nvml
+
+import (
+	"fmt"
+
+	"github.com/netdata/go.d.plugin/modules"
+)
+
+type (
+	// Charts is an alias for modules.Charts
+	Charts = modules.Charts
+	// Dims is an alias for modules.Dims
+	Dims = modules.Dims
+)
+
+var charts = Charts{
+	{
+		ID:    "%s_utilization",
+		Title: "Utilization Rates",
+		Units: "percent",
+		Ctx:   "nvidia_nvml.utilization_rates",
+		Dims: Dims{
+			{ID: "%s_gpu_util", Name: "gpu"},
+			{ID: "%s_mem_util", Name: "memory"},
+			{ID: "%s_encoder_util", Name: "encoder"},
+			{ID: "%s_decoder_util", Name: "decoder"},
+		},
+	},
+	{
+		ID:    "%s_memory",
+		Title: "Memory Usage",
+		Units: "KiB",
+		Ctx:   "nvidia_nvml.memory_usage",
+		Dims: Dims{
+			{ID: "%s_memory_free", Name: "free", Div: 1024},
+			{ID: "%s_memory_used", Name: "used", Div: 1024},
+		},
+	},
+	{
+		ID:    "%s_temperature",
+		Title: "Temperature",
+		Units: "celsius",
+		Ctx:   "nvidia_nvml.temperature",
+		Dims: Dims{
+			{ID: "%s_temperature", Name: "temperature"},
+		},
+	},
+	{
+		ID:    "%s_fan_speed",
+		Title: "Fan Speed",
+		Units: "percent",
+		Ctx:   "nvidia_nvml.fan_speed",
+		Dims: Dims{
+			{ID: "%s_fan_speed", Name: "speed"},
+		},
+	},
+	{
+		ID:    "%s_power_usage",
+		Title: "Power Usage",
+		Units: "watts",
+		Ctx:   "nvidia_nvml.power_usage",
+		Dims: Dims{
+			{ID: "%s_power_usage", Name: "usage", Div: 1000},
+		},
+	},
+}
+
+func createGPUCharts(gpu gpu) *Charts {
+	charts := charts.Copy()
+
+	for _, chart := range *charts {
+		chart.ID = fmt.Sprintf(chart.ID, gpu.uniqName())
+		chart.Fam = gpu.name
+		for _, dim := range chart.Dims {
+			dim.ID = fmt.Sprintf(dim.ID, gpu.uniqName())
+		}
+	}
+
+	return charts
+}
--- modules/nvidia_nvml/nvidia_nvml.go
+++ modules/nvidia_nvml/nvidia_nvml.go
@@ -1,1 +1,85 @@
 package nvidia_nvml
+
+import (
+	"github.com/mindprince/gonvml"
+	"github.com/netdata/go.d.plugin/modules"
+)
+
+func init() {
+	creator := modules.Creator{
+		//DisabledByDefault: true,
+		Create: func() modules.Module { return New() },
+	}
+
+	modules.Register("nvidia_nvml", creator)
+}
+
+// New creates NvidiaNVML with default values.
+func New() *NvidiaNVML {
+	return &NvidiaNVML{
+		charts: &Charts{},
+	}
+}
+
+// NvidiaNVML NvidiaNVML module.
+type NvidiaNVML struct {
+	modules.Base
+
+	charts *Charts
+}
+
+// Cleanup makes cleanup.
+func (NvidiaNVML) Cleanup() {
+	_ = gonvml.Shutdown()
+}
+
+// Init makes initialization.
+func (n NvidiaNVML) Init() bool {
+	if err := gonvml.Initialize(); err != nil {
+		n.Errorf("error on nvml initialization : %v", err)
+		return false
+	}
+
+	gpus, err := getGPUs()
+
+	if err != nil {
+		n.Error(err)
+		return false
+	}
+
+	for _, g := range gpus {
+		_ = n.charts.Add(*createGPUCharts(g)...)
+	}
+
+	return true
+}
+
+// Check makes check.
+func (NvidiaNVML) Check() bool {
+	return true
+}
+
+// Charts creates Charts.
+func (n NvidiaNVML) Charts() *Charts {
+	return n.charts
+}
+
+// Collect collects metrics.
+func (n *NvidiaNVML) Collect() map[string]int64 {
+	m := make(map[string]int64)
+
+	gpus, err := getGPUs()
+
+	if err != nil {
+		n.Error(err)
+		return nil
+	}
+
+	for _, gpu := range gpus {
+		for k, v := range gpu.stats.asMap() {
+			m[gpu.uniqName()+"_"+k] = v
+		}
+	}
+
+	return m
+}
