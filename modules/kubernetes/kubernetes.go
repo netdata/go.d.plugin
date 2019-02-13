767d8713e4950cbfe417b42912f40f31a41de360
lgz <ilyamaschenko@gmail.com>
2019-02-13 12:47:30 +0300

    wip

--- modules/kubernetes/kubernetes.go
+++ modules/kubernetes/kubernetes.go
@@ -1,6 +1,10 @@
 package kubernetes
 
 import (
+	"time"
+
+	"github.com/netdata/go.d.plugin/pkg/web"
+
 	"github.com/netdata/go-orchestrator/module"
 )
 
@@ -12,13 +16,36 @@ func init() {
 	module.Register("kubernetes", creator)
 }
 
+const (
+	defaultHTTPTimeout = time.Second * 2
+	defaultURL         = "http://127.0.0.1:10255"
+)
+
+type Config struct {
+	web.HTTP `yaml:",inline"`
+}
+
 // New creates Kubernetes with default values.
-func New() *Kubernetes { return &Kubernetes{} }
+func New() *Kubernetes {
+	return &Kubernetes{
+		Config: Config{
+			HTTP: web.HTTP{
+				Request: web.Request{URL: defaultURL},
+				Client:  web.Client{Timeout: web.Duration{Duration: defaultHTTPTimeout}},
+			},
+		},
+		activePods: make(map[string]bool),
+	}
+}
 
 // Kubernetes Kubernetes module.
 type Kubernetes struct {
 	module.Base
+	Config `yaml:",inline"`
+
 	apiClient apiClient
+	// TODO: likely wrong
+	activePods map[string]bool
 }
 
 // Cleanup makes cleanup.
