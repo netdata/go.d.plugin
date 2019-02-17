13b5adb2e80a411ac3f9a763fd59002b0a1e2bf6
Ilya Mashchenko <ilyamaschenko@gmail.com>
2019-02-17 22:58:20 +0300

    apache fix (#152)
    * refactor
    
    * update testdata
    
    * minor

--- modules/apache/apache_test.go
+++ modules/apache/apache_test.go
@@ -6,8 +6,6 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/netdata/go.d.plugin/pkg/web"
-
 	"github.com/netdata/go-orchestrator/module"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -18,83 +16,103 @@ var (
 	simpleStatus, _   = ioutil.ReadFile("testdata/status-simple.txt")
 )
 
-func TestApache_Cleanup(t *testing.T) {
-	New().Cleanup()
-}
+func TestApache_Cleanup(t *testing.T) { New().Cleanup() }
 
 func TestNew(t *testing.T) {
-	mod := New()
+	job := New()
 
-	assert.Implements(t, (*module.Module)(nil), mod)
-	assert.Equal(t, defaultURL, mod.URL)
-	assert.Equal(t, defaultHTTPTimeout, mod.Timeout.Duration)
+	assert.Implements(t, (*module.Module)(nil), job)
+	assert.Equal(t, defaultURL, job.URL)
+	assert.Equal(t, defaultHTTPTimeout, job.Timeout.Duration)
+	assert.NotNil(t, job.charts)
 }
 
 func TestApache_Init(t *testing.T) {
-	mod := New()
+	job := New()
 
-	require.True(t, mod.Init())
-	assert.NotNil(t, mod.apiClient)
+	require.True(t, job.Init())
+	assert.NotNil(t, job.apiClient)
 }
 
 func TestApache_InitNG(t *testing.T) {
-	mod := New()
+	job := New()
 
-	mod.HTTP.Request = web.Request{URL: ""}
-	assert.False(t, mod.Init())
+	job.URL = ""
+	assert.False(t, job.Init())
 }
 
 func TestApache_Check(t *testing.T) {
 	ts := httptest.NewServer(
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
-				if r.URL.Path == "/server-status" {
-					_, _ = w.Write(simpleStatus)
-					return
-				}
+				_, _ = w.Write(simpleStatus)
 			}))
-
 	defer ts.Close()
 
-	mod := New()
-
-	mod.HTTP.Request = web.Request{URL: ts.URL + "/server-status?auto"}
-	require.True(t, mod.Init())
-	assert.True(t, mod.Check())
+	job := New()
+	job.URL = ts.URL + "/server-status?auto"
+	require.True(t, job.Init())
+	assert.True(t, job.Check())
 }
 
 func TestApache_CheckNG(t *testing.T) {
-	mod := New()
+	job := New()
 
-	mod.HTTP.Request = web.Request{URL: "http://127.0.0.1:38001/server-status?auto"}
-	require.True(t, mod.Init())
-	assert.False(t, mod.Check())
+	job.URL = "http://127.0.0.1:38001/server-status?auto"
+	require.True(t, job.Init())
+	assert.False(t, job.Check())
 }
 
-func TestApache_Charts(t *testing.T) {
-	mod := New()
-
-	assert.NotNil(t, mod.Charts())
-}
+func TestApache_Charts(t *testing.T) { assert.NotNil(t, New().Charts()) }
 
 func TestApache_Collect(t *testing.T) {
 	ts := httptest.NewServer(
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
-				if r.URL.Path == "/server-status" {
-					_, _ = w.Write(extendedStatus)
-					return
-				}
+				_, _ = w.Write(simpleStatus)
 			}))
 	defer ts.Close()
 
-	mod := New()
-	mod.HTTP.Request = web.Request{URL: ts.URL + "/server-status?auto"}
+	job := New()
+	job.URL = ts.URL + "/server-status?auto"
+	require.True(t, job.Init())
+	require.True(t, job.Check())
 
-	require.True(t, mod.Init())
-	require.True(t, mod.Check())
+	expected := map[string]int64{
+		"conns_async_closing":     0,
+		"scoreboard_waiting":      74,
+		"scoreboard_reading":      0,
+		"busy_workers":            1,
+		"conns_async_writing":     0,
+		"conns_async_keep_alive":  0,
+		"scoreboard_starting":     0,
+		"idle_workers":            74,
+		"conns_total":             0,
+		"scoreboard_keepalive":    0,
+		"scoreboard_idle_cleanup": 0,
+		"scoreboard_sending":      1,
+		"scoreboard_dns_lookup":   0,
+		"scoreboard_closing":      0,
+		"scoreboard_logging":      0,
+		"scoreboard_finishing":    0,
+		"scoreboard_open":         325,
+	}
 
-	metrics := mod.Collect()
+	assert.Equal(t, expected, job.Collect())
+}
+
+func TestApache_CollectExtended(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(extendedStatus)
+			}))
+	defer ts.Close()
+
+	job := New()
+	job.URL = ts.URL + "/server-status?auto"
+	require.True(t, job.Init())
+	require.True(t, job.Check())
 
 	expected := map[string]int64{
 		"conns_async_closing":     0,
@@ -122,36 +140,33 @@ func TestApache_Collect(t *testing.T) {
 		"bytes_per_req":           136533000,
 	}
 
-	assert.Equal(t, expected, metrics)
+	assert.Equal(t, expected, job.Collect())
 }
 
 func TestApache_InvalidData(t *testing.T) {
 	ts := httptest.NewServer(
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
-				if r.URL.Path == "/server-status" {
-					_, _ = w.Write([]byte("hello and goodbye"))
-					return
-				}
+				_, _ = w.Write([]byte("hello and goodbye"))
 			}))
 	defer ts.Close()
 
-	mod := New()
-	mod.HTTP.Request = web.Request{URL: ts.URL + "/server-status?auto"}
-
-	require.True(t, mod.Init())
-	assert.False(t, mod.Check())
+	job := New()
+	job.URL = ts.URL + "/server-status?auto"
+	require.True(t, job.Init())
+	assert.False(t, job.Check())
 }
 
 func TestApache_404(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(404)
-	}))
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusNotFound)
+			}))
 	defer ts.Close()
 
-	mod := New()
-	mod.HTTP.Request = web.Request{URL: ts.URL + "/server-status?auto"}
-
-	require.True(t, mod.Init())
-	assert.False(t, mod.Check())
+	job := New()
+	job.URL = ts.URL + "/server-status?auto"
+	require.True(t, job.Init())
+	assert.False(t, job.Check())
 }
