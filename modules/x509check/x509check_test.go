6f703c13c8010abfb116ea9ca7e387f413112e40
lgz <ilyamaschenko@gmail.com>
2019-03-14 10:00:38 +0300

    minor

--- modules/x509check/x509check_test.go
+++ modules/x509check/x509check_test.go
@@ -25,19 +25,20 @@ func TestX509Check_Charts(t *testing.T) {
 
 }
 
-//func TestX509Check_Init(t *testing.T) {
-//	job := New()
-//	assert.False(t, job.Init())
-//
-//	job = New()
-//	job.Source = "wrong"
-//	assert.False(t, job.Init())
-//
-//	job = New()
-//	job.Source = "https://example.org:443"
-//	assert.True(t, job.Init())
-//}
+func TestX509Check_Init(t *testing.T) {
+	job := New()
+	assert.False(t, job.Init())
 
+	job = New()
+	job.Source = "wrong"
+	assert.False(t, job.Init())
+
+	job = New()
+	job.Source = "https://example.org:443"
+	assert.True(t, job.Init())
+}
+
+// TODO:
 func TestX509Check_Check(t *testing.T) {}
 
 func TestX509Check_Collect(t *testing.T) {}
