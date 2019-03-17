558034a4be51b177e2aa68972a8e60f6e1da5835
Wing924 <weihe924stephen@gmail.com>
2019-03-17 14:32:24 +0900

    wip

--- modules/weblog/charts.go
+++ modules/weblog/charts.go
@@ -124,260 +124,158 @@ var (
 		Type:  module.Stacked,
 	}
 
-//	requestsPerUserDefined = Chart{
-//		ID:    "requests_per_user_defined",
-//		Title: "Requests Per User Defined Pattern",
-//		Units: "requests/s",
-//		Fam:   "user defined",
-//		Ctx:   "web_log.requests_per_user_defined",
-//		Type:  module.Stacked,
-//	}
-//	requestsPerHTTPMethod = Chart{
-//		ID:    "requests_per_http_method",
-//		Title: "Requests Per HTTP Method",
-//		Units: "requests/s",
-//		Fam:   "http methods",
-//		Ctx:   "web_log.requests_per_http_method",
-//		Type:  module.Stacked,
-//		Dims: Dims{
-//			{ID: "GET", Algo: module.Incremental},
-//		},
-//	}
-//	requestsPerHTTPVersion = Chart{
-//		ID:    "requests_per_http_version",
-//		Title: "Requests Per HTTP Version",
-//		Units: "requests/s",
-//		Fam:   "http versions",
-//		Ctx:   "web_log.requests_per_http_version",
-//		Type:  module.Stacked,
-//	}
-//	requestsPerIPProto = Chart{
-//		ID:    "requests_per_ip_proto",
-//		Title: "Requests Per IP Protocol",
-//		Units: "requests/s",
-//		Fam:   "ip protocols",
-//		Ctx:   "web_log.requests_per_ip_proto",
-//		Type:  module.Stacked,
-//		Dims: Dims{
-//			{ID: "req_ipv4", Name: "ipv4", Algo: module.Incremental},
-//			{ID: "req_ipv6", Name: "ipv6", Algo: module.Incremental},
-//		},
-//	}
-//	requestsPerVhost = Chart{
-//		ID:    "requests_per_vhost",
-//		Title: "Requests Per Vhost",
-//		Units: "requests/s",
-//		Fam:   "vhost",
-//		Ctx:   "web_log.requests_per_vhost",
-//		Type:  module.Stacked,
-//	}
-//	currentPollIPs = Chart{
-//		ID:    "clients_current",
-//		Title: "Current Poll Unique Client IPs",
-//		Units: "unique ips",
-//		Fam:   "clients",
-//		Ctx:   "web_log.current_poll_ips",
-//		Type:  module.Stacked,
-//		Dims: Dims{
-//			{ID: "unique_current_poll_ipv4", Name: "ipv4", Algo: module.Incremental},
-//			{ID: "unique_current_poll_ipv6", Name: "ipv6", Algo: module.Incremental},
-//		},
-//	}
+	requestsPerUserDefined = Chart{
+		ID:    "requests_per_user_defined",
+		Title: "Requests Per User Defined Pattern",
+		Units: "requests/s",
+		Fam:   "user defined",
+		Ctx:   "web_log.requests_per_user_defined",
+		Type:  module.Stacked,
+	}
+
+	requestsPerHTTPMethod = Chart{
+		ID:    "requests_per_http_method",
+		Title: "Requests Per HTTP Method",
+		Units: "requests/s",
+		Fam:   "http methods",
+		Ctx:   "web_log.requests_per_http_method",
+		Type:  module.Stacked,
+		Dims: Dims{
+			{ID: "GET", Algo: module.Incremental},
+		},
+	}
+	requestsPerHTTPVersion = Chart{
+		ID:    "requests_per_http_version",
+		Title: "Requests Per HTTP Version",
+		Units: "requests/s",
+		Fam:   "http versions",
+		Ctx:   "web_log.requests_per_http_version",
+		Type:  module.Stacked,
+	}
+
+	requestsPerIPProto = Chart{
+		ID:    "requests_per_ip_proto",
+		Title: "Requests Per IP Protocol",
+		Units: "requests/s",
+		Fam:   "ip protocols",
+		Ctx:   "web_log.requests_per_ip_proto",
+		Type:  module.Stacked,
+		Dims: Dims{
+			{ID: "req_ipv4", Name: "ipv4", Algo: module.Incremental},
+			{ID: "req_ipv6", Name: "ipv6", Algo: module.Incremental},
+		},
+	}
+	requestsPerVhost = Chart{
+		ID:    "requests_per_vhost",
+		Title: "Requests Per Vhost",
+		Units: "requests/s",
+		Fam:   "vhost",
+		Ctx:   "web_log.requests_per_vhost",
+		Type:  module.Stacked,
+	}
+
+	currentPollIPs = Chart{
+		ID:    "clients_current",
+		Title: "Current Poll Unique Client IPs",
+		Units: "unique ips",
+		Fam:   "clients",
+		Ctx:   "web_log.current_poll_ips",
+		Type:  module.Stacked,
+		Dims: Dims{
+			{ID: "unique_current_poll_ipv4", Name: "ipv4", Algo: module.Absolute},
+			{ID: "unique_current_poll_ipv6", Name: "ipv6", Algo: module.Absolute},
+		},
+	}
 )
 
 //
-//func responseCodesDetailedPerFamily() []*Chart {
-//	return []*Chart{
-//		{
-//			ID:    responseCodesDetailed.ID + "_1xx",
-//			Title: "Detailed Response Codes 1xx",
-//			Units: "requests/s",
-//			Fam:   "responses",
-//			Ctx:   "web_log.response_codes_detailed_1xx",
-//			Type:  module.Stacked,
-//		},
-//		{
-//			ID:    responseCodesDetailed.ID + "_2xx",
-//			Title: "Detailed Response Codes 2xx",
-//			Units: "requests/s",
-//			Fam:   "responses",
-//			Ctx:   "web_log.response_codes_detailed_2xx",
-//			Type:  module.Stacked,
-//		},
-//		{
-//			ID:    responseCodesDetailed.ID + "_3xx",
-//			Title: "Detailed Response Codes 3xx",
-//			Units: "requests/s",
-//			Fam:   "responses",
-//			Ctx:   "web_log.response_codes_detailed_3xx",
-//			Type:  module.Stacked,
-//		},
-//		{
-//			ID:    responseCodesDetailed.ID + "_4xx",
-//			Title: "Detailed Response Codes 4xx",
-//			Units: "requests/s",
-//			Fam:   "responses",
-//			Ctx:   "web_log.response_codes_detailed_4xx",
-//			Type:  module.Stacked,
-//		},
-//		{
-//			ID:    responseCodesDetailed.ID + "_5xx",
-//			Title: "Detailed Response Codes 5xx",
-//			Units: "requests/s",
-//			Fam:   "responses",
-//			Ctx:   "web_log.response_codes_detailed_5xx",
-//			Type:  module.Stacked,
-//		},
-//		{
-//			ID:    responseCodesDetailed.ID + "_other",
-//			Title: "Detailed Response Codes Other",
-//			Units: "requests/s",
-//			Fam:   "responses",
-//			Ctx:   "web_log.response_codes_detailed_other",
-//			Type:  module.Stacked,
-//		},
-//	}
-//}
-//
-//func perCategoryStats(id string) []*Chart {
-//	return []*Chart{
-//		{
-//			ID:    responseCodesDetailed.ID + "_" + id,
-//			Title: "Detailed Response Codes",
-//			Units: "requests/s",
-//			Fam:   "url " + id,
-//			Ctx:   "web_log.response_codes_detailed_per_url",
-//			Type:  module.Stacked,
-//		},
-//		{
-//			ID:    bandwidth.ID + "_" + id,
-//			Title: "Bandwidth",
-//			Units: "kilobits/s",
-//			Fam:   "url " + id,
-//			Ctx:   "web_log.bandwidth_per_url",
-//			Type:  module.Area,
-//			Dims: Dims{
-//				{ID: id + "_resp_length", Name: "received", Algo: module.Incremental, Mul: 8, Div: 1000},
-//				{ID: id + "_bytes_sent", Name: "sent", Algo: module.Incremental, Mul: -8, Div: 1000},
-//			},
-//		},
-//		{
-//			ID:    responseTime.ID + "_" + id,
-//			Title: "Processing Time",
-//			Units: "milliseconds",
-//			Fam:   "url " + id,
-//			Ctx:   "web_log.response_time_per_url",
-//			Type:  module.Area,
-//			Dims: Dims{
-//				{ID: id + "_resp_time_min", Name: "min", Algo: module.Incremental, Div: 1000},
-//				{ID: id + "_resp_time_max", Name: "max", Algo: module.Incremental, Div: 1000},
-//				{ID: id + "_resp_time_avg", Name: "avg", Algo: module.Incremental, Div: 1000},
-//			},
-//		},
-//	}
-//}
-//
-//func (w *WebLog) createCharts() {
-//	var charts module.Charts
-//
-//	_ = charts.Add(responseStatuses.Copy(), responseCodes.Copy())
-//
-//	if w.DoCodesAggregate {
-//		_ = charts.Add(responseCodesDetailed.Copy())
-//	} else {
-//		_ = charts.Add(responseCodesDetailedPerFamily()...)
-//	}
-//
-//	if w.gm.has(keyBytesSent) || w.gm.has(keyReqLength) {
-//		_ = charts.Add(bandwidth.Copy())
-//	}
-//
-//	if w.gm.has(keyRequest) && len(w.URLCategories) > 0 {
-//		chart := requestsPerURL.Copy()
-//		_ = charts.Add(chart)
-//		for _, cat := range w.worker.urlCats {
-//			_ = chart.AddDim(&Dim{
-//				ID:   cat.name,
-//				Algo: module.Incremental,
-//			})
-//		}
-//	}
-//
-//	if w.gm.has(keyRequest) && len(w.URLCategories) > 0 {
-//		for _, cat := range w.worker.urlCats {
-//			for _, chart := range perCategoryStats(cat.name) {
-//				_ = charts.Add(chart)
-//				for _, d := range chart.Dims {
-//					w.worker.metrics[d.ID] = 0
-//				}
-//			}
-//		}
-//	}
-//
-//	if w.gm.has(keyRequest) && len(w.UserCategories) > 0 {
-//		chart := requestsPerUserDefined.Copy()
-//		_ = charts.Add(chart)
-//
-//		for _, cat := range w.worker.userCats {
-//			_ = chart.AddDim(&Dim{
-//				ID:   cat.name,
-//				Algo: module.Incremental,
-//			})
-//		}
-//	}
-//
-//	if w.gm.has(keyRespTime) {
-//		_ = charts.Add(responseTime.Copy())
-//	}
-//
-//	if w.gm.has(keyRespTime) && len(w.Histogram) != 0 {
-//		chart := responseTimeHistogram.Copy()
-//		_ = charts.Add(chart)
-//		for _, v := range w.worker.histograms[keyRespTimeHistogram] {
-//			_ = chart.AddDim(&Dim{
-//				ID:   v.id,
-//				Name: v.name,
-//				Algo: module.Incremental,
-//			})
-//		}
-//	}
-//
-//	if w.gm.has(keyRespTimeUpstream) {
-//		_ = charts.Add(responseTimeUpstream.Copy())
-//	}
-//
-//	if w.gm.has(keyRespTimeUpstream) && len(w.Histogram) != 0 {
-//		chart := responseTimeUpstreamHistogram.Copy()
-//		_ = charts.Add(chart)
-//		for _, v := range w.worker.histograms[keyRespTimeUpstreamHistogram] {
-//			_ = chart.AddDim(&Dim{
-//				ID:   v.id,
-//				Name: v.name,
-//				Algo: module.Incremental,
-//			})
-//		}
-//	}
-//
-//	if w.gm.has(keyRequest) {
-//		_ = charts.Add(requestsPerHTTPMethod.Copy())
-//		_ = charts.Add(requestsPerHTTPVersion.Copy())
-//	}
-//
-//	if w.gm.has(keyVhost) {
-//		_ = charts.Add(requestsPerVhost.Copy())
-//	}
-//
-//	if w.gm.has(keyAddress) {
-//		_ = charts.Add(requestsPerIPProto.Copy())
-//		_ = charts.Add(currentPollIPs.Copy())
-//		if w.DoAllTimeIPs {
-//			_ = charts.Add(allTimeIPs.Copy())
-//		}
-//	}
-//
-//	w.charts = &charts
-//}
-//
-//func (w *WebLog) Charts() *Charts {
-//	return w.charts
-//}
+func responseCodesDetailedPerFamily() []*Chart {
+	return []*Chart{
+		{
+			ID:    responseCodesDetailed.ID + "_1xx",
+			Title: "Detailed Response Codes 1xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_1xx",
+			Type:  module.Stacked,
+		},
+		{
+			ID:    responseCodesDetailed.ID + "_2xx",
+			Title: "Detailed Response Codes 2xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_2xx",
+			Type:  module.Stacked,
+		},
+		{
+			ID:    responseCodesDetailed.ID + "_3xx",
+			Title: "Detailed Response Codes 3xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_3xx",
+			Type:  module.Stacked,
+		},
+		{
+			ID:    responseCodesDetailed.ID + "_4xx",
+			Title: "Detailed Response Codes 4xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_4xx",
+			Type:  module.Stacked,
+		},
+		{
+			ID:    responseCodesDetailed.ID + "_5xx",
+			Title: "Detailed Response Codes 5xx",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_5xx",
+			Type:  module.Stacked,
+		},
+		{
+			ID:    responseCodesDetailed.ID + "_other",
+			Title: "Detailed Response Codes Other",
+			Units: "requests/s",
+			Fam:   "responses",
+			Ctx:   "web_log.response_codes_detailed_other",
+			Type:  module.Stacked,
+		},
+	}
+}
+
+func perCategoryStats(id string) []*Chart {
+	return []*Chart{
+		{
+			ID:    responseCodesDetailed.ID + "_" + id,
+			Title: "Detailed Response Codes",
+			Units: "requests/s",
+			Fam:   "url " + id,
+			Ctx:   "web_log.response_codes_detailed_per_url",
+			Type:  module.Stacked,
+		},
+		{
+			ID:    bandwidth.ID + "_" + id,
+			Title: "Bandwidth",
+			Units: "kilobits/s",
+			Fam:   "url " + id,
+			Ctx:   "web_log.bandwidth_per_url",
+			Type:  module.Area,
+			Dims: Dims{
+				{ID: id + "_resp_length", Name: "received", Algo: module.Incremental, Mul: 8, Div: 1000},
+				{ID: id + "_bytes_sent", Name: "sent", Algo: module.Incremental, Mul: -8, Div: 1000},
+			},
+		},
+		{
+			ID:    responseTime.ID + "_" + id,
+			Title: "Processing Time",
+			Units: "milliseconds",
+			Fam:   "url " + id,
+			Ctx:   "web_log.response_time_per_url",
+			Type:  module.Area,
+			Dims: Dims{
+				{ID: id + "_resp_time_min", Name: "min", Algo: module.Incremental, Div: 1000},
+				{ID: id + "_resp_time_max", Name: "max", Algo: module.Incremental, Div: 1000},
+				{ID: id + "_resp_time_avg", Name: "avg", Algo: module.Incremental, Div: 1000},
+			},
+		},
+	}
+}
--- modules/weblog/weblog.go
+++ modules/weblog/weblog.go
@@ -4,6 +4,10 @@ import (
 	"bytes"
 	"encoding/csv"
 	"fmt"
+	"io"
+	"strconv"
+
+	"github.com/netdata/go.d.plugin/pkg/stm"
 
 	"github.com/netdata/go.d.plugin/pkg/simpletail"
 
@@ -60,10 +64,6 @@ type (
 	}
 )
 
-func (w *WebLog) Charts() *module.Charts {
-	panic("implement me")
-}
-
 func (w *WebLog) Init() bool {
 	if err := w.initFilter(); err != nil {
 		w.Error(err)
@@ -111,12 +111,187 @@ func (w *WebLog) Check() bool {
 			return false
 		}
 	}
+	return true
+}
 
-	panic("TODO")
+func (w *WebLog) Charts() *module.Charts {
+	var charts module.Charts
+	_ = charts.Add(responseStatuses.Copy(), responseCodes.Copy())
+	if w.DetailedStatus {
+		_ = charts.Add(responseCodesDetailedPerFamily()...)
+
+	} else {
+		_ = charts.Add(responseCodesDetailed.Copy())
+	}
+	if w.format.BytesSent >= 0 || w.format.ReqLength >= 0 {
+		_ = charts.Add(bandwidth.Copy())
+	}
+
+	if w.format.Request >= 0 {
+		_ = charts.Add(requestsPerHTTPMethod.Copy())
+		_ = charts.Add(requestsPerHTTPVersion.Copy())
+
+		if len(w.urlCategories) > 0 {
+			chart := requestsPerURL.Copy()
+			for _, category := range w.urlCategories {
+				_ = chart.AddDim(&Dim{
+					ID:   category.name, //TODO: fix name
+					Algo: module.Incremental,
+				})
+				for _, catChart := range perCategoryStats(category.name) {
+					_ = charts.Add(catChart)
+				}
+			}
+			_ = charts.Add(chart)
+		}
+	}
+
+	if w.format.Custom >= 0 && len(w.userCategories) > 0 {
+		chart := requestsPerUserDefined.Copy()
+		for _, category := range w.userCategories {
+			_ = chart.AddDim(&Dim{
+				ID:   category.name,
+				Algo: module.Incremental,
+			})
+		}
+		_ = charts.Add(chart)
+	}
+
+	if w.format.ReqTime >= 0 {
+		_ = charts.Add(responseTime.Copy())
+		if len(w.Histogram) > 0 {
+			chart := responseTimeHistogram.Copy()
+			_ = charts.Add(chart)
+			for _, v := range w.Histogram {
+				name := fmt.Sprintf("%f", v)
+				_ = chart.AddDim(&Dim{
+					ID:   name, //FIXME
+					Name: name,
+					Algo: module.Incremental,
+				})
+			}
+		}
+	}
+
+	if w.format.UpstreamRespTime >= 0 {
+		_ = charts.Add(responseTimeUpstream.Copy())
+		if len(w.Histogram) > 0 {
+			chart := responseTimeUpstreamHistogram.Copy()
+			_ = charts.Add(chart)
+			for _, v := range w.Histogram {
+				name := fmt.Sprintf("%f", v)
+				_ = chart.AddDim(&Dim{
+					ID:   name, //FIXME
+					Name: name,
+					Algo: module.Incremental,
+				})
+			}
+		}
+	}
+
+	if w.format.Host >= 0 {
+		_ = charts.Add(requestsPerVhost.Copy())
+	}
+
+	if w.format.RemoteAddr >= 0 {
+		_ = charts.Add(requestsPerIPProto.Copy())
+		_ = charts.Add(currentPollIPs.Copy())
+	}
+
+	return &charts
 }
 
 func (w *WebLog) Collect() map[string]int64 {
-	panic("TODO")
+	w.metrics.Reset()
+
+	for {
+		fields, err := w.parser.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			w.Logger.Errorf("collect error: %v", err)
+			return nil
+		}
+		line, err := w.format.Parse(fields)
+		if err != nil {
+			w.Logger.Errorf("parse error: %v", err)
+			return nil
+		}
+
+		w.metrics.Requests.Inc()
+		if line.Version != "" {
+			w.metrics.ReqVersion.Get(line.Version).Inc()
+		}
+		if line.Status > 0 {
+			status := line.Status
+			switch {
+			case status >= 100 && status < 300, status == 304:
+				w.metrics.RespSuccessful.Inc()
+			case status >= 300 && status < 400:
+				w.metrics.RespRedirect.Inc()
+			case status >= 400 && status < 500:
+				w.metrics.RespClientError.Inc()
+			case status >= 500 && status < 600:
+				w.metrics.RespServerError.Inc()
+			}
+
+			switch status / 100 {
+			case 1:
+				w.metrics.Resp1xx.Inc()
+			case 2:
+				w.metrics.Resp2xx.Inc()
+			case 3:
+				w.metrics.Resp3xx.Inc()
+			case 4:
+				w.metrics.Resp4xx.Inc()
+			case 5:
+				w.metrics.Resp5xx.Inc()
+			}
+
+			if w.DetailedStatus {
+				w.metrics.RespCode.Get(strconv.Itoa(status)).Inc()
+			}
+		}
+		if line.Method != "" {
+			w.metrics.ReqMethod.Get(line.Method).Inc()
+		}
+
+		if line.BytesSent > 0 {
+			w.metrics.BytesSent.Add(float64(line.BytesSent))
+		}
+		if line.ReqLength > 0 {
+			w.metrics.BytesReceived.Add(float64(line.ReqLength))
+		}
+
+		if line.ReqTime >= 0 {
+			w.metrics.RespTime.Observe(line.ReqTime)
+			if w.metrics.RespTimeHist != nil {
+				w.metrics.RespTimeHist.Observe(line.ReqTime)
+			}
+		}
+		if line.UpstreamRespTime != nil {
+			for _, time := range line.UpstreamRespTime {
+				w.metrics.RespTimeUpstream.Observe(time)
+				if w.metrics.RespTimeUpstreamHist != nil {
+					w.metrics.RespTimeUpstreamHist.Observe(line.ReqTime)
+				}
+			}
+		}
+
+		if line.RemoteAddr != "" {
+			w.metrics.UniqueIPs.Insert(line.RemoteAddr)
+		}
+		for _, cat := range w.urlCategories {
+			if cat.Matcher.MatchString(line.URI) {
+				// TODO add metrics
+				break
+			}
+		}
+		// TODO add user defined
+	}
+
+	return stm.ToMap(w.metrics)
 }
 
 func (w *WebLog) Cleanup() {
