cc351754f20d207cf7ab0fc019cc7cffec55987d
lgz <ilyamaschenko@gmail.com>
2019-03-21 22:43:27 +0300

    wip

--- modules/k8s_kubeproxy/k8s_kubeproxy.go
+++ modules/k8s_kubeproxy/k8s_kubeproxy.go
@@ -1,7 +1,6 @@
 package k8s_kubeproxy
 
 import (
-	"strings"
 	"time"
 
 	mtx "github.com/netdata/go.d.plugin/pkg/metrics"
@@ -41,9 +40,6 @@ func New() *KubeProxy {
 
 func newMetrics() *metrics {
 	return &metrics{
-		SyncProxyRules: syncProxyRulesMetrics{
-			Latency: make(map[string]mtx.Gauge),
-		},
 		RestClientMetrics: restClientMetrics{
 			HTTPRequestsByStatusCode: make(map[string]mtx.Gauge),
 			HTTPRequestsByMethod:     make(map[string]mtx.Gauge),
@@ -52,13 +48,28 @@ func newMetrics() *metrics {
 }
 
 type metrics struct {
-	SyncProxyRules    syncProxyRulesMetrics `stm:"sync_proxy_rules"`
-	RestClientMetrics restClientMetrics     `stm:"rest_client"`
-}
-
-type syncProxyRulesMetrics struct {
-	Count   mtx.Gauge            `stm:"count"`
-	Latency map[string]mtx.Gauge `stm:"bucket"`
+	SyncProxyRules struct {
+		Count   mtx.Gauge `stm:"count"`
+		Latency struct {
+			LE1000     mtx.Gauge `stm:"1000"`
+			LE2000     mtx.Gauge `stm:"2000"`
+			LE4000     mtx.Gauge `stm:"4000"`
+			LE8000     mtx.Gauge `stm:"8000"`
+			LE16000    mtx.Gauge `stm:"16000"`
+			LE32000    mtx.Gauge `stm:"32000"`
+			LE64000    mtx.Gauge `stm:"64000"`
+			LE128000   mtx.Gauge `stm:"128000"`
+			LE256000   mtx.Gauge `stm:"256000"`
+			LE512000   mtx.Gauge `stm:"512000"`
+			LE1024000  mtx.Gauge `stm:"1024000"`
+			LE2048000  mtx.Gauge `stm:"2048000"`
+			LE4096000  mtx.Gauge `stm:"4096000"`
+			LE8192000  mtx.Gauge `stm:"8192000"`
+			LE16384000 mtx.Gauge `stm:"16384000"`
+			Inf        mtx.Gauge `stm:"+Inf"`
+		} `stm:"bucket"`
+	} `stm:"sync_proxy_rules"`
+	RestClientMetrics restClientMetrics `stm:"rest_client"`
 }
 
 type restClientMetrics struct {
@@ -133,13 +144,41 @@ func (kp *KubeProxy) collectSyncProxyRuleLatency(raw prometheus.Metrics) {
 	metricName := "kubeproxy_sync_proxy_rules_latency_microseconds_bucket"
 
 	for _, metric := range raw.FindByName(metricName) {
-		val := metric.Labels.Get("le")
-		if val == "" {
-			continue
+		value := metric.Labels.Get("le")
+		switch value {
+		case "1000":
+			kp.mx.SyncProxyRules.Latency.LE1000.Set(metric.Value)
+		case "2000":
+			kp.mx.SyncProxyRules.Latency.LE2000.Set(metric.Value)
+		case "4000":
+			kp.mx.SyncProxyRules.Latency.LE4000.Set(metric.Value)
+		case "8000":
+			kp.mx.SyncProxyRules.Latency.LE8000.Set(metric.Value)
+		case "16000":
+			kp.mx.SyncProxyRules.Latency.LE16000.Set(metric.Value)
+		case "32000":
+			kp.mx.SyncProxyRules.Latency.LE32000.Set(metric.Value)
+		case "64000":
+			kp.mx.SyncProxyRules.Latency.LE64000.Set(metric.Value)
+		case "128000":
+			kp.mx.SyncProxyRules.Latency.LE128000.Set(metric.Value)
+		case "256000":
+			kp.mx.SyncProxyRules.Latency.LE256000.Set(metric.Value)
+		case "512000":
+			kp.mx.SyncProxyRules.Latency.LE512000.Set(metric.Value)
+		case "1.024e+06":
+			kp.mx.SyncProxyRules.Latency.LE1024000.Set(metric.Value)
+		case "2.048e+06":
+			kp.mx.SyncProxyRules.Latency.LE2048000.Set(metric.Value)
+		case "4.096e+06":
+			kp.mx.SyncProxyRules.Latency.LE4096000.Set(metric.Value)
+		case "8.192e+06":
+			kp.mx.SyncProxyRules.Latency.LE8192000.Set(metric.Value)
+		case "1.6384e+07":
+			kp.mx.SyncProxyRules.Latency.LE16384000.Set(metric.Value)
+		case "+Inf":
+			kp.mx.SyncProxyRules.Latency.Inf.Set(metric.Value)
 		}
-		// TODO: FIX
-		newVal := strings.Replace(val, ".", "_", -1)
-		kp.mx.SyncProxyRules.Latency[newVal] = mtx.Gauge(metric.Value)
 	}
 }
 
