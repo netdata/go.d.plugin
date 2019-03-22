d721231be491b4b4e03792f3863262c0dd67bf58
Ilya Mashchenko <ilyamaschenko@gmail.com>
2019-03-22 19:18:10 +0300

    docker_engine add few charts (#177)

--- modules/docker_engine/docker_engine_test.go
+++ modules/docker_engine/docker_engine_test.go
@@ -53,7 +53,7 @@ func TestDockerEngine_CheckNG(t *testing.T) {
 	assert.False(t, job.Check())
 }
 
-func TestExample_Collect(t *testing.T) {
+func TestDockerEngine_Collect(t *testing.T) {
 	ts := httptest.NewServer(
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
@@ -67,14 +67,23 @@ func TestExample_Collect(t *testing.T) {
 	require.True(t, job.Check())
 
 	expected := map[string]int64{
-		"actions_changes": 1,
-		"actions_commit":  1,
-		"actions_create":  1,
-		"actions_delete":  1,
-		"actions_start":   1,
-		"states_paused":   11,
-		"states_running":  12,
-		"states_stopped":  13,
+		"container_actions_changes":                      1,
+		"container_actions_start":                        1,
+		"container_actions_commit":                       1,
+		"container_actions_delete":                       1,
+		"container_actions_create":                       1,
+		"container_states_paused":                        11,
+		"container_states_running":                       12,
+		"container_states_stopped":                       13,
+		"builder_fails_dockerfile_empty_error":           4,
+		"builder_fails_dockerfile_syntax_error":          5,
+		"builder_fails_error_processing_commands_error":  6,
+		"builder_fails_build_canceled":                   1,
+		"builder_fails_build_target_not_reachable_error": 2,
+		"builder_fails_command_not_supported_error":      3,
+		"builder_fails_missing_onbuild_arguments_error":  7,
+		"builder_fails_unknown_instruction_error":        8,
+		"health_checks_failed":                           33,
 	}
 
 	assert.Equal(t, expected, job.Collect())
