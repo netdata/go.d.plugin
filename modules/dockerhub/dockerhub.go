eb8e37df35920dc253d6bc5382e89febb4196429
Ilya Mashchenko <ilyamaschenko@gmail.com>
2019-03-29 23:45:58 +0300

    Update dockerhub.go

--- modules/dockerhub/dockerhub.go
+++ modules/dockerhub/dockerhub.go
@@ -38,14 +38,14 @@ func New() *DockerHub {
 
 // Config is the DockerHub module configuration.
 type Config struct {
-	web.HTTP `yaml:",inline"`
+	web.HTTP     `yaml:",inline"`
 	Repositories []string
 }
 
 // DockerHub DockerHub module.
 type DockerHub struct {
 	module.Base
-	Config       `yaml:",inline"`
+	Config `yaml:",inline"`
 	client *apiClient
 }
 
