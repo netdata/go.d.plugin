dc9c057950b5242db080ed815760e776c4a5b91e
lgz <ilyamaschenko@gmail.com>
2019-04-21 02:05:37 +0300

    add tengine collector

---
+++ modules/tengine/README.md
@@ -0,0 +1,54 @@
+# tengine
+
+This module will monitor one or more tengine instances via the `ngx_http_reqstat_module` module.
+
+**Requirements:**
+ * tengine with configured [`ngx_http_reqstat_module`](http://tengine.taobao.org/document/http_reqstat.html)
+ * collector expects [default line format](http://tengine.taobao.org/document/http_reqstat.html)
+
+It produces the following charts:
+
+<br>
+Summary:
+
+1. **Bandwidth** in B/s
+
+2. **Connections** in connections/s
+
+3. **Requests** in requests/s
+
+4. **Requests Per Response Code Family** in requests/s
+  * 2xx, 3xx, 4xx, 5xx, other
+  
+5. **Requests Per Response Code Detailed** in requests/s
+  * 200, 206, 302, 304, 403, ..., other
+  
+6. **Number Of Requests Calling For Upstream** in requests/s
+
+7. **Number Of Times Calling For Upstream** in calls/s
+
+7. **Requests Per Response Code Family** in requests/s
+  * 4xx, 5xx
+
+
+
+### configuration
+
+Needs only `url`.
+
+Here is an example:
+
+```yaml
+jobs:
+  - name: local
+    url : http://127.0.0.1/us
+      
+  - name: remote
+    url : http://100.64.0.1us
+```
+
+For all available options please see module [configuration file](https://github.com/netdata/go.d.plugin/blob/master/config/go.d/tengine.conf).
+
+Without configuration, module attempts to connect to `http://127.0.0.1/us`
+
+---
---
+++ modules/tengine/tengine_test.go
@@ -0,0 +1,133 @@
+package tengine
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/netdata/go-orchestrator/module"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	testStatusData, _ = ioutil.ReadFile("testdata/status.txt")
+)
+
+func TestTengine_Cleanup(t *testing.T) { New().Cleanup() }
+
+func TestNew(t *testing.T) {
+	job := New()
+
+	assert.Implements(t, (*module.Module)(nil), job)
+	assert.Equal(t, defaultURL, job.URL)
+	assert.Equal(t, defaultHTTPTimeout, job.Timeout.Duration)
+}
+
+func TestTengine_Init(t *testing.T) {
+	job := New()
+
+	require.True(t, job.Init())
+	assert.NotNil(t, job.apiClient)
+}
+
+func TestTengine_Check(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(testStatusData)
+			}))
+	defer ts.Close()
+
+	job := New()
+	job.URL = ts.URL
+	require.True(t, job.Init())
+	assert.True(t, job.Check())
+}
+
+func TestTengine_CheckNG(t *testing.T) {
+	job := New()
+
+	job.URL = "http://127.0.0.1:38001/us"
+	require.True(t, job.Init())
+	assert.False(t, job.Check())
+}
+
+func TestTengine_Charts(t *testing.T) { assert.NotNil(t, New().Charts()) }
+
+func TestTengine_Collect(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(testStatusData)
+			}))
+	defer ts.Close()
+
+	job := New()
+	job.URL = ts.URL
+	require.True(t, job.Init())
+	require.True(t, job.Check())
+
+	expected := map[string]int64{
+		"bytes_in":                 5944,
+		"bytes_out":                20483,
+		"conn_total":               64,
+		"http_200":                 65,
+		"http_206":                 0,
+		"http_2xx":                 65,
+		"http_302":                 0,
+		"http_304":                 0,
+		"http_3xx":                 0,
+		"http_403":                 0,
+		"http_404":                 0,
+		"http_416":                 0,
+		"http_499":                 0,
+		"http_4xx":                 0,
+		"http_500":                 0,
+		"http_502":                 0,
+		"http_503":                 0,
+		"http_504":                 0,
+		"http_508":                 0,
+		"http_5xx":                 0,
+		"http_other_detail_status": 0,
+		"http_other_status":        0,
+		"http_ups_4xx":             0,
+		"http_ups_5xx":             0,
+		"req_total":                65,
+		"rt":                       0,
+		"ups_req":                  0,
+		"ups_rt":                   0,
+		"ups_tries":                0,
+	}
+
+	assert.Equal(t, expected, job.Collect())
+}
+
+func TestTengine_InvalidData(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write([]byte("hello and goodbye"))
+			}))
+	defer ts.Close()
+
+	job := New()
+	job.URL = ts.URL
+	require.True(t, job.Init())
+	assert.False(t, job.Check())
+}
+
+func TestTengine_404(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusNotFound)
+			}))
+	defer ts.Close()
+
+	job := New()
+	job.URL = ts.URL
+	require.True(t, job.Init())
+	assert.False(t, job.Check())
+}
