cf11917fec51795a532a18f67d8242e575d08bd4
lgz <ilyamaschenko@gmail.com>
2019-04-26 17:12:47 +0300

    docker_engine fixes

--- modules/docker_engine/docker_engine_test.go
+++ modules/docker_engine/docker_engine_test.go
@@ -16,7 +16,7 @@ func TestNew(t *testing.T) {
 	job := New()
 
 	assert.IsType(t, (*DockerEngine)(nil), job)
-	assert.Equal(t, defaultURL, job.URL)
+	assert.Equal(t, defaultURL, job.UserURL)
 	assert.Equal(t, defaultHTTPTimeout, job.Timeout.Duration)
 }
 
@@ -28,7 +28,7 @@ func TestDockerEngine_Init(t *testing.T) { assert.True(t, New().Init()) }
 
 func TestDockerEngine_InitNG(t *testing.T) {
 	job := New()
-	job.URL = ""
+	job.UserURL = ""
 	assert.False(t, job.Init())
 }
 
@@ -41,14 +41,14 @@ func TestDockerEngine_Check(t *testing.T) {
 	defer ts.Close()
 
 	job := New()
-	job.URL = ts.URL + "/metrics"
+	job.UserURL = ts.URL + "/metrics"
 	require.True(t, job.Init())
 	assert.True(t, job.Check())
 }
 
 func TestDockerEngine_CheckNG(t *testing.T) {
 	job := New()
-	job.URL = "http://127.0.0.1:38001/metrics"
+	job.UserURL = "http://127.0.0.1:38001/metrics"
 	require.True(t, job.Init())
 	assert.False(t, job.Check())
 }
@@ -62,7 +62,7 @@ func TestDockerEngine_Collect(t *testing.T) {
 	defer ts.Close()
 
 	job := New()
-	job.URL = ts.URL + "/metrics"
+	job.UserURL = ts.URL + "/metrics"
 	require.True(t, job.Init())
 	require.True(t, job.Check())
 
@@ -98,7 +98,7 @@ func TestDockerEngine_InvalidData(t *testing.T) {
 	defer ts.Close()
 
 	job := New()
-	job.URL = ts.URL + "/metrics"
+	job.UserURL = ts.URL + "/metrics"
 	require.True(t, job.Init())
 	assert.False(t, job.Check())
 }
@@ -112,7 +112,7 @@ func TestDockerEngine_404(t *testing.T) {
 	defer ts.Close()
 
 	job := New()
-	job.URL = ts.URL + "/metrics"
+	job.UserURL = ts.URL + "/metrics"
 	require.True(t, job.Init())
 	assert.False(t, job.Check())
 }
