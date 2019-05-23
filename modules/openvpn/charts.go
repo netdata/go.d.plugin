19f748b6c20c14a0f01e48d8816610d44349bdf5
Ilya Mashchenko <ilyamaschenko@gmail.com>
2019-05-23 17:40:33 +0900

    openvpn bw charts fix and readme update (#223)
    * change bw charts units to kilobits/s
    
    * update plugin readme

--- modules/openvpn/charts.go
+++ modules/openvpn/charts.go
@@ -23,13 +23,13 @@ var charts = Charts{
 	{
 		ID:    "total_traffic",
 		Title: "Total Traffic",
-		Units: "KiB/s",
+		Units: "kilobits/s",
 		Fam:   "traffic",
 		Ctx:   "openvpn.total_traffic",
 		Type:  module.Area,
 		Dims: Dims{
-			{ID: "bytes_in", Name: "in", Algo: module.Incremental, Div: 1000},
-			{ID: "bytes_out", Name: "out", Algo: module.Incremental, Div: -1000},
+			{ID: "bytes_in", Name: "in", Algo: module.Incremental, Mul: 8, Div: 1000},
+			{ID: "bytes_out", Name: "out", Algo: module.Incremental, Mul: 8, Div: -1000},
 		},
 	},
 }
@@ -38,13 +38,13 @@ var userCharts = Charts{
 	{
 		ID:    "%s_user_traffic",
 		Title: "User Traffic",
-		Units: "KiB/s",
+		Units: "kilobits/s",
 		Fam:   "user %s",
 		Ctx:   "openvpn.user_traffic",
 		Type:  module.Area,
 		Dims: Dims{
-			{ID: "%s_bytes_received", Name: "received", Algo: module.Incremental, Div: 1000},
-			{ID: "%s_bytes_sent", Name: "sent", Algo: module.Incremental, Div: -1000},
+			{ID: "%s_bytes_received", Name: "received", Algo: module.Incremental, Mul: 8, Div: 1000},
+			{ID: "%s_bytes_sent", Name: "sent", Algo: module.Incremental, Mul: 8, Div: -1000},
 		},
 	},
 	{
