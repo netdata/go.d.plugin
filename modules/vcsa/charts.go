b6c910082530a42146f32e0969428c90dbd6b4a0
Ilya Mashchenko <ilya@netdata.cloud>
2019-09-03 19:50:40 +0300

    vCenter Server Appliance module (#260)
    * add vcsa health monitoring module

---
+++ modules/vcsa/charts.go
@@ -0,0 +1,30 @@
+package vcsa
+
+import "github.com/netdata/go-orchestrator/module"
+
+type (
+	// Charts is an alias for module.Charts
+	Charts = module.Charts
+	// Dims is an alias for module.Dims
+	Dims = module.Dims
+)
+
+var charts = Charts{
+	{
+		ID:    "health",
+		Title: "Health Status",
+		Units: "status",
+		Fam:   "health",
+		Ctx:   "vcsa.health",
+		Dims: Dims{
+			{ID: "appl_mgmt"},
+			{ID: "database_storage"},
+			{ID: "load"},
+			{ID: "mem"},
+			{ID: "software_packages"},
+			{ID: "storage"},
+			{ID: "swap"},
+			{ID: "system"},
+		},
+	},
+}
