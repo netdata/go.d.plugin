63c6e12e159dddcb0aae4953a9439a9ccfab6c0e
ilyam8 <ilya@netdata.cloud>
2019-09-18 17:40:00 +0300

    FSNamesystem metrics

--- modules/hdfs/collect.go
+++ modules/hdfs/collect.go
@@ -8,12 +8,45 @@ import (
 	"github.com/netdata/go.d.plugin/pkg/stm"
 )
 
+func isJvm(data rawData) bool {
+	v, ok := data["modelerType"]
+	return ok && bytes.Equal(v, []byte("\"JvmMetrics\""))
+
+}
+
+func isFsn(data rawData) bool {
+	v, ok := data["modelerType"]
+	return ok && bytes.Equal(v, []byte("\"FSNamesystem\""))
+
+}
+
 type (
 	rawData map[string]json.RawMessage
 	rawJMX  struct {
 		Beans []rawData
 	}
 )
+
+func (r rawJMX) isEmpty() bool {
+	return len(r.Beans) == 0
+}
+
+func (r rawJMX) find(f func(rawData) bool) rawData {
+	for _, v := range r.Beans {
+		if f(v) {
+			return v
+		}
+	}
+	return nil
+}
+
+func (r rawJMX) findJvm() rawData {
+	return r.find(isJvm)
+}
+
+func (r rawJMX) findFsn() rawData {
+	return r.find(isFsn)
+}
 
 func (h *HDFS) collect() (map[string]int64, error) {
 	var raw rawJMX
@@ -22,48 +55,64 @@ func (h *HDFS) collect() (map[string]int64, error) {
 		return nil, err
 	}
 
+	if raw.isEmpty() {
+		return nil, errors.New("empty response")
+	}
+
 	var mx metrics
 	err = h.collectJVM(&mx, raw)
 	if err != nil {
-		return nil, err
+		h.Errorf("error on collecting jvm : %v", err)
+	}
+
+	// Note: only name nodes contains 'fns', it would be better to identify node type on start
+	// and use appropriate collect method after
+	err = h.collectFns(&mx, raw)
+	if err != nil {
+		h.Errorf("error on collecting fns : %v", err)
 	}
 
 	return stm.ToMap(mx), nil
 }
 
 func (h HDFS) collectJVM(mx *metrics, raw rawJMX) error {
-	rawJvm := findJvm(raw)
+	rawJvm := raw.findJvm()
 	if rawJvm == nil {
-		return errors.New("")
+		return errors.New("couldn't find jvm data")
 	}
 
 	b, err := json.Marshal(rawJvm)
 	if err != nil {
-		return errors.New("")
+		return err
 	}
 
 	var jvm jvmMetrics
 	err = json.Unmarshal(b, &jvm)
 	if err != nil {
-		return errors.New("")
+		return err
 	}
 
 	mx.jvmMetrics = &jvm
 	return nil
 }
 
-func isJvm(data rawData) bool {
-	v, ok := data["modelerType"]
-	return ok && bytes.Equal(v, []byte("\"JvmMetrics\""))
-
-}
-
-func findJvm(raw rawJMX) (jvm rawData) {
-	for _, v := range raw.Beans {
-		if !isJvm(v) {
-			continue
-		}
-		return v
+func (h HDFS) collectFns(mx *metrics, raw rawJMX) error {
+	rawFns := raw.findFsn()
+	if rawFns == nil {
+		return nil
 	}
+
+	b, err := json.Marshal(rawFns)
+	if err != nil {
+		return err
+	}
+
+	var fns fsnNameSystemMetrics
+	err = json.Unmarshal(b, &fns)
+	if err != nil {
+		return err
+	}
+
+	mx.fsnNameSystemMetrics = &fns
 	return nil
 }
--- modules/hdfs/hdfs_test.go
+++ modules/hdfs/hdfs_test.go
@@ -14,10 +14,12 @@ import (
 
 var (
 	testDataNodeData, _ = ioutil.ReadFile("testdata/datanode.json")
+	testNameNodeData, _ = ioutil.ReadFile("testdata/namenode.json")
 )
 
 func Test_readTestData(t *testing.T) {
 	assert.NotNil(t, testDataNodeData)
+	assert.NotNil(t, testNameNodeData)
 }
 
 func TestNew(t *testing.T) {
@@ -68,7 +70,7 @@ func TestHDFS_Cleanup(t *testing.T) {
 	New().Cleanup()
 }
 
-func TestHDFS_Collect(t *testing.T) {
+func TestHDFS_CollectDataNode(t *testing.T) {
 	ts := httptest.NewServer(
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
@@ -104,6 +106,112 @@ func TestHDFS_Collect(t *testing.T) {
 		"jvm_threads_terminated":             0,
 		"jvm_threads_timed_waiting":          25,
 		"jvm_threads_waiting":                11,
+	}
+
+	assert.Equal(t, expected, job.Collect())
+}
+
+func TestHDFS_CollectNameNode(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(testNameNodeData)
+			}))
+	defer ts.Close()
+
+	job := New()
+	job.UserURL = ts.URL
+	require.True(t, job.Init())
+	require.True(t, job.Check())
+
+	expected := map[string]int64{
+		"fsn_block_capacity":                                    2097152,
+		"fsn_blocks_total":                                      15,
+		"fsn_bytes_in_future_ec_block_groups":                   0,
+		"fsn_bytes_in_future_replicated_blocks":                 0,
+		"fsn_capacity_remaining":                                65861697536,
+		"fsn_capacity_remaining_gb":                             61,
+		"fsn_capacity_total":                                    107351072768,
+		"fsn_capacity_total_gb":                                 100,
+		"fsn_capacity_used":                                     2372116480,
+		"fsn_capacity_used_gb":                                  2,
+		"fsn_capacity_used_non_dfs":                             39117258752,
+		"fsn_corrupt_blocks":                                    0,
+		"fsn_corrupt_ec_block_groups":                           0,
+		"fsn_corrupt_replicated_blocks":                         0,
+		"fsn_estimated_capacity_lost_total":                     0,
+		"fsn_excess_blocks":                                     0,
+		"fsn_expired_heartbeats":                                0,
+		"fsn_files_total":                                       12,
+		"fsn_highest_priority_low_redundancy_ec_blocks":         0,
+		"fsn_highest_priority_low_redundancy_replicated_blocks": 0,
+		"fsn_last_checkpoint_time":                              1566814983890,
+		"fsn_last_written_transaction_id":                       624,
+		"fsn_lock_queue_length":                                 0,
+		"fsn_low_redundancy_blocks":                             0,
+		"fsn_low_redundancy_ec_block_groups":                    0,
+		"fsn_low_redundancy_replicated_blocks":                  0,
+		"fsn_millis_since_last_loaded_edits":                    0,
+		"fsn_missing_blocks":                                    0,
+		"fsn_missing_ec_block_groups":                           0,
+		"fsn_missing_repl_one_blocks":                           0,
+		"fsn_missing_replicated_blocks":                         0,
+		"fsn_missing_replication_one_blocks":                    0,
+		"fsn_num_active_clients":                                0,
+		"fsn_num_dead_data_nodes":                               0,
+		"fsn_num_decom_dead_data_nodes":                         0,
+		"fsn_num_decom_live_data_nodes":                         0,
+		"fsn_num_decommissioning_data_nodes":                    0,
+		"fsn_num_encryption_zones":                              0,
+		"fsn_num_entering_maintenance_data_nodes":               0,
+		"fsn_num_files_under_construction":                      0,
+		"fsn_num_in_maintenance_dead_data_nodes":                0,
+		"fsn_num_in_maintenance_live_data_nodes":                0,
+		"fsn_num_live_data_nodes":                               2,
+		"fsn_num_stale_storages":                                0,
+		"fsn_num_timed_out_pending_reconstructions":             0,
+		"fsn_pending_data_node_message_count":                   0,
+		"fsn_pending_deletion_blocks":                           0,
+		"fsn_pending_deletion_ec_blocks":                        0,
+		"fsn_pending_deletion_replicated_blocks":                0,
+		"fsn_pending_reconstruction_blocks":                     0,
+		"fsn_pending_replication_blocks":                        0,
+		"fsn_postponed_misreplicated_blocks":                    0,
+		"fsn_provided_capacity_total":                           0,
+		"fsn_scheduled_replication_blocks":                      0,
+		"fsn_snapshots":                                         0,
+		"fsn_snapshottable_directories":                         0,
+		"fsn_stale_data_nodes":                                  0,
+		"fsn_total_ec_block_groups":                             0,
+		"fsn_total_load":                                        2,
+		"fsn_total_replicated_blocks":                           15,
+		"fsn_total_sync_count":                                  2,
+		"fsn_transactions_since_last_checkpoint":                1,
+		"fsn_transactions_since_last_log_roll":                  1,
+		"fsn_under_replicated_blocks":                           0,
+		"fsn_volume_failures_total":                             0,
+		"jvm_gc_count":                                          1699,
+		"jvm_gc_num_info_threshold_exceeded":                    0,
+		"jvm_gc_num_warn_threshold_exceeded":                    0,
+		"jvm_gc_time_millis":                                    3483,
+		"jvm_gc_total_extra_sleep_time":                         1944,
+		"jvm_log_error":                                         0,
+		"jvm_log_fatal":                                         0,
+		"jvm_log_info":                                          3382077,
+		"jvm_log_warn":                                          3378983,
+		"jvm_mem_heap_committed":                                67,
+		"jvm_mem_heap_max":                                      843,
+		"jvm_mem_heap_used":                                     26,
+		"jvm_mem_max":                                           843,
+		"jvm_mem_non_heap_committed":                            67,
+		"jvm_mem_non_heap_max":                                  -1,
+		"jvm_mem_non_heap_used":                                 66,
+		"jvm_threads_blocked":                                   0,
+		"jvm_threads_new":                                       0,
+		"jvm_threads_runnable":                                  7,
+		"jvm_threads_terminated":                                0,
+		"jvm_threads_timed_waiting":                             34,
+		"jvm_threads_waiting":                                   6,
 	}
 
 	assert.Equal(t, expected, job.Collect())
