2db77212d9d5e1eb6235a84daa5ce08964eca0e2
ilyam8 <ilya@netdata.cloud>
2019-11-14 19:00:35 +0300

    collect wip

--- modules/unbound/collect.go
+++ modules/unbound/collect.go
@@ -10,6 +10,14 @@ import (
 	"github.com/netdata/go.d.plugin/pkg/stm"
 )
 
+type (
+	stats []stat
+	stat  struct {
+		name  string
+		value float64
+	}
+)
+
 func (u *Unbound) collect() (map[string]int64, error) {
 	if err := u.collectStats(); err != nil {
 		return nil, err
@@ -36,89 +44,149 @@ func (u *Unbound) collectStats() error {
 		return err
 	}
 
-	u.collectTotal(stats)
+	for _, s := range stats {
+		u.collectStat(s)
+	}
 	return nil
 }
 
-func (u *Unbound) collectTotal(ss stats) {
-	for _, s := range ss.find("total.") {
-		v := s.value
-		switch s.name {
-		case "total.num.queries":
-			u.mx.Total.Queries.Set(v)
-		case "total.num.queries_ip_ratelimited":
-			u.mx.Total.QueriesIPRL.Set(v)
-		case "total.num.cachehits":
-			u.mx.Total.Cache.Hits.Set(v)
-		case "total.num.cachemiss":
-			u.mx.Total.Cache.Miss.Set(v)
-		case "total.num.prefetch":
-			u.mx.Total.Prefetch.Set(v)
-		case "total.num.zero_ttl":
-			u.mx.Total.ZeroTTL.Set(v)
-		case "total.num.recursivereplies":
-			u.mx.Total.RecursiveReplies.Set(v)
-		case "total.num.dnscrypt.crypted":
-			u.mx.Total.DNSCrypt.Crypted.Set(v)
-		case "total.num.dnscrypt.cert":
-			u.mx.Total.DNSCrypt.Cert.Set(v)
-		case "total.num.dnscrypt.cleartext":
-			u.mx.Total.DNSCrypt.ClearText.Set(v)
-		case "total.num.dnscrypt.malformed":
-			u.mx.Total.DNSCrypt.Malformed.Set(v)
-		case "total.requestlist.avg":
-			u.mx.Total.RequestList.Avg.Set(v)
-		case "total.requestlist.max":
-			u.mx.Total.RequestList.Max.Set(v)
-		case "total.requestlist.overwritten":
-			u.mx.Total.RequestList.Overwritten.Set(v)
-		case "total.requestlist.exceeded":
-			u.mx.Total.RequestList.Exceeded.Set(v)
-		case "total.requestlist.current.all":
-			u.mx.Total.RequestList.CurrentAll.Set(v)
-		case "total.requestlist.current.user":
-			u.mx.Total.RequestList.CurrentUser.Set(v)
-		case "total.recursion.time.avg":
-			u.mx.Total.RecursionTime.Avg.Set(v)
-		case "total.recursion.time.median":
-			u.mx.Total.RecursionTime.Median.Set(v)
-		case "total.tcpusage":
-			u.mx.Total.TCPUsage.Set(v)
-		}
+func (u *Unbound) collectStat(s stat) {
+	switch {
+	case strings.HasPrefix(s.name, "thread"):
+		u.collectThread(s)
+	case strings.HasPrefix(s.name, "total."):
+		u.collectTotal(s)
+	case strings.HasPrefix(s.name, "time."):
+		u.collectTime(s)
+	case strings.HasPrefix(s.name, "num.query.type."):
+		u.collectQueryType(s)
+	case strings.HasPrefix(s.name, "num.query.class."):
+		u.collectQueryClass(s)
+	case strings.HasPrefix(s.name, "num.query.opcode."):
+		u.collectQueryOpCode(s)
+	case strings.HasPrefix(s.name, "num.answer.rcode."):
+		u.collectAnswerRCode(s)
+	case strings.HasPrefix(s.name, "mem."):
+		u.collectMem(s)
 	}
 }
 
-func (u *Unbound) collectTime(ss stats) {
-	for _, s := range ss.find("time.") {
-		switch s.name {
-		case "time.up":
-			u.mx.Uptime.Set(s.value)
-		}
+func (u *Unbound) collectMem(s stat) {
+
+}
+
+func (u *Unbound) collectThread(s stat) {
+	//thread0.*
+	i := strings.IndexByte(s.name, '.')
+	if i == -1 || i < 6 {
+		return
+	}
+	threadID := s.name[6:i]
+	c, ok := u.mx.Thread[threadID]
+	if !ok {
+		c = &common{}
+		u.mx.Thread[threadID] = c
+	}
+	s.name = s.name[i+1:]
+	collectCommon(c, s)
+}
+
+func (u *Unbound) collectTotal(s stat) {
+	i := strings.IndexByte(s.name, '.')
+	s.name = s.name[i+1:]
+	collectCommon(&u.mx.common, s)
+}
+
+func (u *Unbound) collectTime(s stat) {
+	switch s.name {
+	case "time.up":
+		u.mx.Uptime = s.value
 	}
 }
 
-type (
-	stats []stat
-	stat  struct {
-		name  string
-		value float64
+func (u *Unbound) collectQueryType(s stat) {
+	i := len("num.query.type.")
+	typ := s.name[i:]
+	v, ok := u.mx.QueryType[typ]
+	if !ok {
+		//TODO:
 	}
-)
-
-func (ss stats) empty() bool {
-	return len(ss) == 0
+	u.mx.QueryType[typ] += v
 }
 
-func (ss stats) find(prefix string) stats {
-	from := sort.Search(len(ss), func(i int) bool { return ss[i].name >= prefix })
-	if from == len(ss) || !strings.HasPrefix(ss[from].name, prefix) {
-		return nil
+func (u *Unbound) collectQueryClass(s stat) {
+	i := len("num.query.class.")
+	class := s.name[i:]
+	v, ok := u.mx.QueryClass[class]
+	if !ok {
+		//TODO:
 	}
-	until := from + 1
-	for until < len(ss) && strings.HasPrefix(ss[until].name, prefix) {
-		until++
+	u.mx.QueryClass[class] += v
+}
+
+func (u *Unbound) collectQueryOpCode(s stat) {
+	i := len("num.query.opcode.")
+	opcode := s.name[i:]
+	v, ok := u.mx.QueryOpCode[opcode]
+	if !ok {
+		//TODO:
 	}
-	return ss[from:until]
+	u.mx.QueryOpCode[opcode] += v
+}
+
+func (u *Unbound) collectAnswerRCode(s stat) {
+	i := len("num.answer.rcode.")
+	rcode := s.name[i:]
+	v, ok := u.mx.AnswerRCode[rcode]
+	if !ok {
+		//TODO:
+	}
+	u.mx.AnswerRCode[rcode] += v
+}
+
+func collectCommon(c *common, s stat) {
+	switch s.name {
+	case "num.queries":
+		c.Queries = s.value
+	case "num.queries_ip_ratelimited":
+		c.QueriesIPRL = s.value
+	case "num.cachehits":
+		c.Cache.Hits = s.value
+	case "num.cachemiss":
+		c.Cache.Miss = s.value
+	case "num.prefetch":
+		c.Prefetch = s.value
+	case "num.zero_ttl":
+		c.ZeroTTL = s.value
+	case "num.recursivereplies":
+		c.RecursiveReplies = s.value
+	case "num.dnscrypt.crypted":
+		c.DNSCrypt.Crypted = s.value
+	case "num.dnscrypt.cert":
+		c.DNSCrypt.Cert = s.value
+	case "num.dnscrypt.cleartext":
+		c.DNSCrypt.ClearText = s.value
+	case "num.dnscrypt.malformed":
+		c.DNSCrypt.Malformed = s.value
+	case "requestlist.avg":
+		c.RequestList.Avg = s.value
+	case "requestlist.max":
+		c.RequestList.Max = s.value
+	case "requestlist.overwritten":
+		c.RequestList.Overwritten = s.value
+	case "requestlist.exceeded":
+		c.RequestList.Exceeded = s.value
+	case "requestlist.current.all":
+		c.RequestList.CurrentAll = s.value
+	case "requestlist.current.user":
+		c.RequestList.CurrentUser = s.value
+	case "recursion.time.avg":
+		c.RecursionTime.Avg = s.value
+	case "recursion.time.median":
+		c.RecursionTime.Median = s.value
+	case "tcpusage":
+		c.TCPUsage = s.value
+	}
 }
 
 func convertToUnboundStats(lines []string) (stats, error) {
--- modules/unbound/metrics.go
+++ modules/unbound/metrics.go
@@ -1,62 +1,65 @@
 package unbound
 
-import (
-	"github.com/netdata/go.d.plugin/pkg/metrics"
+// https://github.com/NLnetLabs/unbound/blob/master/smallapp/unbound-control.c
+
+type (
+	cache struct {
+		Hits float64 `stm:"hits"`
+		Miss float64 `stm:"miss"`
+	}
+	dnsCrypt struct {
+		Crypted   float64 `stm:"crypted"`
+		Cert      float64 `stm:"cert"`
+		ClearText float64 `stm:"clear_text"`
+		Malformed float64 `stm:"malformed"`
+	}
+	requestList struct {
+		Avg         float64 `stm:"avg"`
+		Max         float64 `stm:"max"`
+		Overwritten float64 `stm:"overwritten"`
+		Exceeded    float64 `stm:"exceeded"`
+		CurrentAll  float64 `stm:"current_all"`
+		CurrentUser float64 `stm:"current_user"`
+	}
+	recursionTime struct {
+		Avg    float64 `stm:"avg"`
+		Median float64 `stm:"median"`
+	}
+	common struct {
+		Queries          float64       `stm:"queries"`
+		QueriesIPRL      float64       `stm:"queries_ip_ratelimited"`
+		Cache            cache         `stm:"cache"`
+		Prefetch         float64       `stm:"prefetch"`
+		ZeroTTL          float64       `stm:"zero_ttl"`
+		RecursiveReplies float64       `stm:"recursive_replies"`
+		DNSCrypt         dnsCrypt      `stm:"dns_crypt"`
+		RequestList      requestList   `stm:"request_list"`
+		RecursionTime    recursionTime `stm:"recursion_time"`
+		TCPUsage         float64       `stm:"tcp_usage"`
+	}
+	extended struct {
+		QueryType   map[string]float64 `stm:"query_type"`
+		QueryClass  map[string]float64 `stm:"query_class"`
+		QueryOpCode map[string]float64 `stm:"query_opcode"`
+		AnswerRCode map[string]float64 `stm:"answer_rcode"`
+	}
 )
 
-/*
-total.num.queries=0
-total.num.queries_ip_ratelimited=0
-total.num.cachehits=0
-total.num.cachemiss=0
-total.num.prefetch=0
-total.num.zero_ttl=0
-total.num.recursivereplies=0
-total.num.dnscrypt.crypted=0
-total.num.dnscrypt.cert=0
-total.num.dnscrypt.cleartext=0
-total.num.dnscrypt.malformed=0
-total.requestlist.avg=0
-total.requestlist.max=0
-total.requestlist.overwritten=0
-total.requestlist.exceeded=0
-total.requestlist.current.all=0
-total.requestlist.current.user=0
-total.recursion.time.avg=0.000000
-total.recursion.time.median=0
-total.tcpusage=0
-*/
-
 type metricsData struct {
-	Total struct {
-		Queries     metrics.Gauge `stm:"queries"`
-		QueriesIPRL metrics.Gauge `stm:"queries_ip_ratelimited"`
-		Cache       struct {
-			Hits metrics.Gauge `stm:"hits"`
-			Miss metrics.Gauge `stm:"miss"`
-		} `stm:"cache"`
-		Prefetch         metrics.Gauge `stm:"prefetch"`
-		ZeroTTL          metrics.Gauge `stm:"zero_ttl"`
-		RecursiveReplies metrics.Gauge `stm:"recursive_replies"`
-		DNSCrypt         struct {
-			Crypted   metrics.Gauge `stm:"crypted"`
-			Cert      metrics.Gauge `stm:"cert"`
-			ClearText metrics.Gauge `stm:"clear_text"`
-			Malformed metrics.Gauge `stm:"_malformed"`
-		} `stm:"dns_crypt"`
-		RequestList struct {
-			Avg         metrics.Gauge `stm:"avg"`
-			Max         metrics.Gauge `stm:"max"`
-			Overwritten metrics.Gauge `stm:"overwritten"`
-			Exceeded    metrics.Gauge `stm:"exceeded"`
-			CurrentAll  metrics.Gauge `stm:"current_all"`
-			CurrentUser metrics.Gauge `stm:"current_user"`
-		} `stm:"request_list"`
-		RecursionTime struct {
-			Avg    metrics.Gauge `stm:"avg"`
-			Median metrics.Gauge `stm:"median"`
-		} `stm:"recursion_time"`
-		TCPUsage metrics.Gauge `stm:"tcp_usage"`
+	common   `stm:"total"`
+	extended `stm:""`
+	Uptime   float64            `stm:"uptime"`
+	Thread   map[string]*common `stm:"thread"`
+}
+
+func newMetricsData() *metricsData {
+	return &metricsData{
+		extended: extended{
+			QueryType:   make(map[string]float64),
+			QueryClass:  make(map[string]float64),
+			QueryOpCode: make(map[string]float64),
+			AnswerRCode: make(map[string]float64),
+		},
+		Thread: make(map[string]*common),
 	}
-	Uptime metrics.Gauge `stm:"uptime"`
 }
