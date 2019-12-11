10581b855b526fa29e4f9195f7d68e4af95a2878
ilyam8 <ilya@netdata.cloud>
2019-12-11 16:02:31 +0300

    logline wip

--- modules/squidlog/logline.go
+++ modules/squidlog/logline.go
@@ -2,6 +2,9 @@ package squidlog
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 // https://wiki.squid-cache.org/Features/LogFormat
@@ -26,15 +29,33 @@ Valid Capture Name: [A-Za-z0-9_]+
 | local                   | squid format code | description                                                            |
 |-------------------------|-------------------|------------------------------------------------------------------------|
 | resp_time               | %tr               | Response time (milliseconds).
-| client_ip               | %>a               | Client source IP address.
+| client_address          | %>a               | Client source IP address.
+| client_address          | %>A               | Client FQDN.
 | cache_code              | %Ss               | Squid request status (TCP_MISS etc).
 | http_code               | %>Hs              | The HTTP response status code from Content Gateway to client.
 | resp_size               | %<st              | Total size of reply sent to client (after adaptation).
 | req_method              | %rm               | Request method (GET/POST etc).
 | hier_code               | %Sh               | Squid hierarchy status (DEFAULT_PARENT etc).
-| server_ip               | %<a               | Server IP address of the last server or peer connection.
+| server_address          | %<a               | Server IP address of the last server or peer connection.
+| server_address          | %<A               | Server FQDN or peer name.
 | mime_type               | %mt               | MIME content type.
+
+Notes:
+- %<a: older versions of Squid would put the origin server hostname here.
 */
+
+var (
+	errEmptyLine     = errors.New("empty line")
+	errBadRespTime   = errors.New("bad response time")
+	errBadClientAddr = errors.New("bad client address")
+	errBadCacheCode  = errors.New("bad cache code")
+	errBadHTTPCode   = errors.New("bad http code")
+	errBadRespSize   = errors.New("bad response size")
+	errBadReqMethod  = errors.New("bad request method")
+	errBadHierCode   = errors.New("bad hier code")
+	errBadServerAddr = errors.New("bad server address")
+	errBadMimeType   = errors.New("bad mime type")
+)
 
 func newEmptyLogLine() *logLine {
 	var l logLine
@@ -44,25 +65,16 @@ func newEmptyLogLine() *logLine {
 
 type (
 	logLine struct {
-		conn  connFields
-		time  timeFields
-		http  httpFields
-		squid squidFields
-	}
-	connFields struct {
-		clientIP string
-		serverIP string
-	}
-	timeFields struct {
+		clientAddr string
+		serverAddr string
+
 		respTime int
-	}
-	httpFields struct {
+		respSize int
+		httpCode int
+
 		reqMethod string
-		respCode  int
 		mimeType  string
-		respSize  int
-	}
-	squidFields struct {
+
 		cacheCode string
 		hierCode  string
 	}
@@ -76,8 +88,8 @@ func (l *logLine) Assign(field string, value string) (err error) {
 	switch field {
 	case "resp_time":
 		err = l.assignRespTime(value)
-	case "client_ip":
-		err = l.assignClientIP(value)
+	case "client_address":
+		err = l.assignClientAddress(value)
 	case "cache_code":
 		err = l.assignCacheCode(value)
 	case "http_code":
@@ -90,90 +102,150 @@ func (l *logLine) Assign(field string, value string) (err error) {
 		err = l.assignHierCode(value)
 	case "mime_type":
 		err = l.assignMimeType(value)
-	case "server_ip":
-		err = l.assignServerIP(value)
+	case "server_address":
+		err = l.assignServerAddress(value)
 	}
 	return err
 }
 
-func (l *logLine) assignRespTime(duration string) error { return nil }
-func (l *logLine) assignClientIP(address string) error  { return nil }
-func (l *logLine) assignCacheCode(code string) error    { return nil }
-func (l *logLine) assignHTTPCode(code string) error     { return nil }
-func (l *logLine) assignRespSize(bytes string) error    { return nil }
-func (l *logLine) assignReqMethod(method string) error  { return nil }
-func (l *logLine) assignHierCode(code string) error     { return nil }
-func (l *logLine) assignMimeType(mime string) error     { return nil }
-func (l *logLine) assignServerIP(address string) error  { return nil }
+func (l *logLine) assignRespTime(time string) error {
+	v, err := strconv.Atoi(time)
+	if err != nil || !isRespTimeValid(v) {
+		return fmt.Errorf("assign '%s': %w", time, errBadRespTime)
+	}
+	l.respTime = v
+	return nil
+}
+func (l *logLine) assignClientAddress(address string) error {
+	return nil
+}
+
+func (l *logLine) assignCacheCode(code string) error {
+	if !isCacheCodeValid(code) {
+		return fmt.Errorf("assign '%s': %w", code, errBadCacheCode)
+	}
+	l.cacheCode = code
+	return nil
+}
+
+func (l *logLine) assignHTTPCode(code string) error {
+	v, err := strconv.Atoi(code)
+	if err != nil || !isHTTPCodeValid(v) {
+		return fmt.Errorf("assign '%s': %w", code, errBadHTTPCode)
+	}
+	l.httpCode = v
+	return nil
+}
+
+func (l *logLine) assignRespSize(size string) error {
+	v, err := strconv.Atoi(size)
+	if err != nil || !isRespSizeValid(v) {
+		return fmt.Errorf("assign '%s': %w", size, errBadRespSize)
+	}
+	l.respSize = v
+	return nil
+}
+
+func (l *logLine) assignReqMethod(method string) error {
+	if !isReqMethodValid(method) {
+		return fmt.Errorf("assign '%s': %w", method, errBadReqMethod)
+	}
+	l.reqMethod = method
+	return nil
+}
+
+func (l *logLine) assignHierCode(code string) error {
+	if !isHierCodeValid(code) {
+		return fmt.Errorf("assign '%s': %w", code, errBadHierCode)
+	}
+	l.hierCode = code
+	return nil
+}
+
+func (l *logLine) assignMimeType(mime string) error {
+	// ICP exchanges usually don't have any content type, and thus are logged "-".
+	//Also, some weird replies have content types ":" or even empty ones.
+	if mime == "-" || mime == ":" {
+		return nil
+	}
+	l.mimeType = mime
+	return nil
+}
+
+func (l *logLine) assignServerAddress(address string) error {
+	// Logged as "-" if there is no hierarchy information.
+	// For TCP HIT, TCP failures, cachemgr requests and all UDP requests, there is no hierarchy information.
+	if address == "-" {
+		return nil
+	}
+	l.serverAddr = address
+	return nil
+}
 
 func (l logLine) verify() error {
 	if l.empty() {
-		return errors.New("")
+		return fmt.Errorf("verify: %w", errEmptyLine)
 	}
 	if l.hasRespTime() && !l.isRespTimeValid() {
-		return errors.New("")
+		return fmt.Errorf("verify '%d': %w", l.respTime, errBadRespTime)
 	}
-	if l.hasClientIP() && !l.isClientIPValid() {
-		return errors.New("")
+	if l.hasClientAddress() && !l.isClientAddressValid() {
+		return fmt.Errorf("verify '%s': %w", l.clientAddr, errBadClientAddr)
 	}
 	if l.hasCacheCode() && !l.isCacheCodeValid() {
-		return errors.New("")
+		return fmt.Errorf("verify '%s': %w", l.cacheCode, errBadCacheCode)
 	}
 	if l.hasHTTPCode() && !l.isHTTPCodeValid() {
-		return errors.New("")
+		return fmt.Errorf("verify '%d': %w", l.httpCode, errBadHTTPCode)
 	}
 	if l.hasRespSize() && !l.isRespSizeValid() {
-		return errors.New("")
+		return fmt.Errorf("verify '%d': %w", l.respSize, errBadRespSize)
 	}
 	if l.hasReqMethod() && !l.isReqMethodValid() {
-		return errors.New("")
+		return fmt.Errorf("verify '%s': %w", l.reqMethod, errBadReqMethod)
 	}
 	if l.hasHierCode() && !l.isHierCodeValid() {
-		return errors.New("")
+		return fmt.Errorf("verify '%s': %w", l.hierCode, errBadHierCode)
 	}
 	if l.hasMimeType() && !l.isMimeTypeValid() {
-		return errors.New("")
+		return fmt.Errorf("verify '%s': %w", l.mimeType, errBadMimeType)
 	}
-	if l.hasMimeType() && !l.isMimeTypeValid() {
-		return errors.New("")
-	}
-	if l.hasServerIP() && !l.isServerIPValid() {
-		return errors.New("")
+	if l.hasServerAddress() && !l.isServerAddressValid() {
+		return fmt.Errorf("verify '%s': %w", l.serverAddr, errBadServerAddr)
 	}
 	return nil
 }
 
-func (l logLine) empty() bool        { return l == emptyLogLine }
-func (l logLine) hasRespTime() bool  { return !isEmptyNumber(l.time.respTime) }
-func (l logLine) hasClientIP() bool  { return !isEmptyString(l.conn.clientIP) }
-func (l logLine) hasCacheCode() bool { return !isEmptyString(l.squid.cacheCode) }
-func (l logLine) hasHTTPCode() bool  { return !isEmptyNumber(l.http.respCode) }
-func (l logLine) hasRespSize() bool  { return !isEmptyNumber(l.http.respSize) }
-func (l logLine) hasReqMethod() bool { return !isEmptyString(l.http.reqMethod) }
-func (l logLine) hasHierCode() bool  { return !isEmptyString(l.squid.hierCode) }
-func (l logLine) hasMimeType() bool  { return !isEmptyString(l.http.mimeType) }
-func (l logLine) hasServerIP() bool  { return !isEmptyString(l.conn.serverIP) }
-
-func (l logLine) isRespTimeValid() bool  { return false }
-func (l logLine) isClientIPValid() bool  { return false }
-func (l logLine) isCacheCodeValid() bool { return false }
-func (l logLine) isHTTPCodeValid() bool  { return false }
-func (l logLine) isRespSizeValid() bool  { return false }
-func (l logLine) isReqMethodValid() bool { return false }
-func (l logLine) isHierCodeValid() bool  { return false }
-func (l logLine) isMimeTypeValid() bool  { return false }
-func (l logLine) isServerIPValid() bool  { return false }
+func (l logLine) empty() bool                { return l == emptyLogLine }
+func (l logLine) hasRespTime() bool          { return !isEmptyNumber(l.respTime) }
+func (l logLine) hasClientAddress() bool     { return !isEmptyString(l.clientAddr) }
+func (l logLine) hasCacheCode() bool         { return !isEmptyString(l.cacheCode) }
+func (l logLine) hasHTTPCode() bool          { return !isEmptyNumber(l.httpCode) }
+func (l logLine) hasRespSize() bool          { return !isEmptyNumber(l.respSize) }
+func (l logLine) hasReqMethod() bool         { return !isEmptyString(l.reqMethod) }
+func (l logLine) hasHierCode() bool          { return !isEmptyString(l.hierCode) }
+func (l logLine) hasMimeType() bool          { return !isEmptyString(l.mimeType) }
+func (l logLine) hasServerAddress() bool     { return !isEmptyString(l.serverAddr) }
+func (l logLine) isRespTimeValid() bool      { return isRespTimeValid(l.respTime) }
+func (l logLine) isClientAddressValid() bool { return false }
+func (l logLine) isCacheCodeValid() bool     { return isCacheCodeValid(l.cacheCode) }
+func (l logLine) isHTTPCodeValid() bool      { return isHTTPCodeValid(l.httpCode) }
+func (l logLine) isRespSizeValid() bool      { return isRespSizeValid(l.respSize) }
+func (l logLine) isReqMethodValid() bool     { return isReqMethodValid(l.reqMethod) }
+func (l logLine) isHierCodeValid() bool      { return isHierCodeValid(l.hierCode) }
+func (l logLine) isMimeTypeValid() bool      { return false }
+func (l logLine) isServerAddressValid() bool { return false }
 
 func (l *logLine) reset() {
-	l.conn.clientIP = emptyString
-	l.conn.serverIP = emptyString
-	l.time.respTime = emptyNumber
-	l.http.reqMethod = emptyString
-	l.http.respCode = emptyNumber
-	l.http.respSize = emptyNumber
-	l.http.mimeType = emptyString
-	l.squid.cacheCode = emptyString
-	l.squid.hierCode = emptyString
+	l.clientAddr = emptyString
+	l.serverAddr = emptyString
+	l.respTime = emptyNumber
+	l.reqMethod = emptyString
+	l.httpCode = emptyNumber
+	l.respSize = emptyNumber
+	l.mimeType = emptyString
+	l.cacheCode = emptyString
+	l.hierCode = emptyString
 }
 
 var emptyLogLine = *newEmptyLogLine()
@@ -189,4 +261,61 @@ func isEmptyString(s string) bool {
 
 func isEmptyNumber(n int) bool {
 	return n == emptyNumber
+}
+
+// isCacheCodeValid does not guarantee cache result code is valid, but it is very likely.
+func isCacheCodeValid(code string) bool {
+	// https://wiki.squid-cache.org/SquidFaq/SquidLogs#Squid_result_codes
+	if i := strings.IndexByte(code, '_'); i <= 0 {
+		// TODO: is at least 1 '_' required?
+		return false
+	} else {
+		code = code[:i]
+	}
+	return code == "TCP" || code == "UDP" || code == "NONE"
+}
+
+// isHierCodeValid does not guarantee hierarchy code is valid, but it is very likely.
+func isHierCodeValid(code string) bool {
+	// https://wiki.squid-cache.org/SquidFaq/SquidLogs#Hierarchy_Codes
+	if i := strings.IndexByte(code, '_'); i <= 0 {
+		return false
+	} else {
+		code = code[:i]
+	}
+	return code == "HIER" || code == "TIMEOUT"
+}
+
+func isHTTPCodeValid(code int) bool {
+	// rfc7231
+	// Informational responses (100–199),
+	// Successful responses (200–299),
+	// Redirects (300–399),
+	// Client errors (400–499),
+	// Server errors (500–599).
+	return code >= 100 && code <= 600
+}
+
+func isRespTimeValid(time int) bool {
+	return time >= 0
+}
+
+func isRespSizeValid(size int) bool {
+	return size >= 0
+}
+
+func isReqMethodValid(method string) bool {
+	// https://wiki.squid-cache.org/SquidFaq/SquidLogs#Request_methods
+	if method == "GET" {
+		return true
+	}
+	switch method {
+	case "HEAD", "POST", "PUT", "PATCH", "DELETE", "CONNECT", "OPTIONS", "TRACE":
+		return true
+	case "ICP_QUERY", "PURGE":
+		return true
+	case "PROPFIND", "PROPATCH", "MKCOL", "COPY", "MOVE", "LOCK", "UNLOCK": // rfc2518
+		return true
+	}
+	return false
 }
