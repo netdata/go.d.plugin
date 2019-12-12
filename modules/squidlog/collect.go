dd7047f4c3ec17cc82d59b85ed6031578380a4e9
ilyam8 <ilya@netdata.cloud>
2019-12-12 12:15:49 +0300

    collect wip

--- modules/squidlog/collect.go
+++ modules/squidlog/collect.go
@@ -63,8 +63,74 @@ func (s *SquidLog) collectLogLines() (int, error) {
 	}
 }
 
-func (s *SquidLog) collectLogLine() {}
+func (s *SquidLog) collectLogLine() {
+	s.mx.Requests.Inc()
+	s.collectRespSize()
+	s.collectClientAddress()
+	s.collectCacheCode()
+	s.collectHTPCode()
+	s.collectRespSize()
+	s.collectReqMethod()
+	s.collectHierCode()
+	s.collectServerAddress()
+	s.collectMimeType()
+}
 
 func (s *SquidLog) collectUnmatched() {
+	s.mx.Requests.Inc()
+	s.mx.ReqUnmatched.Inc()
+}
 
+func (s *SquidLog) collectRespTime() {
+	if !s.line.hasRespTime() {
+		return
+	}
+}
+
+func (s *SquidLog) collectClientAddress() {
+	if !s.line.hasClientAddress() {
+		return
+	}
+}
+
+func (s *SquidLog) collectCacheCode() {
+	if !s.line.hasCacheCode() {
+		return
+	}
+}
+
+func (s *SquidLog) collectHTPCode() {
+	if !s.line.hasHTTPCode() {
+		return
+	}
+}
+
+func (s *SquidLog) collectRespSize() {
+	if !s.line.hasRespSize() {
+		return
+	}
+}
+
+func (s *SquidLog) collectReqMethod() {
+	if !s.line.hasReqMethod() {
+		return
+	}
+}
+
+func (s *SquidLog) collectHierCode() {
+	if !s.line.hasHierCode() {
+		return
+	}
+}
+
+func (s *SquidLog) collectServerAddress() {
+	if !s.line.hasServerAddress() {
+		return
+	}
+}
+
+func (s *SquidLog) collectMimeType() {
+	if !s.line.hasMimeType() {
+		return
+	}
 }
