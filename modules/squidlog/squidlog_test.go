ee52813ab40e5a0bb976330e3392f9232638d92b
ilyam8 <ilya@netdata.cloud>
2019-12-13 00:45:14 +0300

    tests

--- modules/squidlog/squidlog_test.go
+++ modules/squidlog/squidlog_test.go
@@ -55,10 +55,10 @@ func TestSquid_Check_ErrorOnCreatingParserUnknownFormat(t *testing.T) {
 	assert.False(t, squid.Check())
 }
 
-func TestSquid_Check_ErrorOnCreatingParserEmptyLine(t *testing.T) {
+func TestSquid_Check_ErrorOnCreatingParserZeroKnownFields(t *testing.T) {
 	squid := New()
 	defer squid.Cleanup()
-	squid.Path = "testdata/unknown.log"
+	squid.Path = "testdata/access.log"
 	squid.Parser.CSV.Format = "$one $two"
 	require.True(t, squid.Init())
 
@@ -151,6 +151,82 @@ func TestSquidLog_Collect(t *testing.T) {
 	assert.Equal(t, expected, squid.Collect())
 }
 
+func TestSquidLog_Collect_ReturnOldDataIfNothingRead(t *testing.T) {
+	squid := prepareSquidCollect(t)
+
+	expected := map[string]int64{
+		"bytes_sent":                                         6827357,
+		"cache_error_tag_ABORTED":                            326,
+		"cache_handling_tag_CF":                              154,
+		"cache_handling_tag_CLIENT":                          172,
+		"cache_load_source_tag_MEM":                          172,
+		"cache_object_tag_NEGATIVE":                          308,
+		"cache_object_tag_STALE":                             172,
+		"cache_result_code_NONE":                             158,
+		"cache_result_code_TCP_CF_NEGATIVE_NEGATIVE_ABORTED": 154,
+		"cache_result_code_UDP_CLIENT_STALE_MEM_ABORTED":     172,
+		"cache_transport_tag_NONE":                           158,
+		"cache_transport_tag_TCP":                            154,
+		"cache_transport_tag_UDP":                            172,
+		"hier_code_HIER_CACHE_DIGEST_HIT":                    128,
+		"hier_code_HIER_NO_CACHE_DIGEST_DIRECT":              130,
+		"hier_code_HIER_PARENT_HIT":                          106,
+		"hier_code_HIER_SINGLE_PARENT":                       120,
+		"http_resp_0xx":                                      51,
+		"http_resp_1xx":                                      45,
+		"http_resp_2xx":                                      62,
+		"http_resp_3xx":                                      120,
+		"http_resp_4xx":                                      112,
+		"http_resp_5xx":                                      46,
+		"http_resp_6xx":                                      48,
+		"http_resp_code_0":                                   51,
+		"http_resp_code_100":                                 45,
+		"http_resp_code_200":                                 62,
+		"http_resp_code_300":                                 58,
+		"http_resp_code_304":                                 62,
+		"http_resp_code_400":                                 56,
+		"http_resp_code_401":                                 56,
+		"http_resp_code_500":                                 46,
+		"http_resp_code_603":                                 48,
+		"mime_type_application":                              52,
+		"mime_type_audio":                                    56,
+		"mime_type_font":                                     44,
+		"mime_type_image":                                    50,
+		"mime_type_message":                                  44,
+		"mime_type_model":                                    62,
+		"mime_type_multipart":                                61,
+		"mime_type_text":                                     61,
+		"mime_type_video":                                    54,
+		"req_method_COPY":                                    84,
+		"req_method_GET":                                     70,
+		"req_method_HEAD":                                    59,
+		"req_method_OPTIONS":                                 99,
+		"req_method_POST":                                    74,
+		"req_method_PURGE":                                   98,
+		"req_type_bad":                                       56,
+		"req_type_error":                                     46,
+		"req_type_redirect":                                  58,
+		"req_type_success":                                   276,
+		"requests":                                           500,
+		"resp_time_avg":                                      0,
+		"resp_time_count":                                    0,
+		"resp_time_max":                                      0,
+		"resp_time_min":                                      0,
+		"resp_time_sum":                                      0,
+		"server_address_2001:db8:2ce:a":                      79,
+		"server_address_2001:db8:2ce:b":                      89,
+		"server_address_203.0.113.100":                       67,
+		"server_address_203.0.113.200":                       70,
+		"server_address_content-gateway":                     87,
+		"uniq_clients":                                       0,
+		"unmatched":                                          16,
+	}
+
+	_ = squid.Collect()
+
+	assert.Equal(t, expected, squid.Collect())
+}
+
 func prepareSquidCollect(t *testing.T) *SquidLog {
 	t.Helper()
 	squid := New()
@@ -184,7 +260,7 @@ func generateLogs(w io.Writer, num int) error {
 	var line string
 	for i := 0; i < num; i++ {
 		unmatched := randInt(1, 100) > 95
-		if unmatched {
+		if i > 0 && unmatched {
 			line = "Unmatched! The rat the cat the dog chased killed ate the malt!\n"
 		} else {
 			// 1576177221.686     0 ::1 TCP_MISS/200 1621 GET cache_object://localhost/counters - HIER_NONE/- text/plain
