ea8fdec231668731ad69cb971713aa3db565696f
ilyam8 <ilya@netdata.cloud>
2019-12-16 15:34:43 +0300

    portcheck test fix

--- modules/portcheck/portcheck_test.go
+++ modules/portcheck/portcheck_test.go
@@ -22,7 +22,7 @@ func TestPortCheck_Init(t *testing.T) {
 	job := New()
 
 	job.Host = "127.0.0.1"
-	job.Ports = []int{38001, 38002}
+	job.Ports = []int{39001, 39002}
 	assert.True(t, job.Init())
 	assert.Len(t, job.ports, 2)
 }
@@ -32,13 +32,17 @@ func TestPortCheck_InitNG(t *testing.T) {
 	assert.False(t, job.Init())
 	job.Host = "127.0.0.1"
 	assert.False(t, job.Init())
-	job.Ports = []int{38001, 38002}
+	job.Ports = []int{39001, 39002}
 	assert.True(t, job.Init())
 }
 
-func TestPortCheck_Check(t *testing.T) { assert.True(t, New().Check()) }
+func TestPortCheck_Check(t *testing.T) {
+	assert.True(t, New().Check())
+}
 
-func TestPortCheck_Cleanup(t *testing.T) { New().Cleanup() }
+func TestPortCheck_Cleanup(t *testing.T) {
+	New().Cleanup()
+}
 
 func TestPortCheck_Charts(t *testing.T) {
 	job := New()
@@ -50,69 +54,69 @@ func TestPortCheck_Collect(t *testing.T) {
 	job := New()
 
 	job.Host = "127.0.0.1"
-	job.Ports = []int{38001, 38002}
+	job.Ports = []int{39001, 39002}
 	job.UpdateEvery = 5
 	job.dial = testDial(nil)
 	require.True(t, job.Init())
 	require.True(t, job.Check())
 
 	expected := map[string]int64{
-		"port_38001_current_state_duration": int64(job.UpdateEvery),
-		"port_38001_failed":                 0,
-		"port_38001_latency":                0,
-		"port_38001_success":                1,
-		"port_38001_timeout":                0,
-		"port_38002_current_state_duration": int64(job.UpdateEvery),
-		"port_38002_failed":                 0,
-		"port_38002_latency":                0,
-		"port_38002_success":                1,
-		"port_38002_timeout":                0,
+		"port_39001_current_state_duration": int64(job.UpdateEvery),
+		"port_39001_failed":                 0,
+		"port_39001_latency":                0,
+		"port_39001_success":                1,
+		"port_39001_timeout":                0,
+		"port_39002_current_state_duration": int64(job.UpdateEvery),
+		"port_39002_failed":                 0,
+		"port_39002_latency":                0,
+		"port_39002_success":                1,
+		"port_39002_timeout":                0,
 	}
 	assert.Equal(t, expected, job.Collect())
 
 	expected = map[string]int64{
-		"port_38001_current_state_duration": int64(job.UpdateEvery) * 2,
-		"port_38001_failed":                 0,
-		"port_38001_latency":                0,
-		"port_38001_success":                1,
-		"port_38001_timeout":                0,
-		"port_38002_current_state_duration": int64(job.UpdateEvery) * 2,
-		"port_38002_failed":                 0,
-		"port_38002_latency":                0,
-		"port_38002_success":                1,
-		"port_38002_timeout":                0,
+		"port_39001_current_state_duration": int64(job.UpdateEvery) * 2,
+		"port_39001_failed":                 0,
+		"port_39001_latency":                0,
+		"port_39001_success":                1,
+		"port_39001_timeout":                0,
+		"port_39002_current_state_duration": int64(job.UpdateEvery) * 2,
+		"port_39002_failed":                 0,
+		"port_39002_latency":                0,
+		"port_39002_success":                1,
+		"port_39002_timeout":                0,
 	}
 	assert.Equal(t, expected, job.Collect())
 
 	job.dial = testDial(errors.New("failed"))
 
 	expected = map[string]int64{
-		"port_38001_current_state_duration": int64(job.UpdateEvery),
-		"port_38001_failed":                 1,
-		"port_38001_latency":                0,
-		"port_38001_success":                0,
-		"port_38001_timeout":                0,
-		"port_38002_current_state_duration": int64(job.UpdateEvery),
-		"port_38002_failed":                 1,
-		"port_38002_latency":                0,
-		"port_38002_success":                0,
-		"port_38002_timeout":                0,
+		"port_39001_current_state_duration": int64(job.UpdateEvery),
+		"port_39001_failed":                 1,
+		"port_39001_latency":                0,
+		"port_39001_success":                0,
+		"port_39001_timeout":                0,
+		"port_39002_current_state_duration": int64(job.UpdateEvery),
+		"port_39002_failed":                 1,
+		"port_39002_latency":                0,
+		"port_39002_success":                0,
+		"port_39002_timeout":                0,
 	}
 	assert.Equal(t, expected, job.Collect())
 
 	job.dial = testDial(timeoutError{})
 
 	expected = map[string]int64{
-		"port_38001_current_state_duration": int64(job.UpdateEvery),
-		"port_38001_failed":                 0,
-		"port_38001_latency":                0,
-		"port_38001_success":                0,
-		"port_38001_timeout":                1,
-		"port_38002_current_state_duration": int64(job.UpdateEvery),
-		"port_38002_failed":                 0,
-		"port_38002_latency":                0,
-		"port_38002_success":                0,
-		"port_38002_timeout":                1,
+		"port_39001_current_state_duration": int64(job.UpdateEvery),
+		"port_39001_failed":                 0,
+		"port_39001_latency":                0,
+		"port_39001_success":                0,
+		"port_39001_timeout":                1,
+		"port_39002_current_state_duration": int64(job.UpdateEvery),
+		"port_39002_failed":                 0,
+		"port_39002_latency":                0,
+		"port_39002_success":                0,
+		"port_39002_timeout":                1,
 	}
 	assert.Equal(t, expected, job.Collect())
 }
@@ -124,5 +128,4 @@ func testDial(err error) dialFunc {
 type timeoutError struct{}
 
 func (timeoutError) Error() string { return "timeout" }
-
 func (timeoutError) Timeout() bool { return true }
