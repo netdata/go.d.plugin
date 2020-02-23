8a6a05c77ff40ad6509db5c2078bb6dcf04f6de7
Ilya Mashchenko <ilya@netdata.cloud>
2020-02-23 22:29:39 +0900

    build: code cleanup and exclude debug info from binary (#342)
    * hack/go-build: fix shellcheck warnings, cleanups
    
    * hack/go-build: add `-w` and `-s` flags to ldflags

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -2,30 +2,77 @@
 
 set -e
 
-WHICH="$1"
-PLATFORMS=(darwin/386 darwin/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips linux/mipsle linux/mips64 linux/mips64le)
+PLATFORMS=(
+  darwin/386
+  darwin/amd64
+  freebsd/386
+  freebsd/amd64
+  freebsd/arm
+  linux/386
+  linux/amd64
+  linux/arm
+  linux/arm64
+  linux/ppc64
+  linux/ppc64le
+  linux/mips
+  linux/mipsle
+  linux/mips64
+  linux/mips64le
+)
 
-VERSION="${TRAVIS_TAG}"
-: "${VERSION:=$(git describe --tags --always --dirty)}"
-GOFLAGS=${GOFLAGS:-}
-GLDFLAGS=${GLDFLAGS:-}
-GLDFLAGS="$GLDFLAGS -X main.version=$VERSION"
+getos() {
+  local IFS=/ && read -ra array <<< "$1" && echo "${array[0]}"
+}
+
+getarch() {
+  local IFS=/ && read -ra array <<< "$1" && echo "${array[1]}"
+}
+
+WHICH="$1"
+
+VERSION="${TRAVIS_TAG:-$(git describe --tags --always --dirty)}"
+
+GOLDFLAGS=${GLDFLAGS:-}
+GOLDFLAGS="$GOLDFLAGS -w -s -X main.version=$VERSION"
+
+build_all_platforms() {
+  for PLATFORM in "${PLATFORMS[@]}"; do
+    GOOS=$(getos "$PLATFORM")
+    GOARCH=$(getarch "$PLATFORM")
+    FILE="bin/go.d.plugin-${VERSION}.${GOOS}-${GOARCH}"
+
+    echo "Building for os/arch ${GOOS}/${GOARCH}"
+    CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build -ldflags "${GOLDFLAGS}" -o "${FILE}" github.com/netdata/go.d.plugin/cmd/godplugin
+
+    ARCHIVE="${FILE}.tar.gz"
+    tar -C bin -cvzf "${ARCHIVE}" "${FILE/bin\//}"
+    rm "${FILE}"
+  done
+}
+
+build_specific_platform() {
+  GOOS=$(getos "$1")
+  GOARCH=$(getarch "$1")
+  : "${GOARCH:=amd64}"
+
+  echo "Building for os/arch ${GOOS}/${GOARCH}"
+  CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build -o bin/godplugin -ldflags "${GOLDFLAGS}" github.com/netdata/go.d.plugin/cmd/godplugin
+}
+
+build_current_platform() {
+  eval "$(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")"
+  GOOS=${GOOS:-$GOHOSTOS}
+  GOARCH=${GOARCH:-$GOHOSTARCH}
+
+  echo "Building for os/arch ${GOOS}/${GOARCH}"
+  CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build -o bin/godplugin -ldflags "${GOLDFLAGS}" github.com/netdata/go.d.plugin/cmd/godplugin
+}
 
 echo "Building binaries for version: $VERSION"
-if [[ "${WHICH}" != "all" ]]; then
-	eval $(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")
-	: "${GOOS:=${GOHOSTOS}}"
-	: "${GOARCH:=${GOHOSTARCH}}"
-	CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/godplugin github.com/netdata/go.d.plugin/cmd/godplugin
+if [[ "$WHICH" == "all" ]]; then
+  build_all_platforms
+elif [[ -n "$WHICH" ]]; then
+  build_specific_platform "$WHICH"
 else
-	for PLATFORM in "${PLATFORMS[@]}"; do
-		PLTFRM_SPLT=(${PLATFORM//\// })
-		GOOS=${PLTFRM_SPLT[0]}
-		GOARCH=${PLTFRM_SPLT[1]}
-		FILE="bin/go.d.plugin-${VERSION}.${GOOS}-${GOARCH}"
-		CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o "${FILE}" github.com/netdata/go.d.plugin/cmd/godplugin
-		ARCHIVE="${FILE}.tar.gz"
-		tar -C bin -cvzf ${ARCHIVE} ${FILE/bin\//}
-		rm ${FILE}
-	done
+  build_current_platform
 fi
