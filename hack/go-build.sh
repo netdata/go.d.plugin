9cd5e5fdbedb3df85bf2788d56787185e8d74bbb
ilyam8 <ilya@netdata.cloud>
2020-02-27 08:03:04 +0300

    build: add freebsd/arm64

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -8,6 +8,7 @@ PLATFORMS=(
   freebsd/386
   freebsd/amd64
   freebsd/arm
+  freebsd/arm64
   linux/386
   linux/amd64
   linux/arm
@@ -35,14 +36,22 @@ VERSION="${TRAVIS_TAG:-$(git describe --tags --always --dirty)}"
 GOLDFLAGS=${GLDFLAGS:-}
 GOLDFLAGS="$GOLDFLAGS -w -s -X main.version=$VERSION"
 
+build() {
+  echo "Building ${GOOS}/${GOARCH}"
+  CGO_ENABLED=0 GOOS="$1" GOARCH="$2" go build -ldflags "${GOLDFLAGS}" -o "$3" "github.com/netdata/go.d.plugin/cmd/godplugin"
+  return
+}
+
 build_all_platforms() {
   for PLATFORM in "${PLATFORMS[@]}"; do
     GOOS=$(getos "$PLATFORM")
     GOARCH=$(getarch "$PLATFORM")
     FILE="bin/go.d.plugin-${VERSION}.${GOOS}-${GOARCH}"
 
-    echo "Building for os/arch ${GOOS}/${GOARCH}"
-    CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build -ldflags "${GOLDFLAGS}" -o "${FILE}" github.com/netdata/go.d.plugin/cmd/godplugin
+    if ! build "$GOOS" "$GOARCH" "$FILE"; then
+      echo "Couldn't build for ${GOOS}/${GOARCH}"
+      continue
+    fi
 
     ARCHIVE="${FILE}.tar.gz"
     tar -C bin -cvzf "${ARCHIVE}" "${FILE/bin\//}"
@@ -55,8 +64,7 @@ build_specific_platform() {
   GOARCH=$(getarch "$1")
   : "${GOARCH:=amd64}"
 
-  echo "Building for os/arch ${GOOS}/${GOARCH}"
-  CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build -o bin/godplugin -ldflags "${GOLDFLAGS}" github.com/netdata/go.d.plugin/cmd/godplugin
+  build "$GOOS" "$GOARCH" bin/godplugin
 }
 
 build_current_platform() {
@@ -64,8 +72,7 @@ build_current_platform() {
   GOOS=${GOOS:-$GOHOSTOS}
   GOARCH=${GOARCH:-$GOHOSTARCH}
 
-  echo "Building for os/arch ${GOOS}/${GOARCH}"
-  CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build -o bin/godplugin -ldflags "${GOLDFLAGS}" github.com/netdata/go.d.plugin/cmd/godplugin
+  build "$GOOS" "$GOARCH" bin/godplugin
 }
 
 echo "Building binaries for version: $VERSION"
