0ebe2755751ace272408d43827b3e95a531cd010
Maciej Klepaczewski <matt@fasterwebsite.com>
2020-08-28 00:27:54 +0200

    Fixed non-unique naming of tests in json_test.go

--- pkg/logs/json_test.go
+++ pkg/logs/json_test.go
@@ -24,7 +24,7 @@ func TestNewJSONParser(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:    "empty config",
+			name:    "basic config",
 			config:  testJSONConfig,
 			wantErr: false,
 		},
