442e92a4051f4ca5b7f5bc93cead1d633c1eeefa
ilyam8 <ilya@netdata.cloud>
2020-09-04 12:47:16 +0300

    phpfpm: add some debug messages

--- modules/phpfpm/init.go
+++ modules/phpfpm/init.go
@@ -26,6 +26,8 @@ func (p Phpfpm) initHTTPClient() (*httpClient, error) {
 	if err != nil {
 		return nil, fmt.Errorf("create HTTP client: %v", err)
 	}
+	p.Debugf("using HTTP client, URL: %s", p.URL)
+	p.Debugf("using timeout: %s", p.Timeout.Duration)
 	return newHTTPClient(c, p.Request), nil
 }
 
@@ -33,5 +35,7 @@ func (p Phpfpm) initSocketClient() (*socketClient, error) {
 	if _, err := os.Stat(p.Socket); err != nil {
 		return nil, fmt.Errorf("the socket '%s' does not exist: %v", p.Socket, err)
 	}
+	p.Debugf("using socket client : %s", p.Socket)
+	p.Debugf("using timeout: %s", p.Timeout.Duration)
 	return newSocketClient(p.Socket, p.Timeout.Duration), nil
 }
