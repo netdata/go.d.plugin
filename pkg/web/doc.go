43c22c0e8188e1dd16899911276c1014b3c907e4
ilyam8 <ilya@netdata.cloud>
2020-09-16 13:39:59 +0300

    pkg/web: wip add some documentation

---
+++ pkg/web/doc.go
@@ -0,0 +1,7 @@
+/*
+Package web contains HTTP request and client configurations.
+HTTP structure aggregates both of them and it's the only structure that intended to be used as part of a module's configuration.
+Every module that uses HTTP requests to gather metrics should use it.
+It allows to have same set of user configurable features across all modules.
+*/
+package web
