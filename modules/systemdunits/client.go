0491b6fd186294227e6164c648f152b9d22d6927
ilyam8 <ilya@netdata.cloud>
2020-09-28 21:14:53 +0300

    module/systemdunits: todos

--- modules/systemdunits/client.go
+++ modules/systemdunits/client.go
@@ -5,6 +5,9 @@ import "github.com/coreos/go-systemd/v22/dbus"
 type systemdDBusClient struct{}
 
 func (systemdDBusClient) connect() (systemdConnection, error) {
+	// TODO: switch when available
+	// New => NewWithContext
+	// ListUnits => ListUnitsContext
 	return dbus.New()
 }
 
