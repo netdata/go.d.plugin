d0567e200a1b0da1481c25bf88760829fee0e544
ilyam8 <ilya@netdata.cloud>
2020-09-30 22:56:59 +0300

    module/systemdunits: update coreos/go-systemd/dbus pkg to the latest, use *Context functions, add timeout option

--- modules/systemdunits/client.go
+++ modules/systemdunits/client.go
@@ -1,14 +1,23 @@
 package systemdunits
 
-import "github.com/coreos/go-systemd/v22/dbus"
+import (
+	"context"
+
+	"github.com/coreos/go-systemd/v22/dbus"
+)
+
+type systemdClient interface {
+	connect() (systemdConnection, error)
+}
+type systemdConnection interface {
+	Close()
+	ListUnitsByPatternsContext(ctx context.Context, states []string, patterns []string) ([]dbus.UnitStatus, error)
+}
 
 type systemdDBusClient struct{}
 
 func (systemdDBusClient) connect() (systemdConnection, error) {
-	// TODO: switch when available
-	// New => NewWithContext
-	// ListUnits => ListUnitsContext
-	return dbus.New()
+	return dbus.NewWithContext(context.Background())
 }
 
 func newSystemdDBusClient() *systemdDBusClient {
