2c03c2d21976062b5bd32aba73e521ce10ac604b
ilyam8 <ilya@netdata.cloud>
2020-11-03 17:39:52 +0300

    modules/example: collect minor

--- modules/example/collect.go
+++ modules/example/collect.go
@@ -9,33 +9,37 @@ import (
 func (e *Example) collect() (map[string]int64, error) {
 	collected := make(map[string]int64)
 
-	var numOfDims int
 	for _, chart := range *e.Charts() {
-		if chart.Opts.Hidden {
-			numOfDims = e.Config.HiddenCharts.Dims
-		} else {
-			numOfDims = e.Config.Charts.Dims
-		}
-
-		for i := 0; i < numOfDims; i++ {
-			name := fmt.Sprintf("random%d", i)
-			id := fmt.Sprintf("%s_%s", chart.ID, name)
-
-			if !e.collectedDims[id] {
-				e.collectedDims[id] = true
-
-				dim := &module.Dim{ID: id, Name: name}
-				if err := chart.AddDim(dim); err != nil {
-					e.Warning(err)
-				}
-				chart.MarkNotCreated()
-			}
-			if i%2 == 0 {
-				collected[id] = e.randInt()
-			} else {
-				collected[id] = -e.randInt()
-			}
-		}
+		e.collectChart(collected, chart)
 	}
 	return collected, nil
+}
+
+func (e *Example) collectChart(collected map[string]int64, chart *module.Chart) {
+	var num int
+	if chart.Opts.Hidden {
+		num = e.Config.HiddenCharts.Dims
+	} else {
+		num = e.Config.Charts.Dims
+	}
+
+	for i := 0; i < num; i++ {
+		name := fmt.Sprintf("random%d", i)
+		id := fmt.Sprintf("%s_%s", chart.ID, name)
+
+		if !e.collectedDims[id] {
+			e.collectedDims[id] = true
+
+			dim := &module.Dim{ID: id, Name: name}
+			if err := chart.AddDim(dim); err != nil {
+				e.Warning(err)
+			}
+			chart.MarkNotCreated()
+		}
+		if i%2 == 0 {
+			collected[id] = e.randInt()
+		} else {
+			collected[id] = -e.randInt()
+		}
+	}
 }
