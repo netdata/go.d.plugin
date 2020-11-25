938faca27e20ffc3c5b1d4a1da04c513259b9020
Thiago Marques <thiagoftsm@gmail.com>
2020-11-25 20:36:00 +0000

    energid: Create the collector basis

--- modules/init.go
+++ modules/init.go
@@ -15,6 +15,7 @@ import (
 	_ "github.com/netdata/go.d.plugin/modules/docker_engine"
 	_ "github.com/netdata/go.d.plugin/modules/dockerhub"
 	_ "github.com/netdata/go.d.plugin/modules/elasticsearch"
+	_ "github.com/netdata/go.d.plugin/modules/energid"
 	_ "github.com/netdata/go.d.plugin/modules/example"
 	_ "github.com/netdata/go.d.plugin/modules/filecheck"
 	_ "github.com/netdata/go.d.plugin/modules/fluentd"
