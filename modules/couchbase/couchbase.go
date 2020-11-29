b8f48f835552f37e2c3c1864a0b757771974f582
Hamed Beiranvand <Hamed.Beiranvand@linkit.nl>
2020-11-29 19:09:26 +0100

    update couchbase config file

--- modules/couchbase/couchbase.go
+++ modules/couchbase/couchbase.go
@@ -10,7 +10,7 @@ import (
 func init() {
 	module.Register("couchbase", module.Creator{
 		Defaults: module.Defaults{
-			UpdateEvery: 3,
+			UpdateEvery: 10,
 		},
 		Create: func() module.Module { return New() },
 	})
