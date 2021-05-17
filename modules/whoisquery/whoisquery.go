52b9d2cab755573421987de68f583f37059a4cd7
ilyam8 <ilya@netdata.cloud>
2021-05-17 14:13:53 +0300

    rename receiver

--- modules/whoisquery/whoisquery.go
+++ modules/whoisquery/whoisquery.go
@@ -41,47 +41,47 @@ type WhoisQuery struct {
 	prov   provider
 }
 
-func (x WhoisQuery) validateConfig() error {
-	if x.Source == "" {
+func (wq WhoisQuery) validateConfig() error {
+	if wq.Source == "" {
 		return errors.New("source is not set")
 	}
 	return nil
 }
 
-func (x *WhoisQuery) initProvider() error {
-	p, err := newProvider(x.Config)
+func (wq *WhoisQuery) initProvider() error {
+	p, err := newProvider(wq.Config)
 	if err != nil {
 		return err
 	}
-	x.prov = p
+	wq.prov = p
 	return nil
 }
 
-func (x *WhoisQuery) Init() bool {
-	if err := x.validateConfig(); err != nil {
-		x.Errorf("error on validating config: %v", err)
+func (wq *WhoisQuery) Init() bool {
+	if err := wq.validateConfig(); err != nil {
+		wq.Errorf("error on validating config: %v", err)
 		return false
 	}
 
-	if err := x.initProvider(); err != nil {
-		x.Errorf("error on initializing whois provider: %v", err)
+	if err := wq.initProvider(); err != nil {
+		wq.Errorf("error on initializing whois provider: %v", err)
 		return false
 	}
 	return true
 }
 
-func (x *WhoisQuery) Check() bool {
-	return len(x.Collect()) > 0
+func (wq *WhoisQuery) Check() bool {
+	return len(wq.Collect()) > 0
 }
 
-func (x WhoisQuery) Charts() *Charts {
+func (wq WhoisQuery) Charts() *Charts {
 	return charts.Copy()
 }
 
-func (x *WhoisQuery) Collect() map[string]int64 {
-	mx, err := x.collect()
+func (wq *WhoisQuery) Collect() map[string]int64 {
+	mx, err := wq.collect()
 	if err != nil {
-		x.Error(err)
+		wq.Error(err)
 	}
 
 	if len(mx) == 0 {
