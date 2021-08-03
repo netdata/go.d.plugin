bcb2dc01170f6efad4c45d9f7d23e9a6f0246c0f
georgeok <giorgos.n.oikonomou@gmail.com>
2021-08-03 19:00:06 +0300

    reorder collect.go functions

--- modules/mongodb/collect.go
+++ modules/mongodb/collect.go
@@ -9,61 +9,18 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func addIfExists(serverStatus map[string]interface{}, id string, ms map[string]int64) {
-	mMap := serverStatus
-	keys := strings.Split(id, ".")
-	for _, key := range keys {
-		k := fromID(key)
-		val, ok := mMap[k]
-		if !ok {
-			return
-		}
-		switch t := val.(type) {
-		case map[string]interface{}:
-			mMap = val.(map[string]interface{})
-		case int64:
-			if _, ok := mMap[fromID(key)]; ok {
-				ms[id] = t
-			}
-		case int32:
-			if _, ok := mMap[fromID(key)]; ok {
-				ms[id] = int64(t)
-			}
-		default:
-			return
-		}
-	}
-}
-
-func iterateServerStatus(ms map[string]int64, status map[string]interface{}) {
-	for _, chart := range serverStatusCharts {
-		for _, dim := range chart.Dims {
-			addIfExists(status, dim.ID, ms)
-		}
-	}
-}
-
-func (m *Mongo) metricExists(serverStatus map[string]interface{}, key string, chart *module.Chart) {
-	keys := strings.Split(key, ".")
-	for _, k := range keys {
-		val, ok := serverStatus[k]
-		if !ok {
-			return
-		}
-		switch val.(type) {
-		case map[string]interface{}:
-			serverStatus = val.(map[string]interface{})
-		default:
-			return
-		}
-	}
-	if !m.charts.Has(chart.ID) {
-		if err := m.charts.Add(chart.Copy()); err != nil {
-			m.Warning(err)
-		}
+func (m *Mongo) serverStatusCollect(ms map[string]int64) {
+	var status map[string]interface{}
+	command := bson.D{{Key: "serverStatus", Value: 1}}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*m.Timeout)
+	defer cancel()
+	err := m.client.Database("admin").RunCommand(ctx, command).Decode(&status)
+	if err != nil {
+		m.Errorf("error get server status from mongo: %s", err)
 		return
 	}
-
+	m.addOptionalCharts(status)
+	iterateServerStatus(ms, status)
 }
 
 func (m *Mongo) addOptionalCharts(status map[string]interface{}) {
@@ -89,16 +46,59 @@ func (m *Mongo) addOptionalCharts(status map[string]interface{}) {
 	m.metricExists(status, "wiredTiger.transaction", &chartWiredTigerTransactions)
 }
 
-func (m *Mongo) serverStatusCollect(ms map[string]int64) {
-	var status map[string]interface{}
-	command := bson.D{{Key: "serverStatus", Value: 1}}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*m.Timeout)
-	defer cancel()
-	err := m.client.Database("admin").RunCommand(ctx, command).Decode(&status)
-	if err != nil {
-		m.Errorf("error get server status from mongo: %s", err)
+func (m *Mongo) metricExists(serverStatus map[string]interface{}, key string, chart *module.Chart) {
+	keys := strings.Split(key, ".")
+	for _, k := range keys {
+		val, ok := serverStatus[k]
+		if !ok {
+			return
+		}
+		switch val.(type) {
+		case map[string]interface{}:
+			serverStatus = val.(map[string]interface{})
+		default:
+			return
+		}
+	}
+	if !m.charts.Has(chart.ID) {
+		if err := m.charts.Add(chart.Copy()); err != nil {
+			m.Warning(err)
+		}
 		return
 	}
-	m.addOptionalCharts(status)
-	iterateServerStatus(ms, status)
+
+}
+
+func iterateServerStatus(ms map[string]int64, status map[string]interface{}) {
+	for _, chart := range serverStatusCharts {
+		for _, dim := range chart.Dims {
+			addIfExists(status, dim.ID, ms)
+		}
+	}
+}
+
+func addIfExists(serverStatus map[string]interface{}, id string, ms map[string]int64) {
+	mMap := serverStatus
+	keys := strings.Split(id, ".")
+	for _, key := range keys {
+		k := fromID(key)
+		val, ok := mMap[k]
+		if !ok {
+			return
+		}
+		switch t := val.(type) {
+		case map[string]interface{}:
+			mMap = val.(map[string]interface{})
+		case int64:
+			if _, ok := mMap[fromID(key)]; ok {
+				ms[id] = t
+			}
+		case int32:
+			if _, ok := mMap[fromID(key)]; ok {
+				ms[id] = int64(t)
+			}
+		default:
+			return
+		}
+	}
 }
