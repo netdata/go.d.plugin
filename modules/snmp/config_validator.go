d00381f62d65f8769c9fc3197d5778829fa07e7f
ilyam8 <ilya@netdata.cloud>
2022-03-20 16:50:26 +0200

    reoder methods in config validator

--- modules/snmp/config_validator.go
+++ modules/snmp/config_validator.go
@@ -7,49 +7,66 @@ import (
 	"github.com/netdata/go.d.plugin/agent/module"
 )
 
-func appendError(err error, msg string) error {
-	var e error
-	if err == nil {
-		if msg == "" {
-			return nil
-		}
-		e = errors.New(msg)
-	} else {
-		if msg == "" {
-			return err
-		}
-		e = fmt.Errorf("%w %s", err, msg)
-	}
-	return e
-}
-
-func (d Dimension) validateConfig(chartIdx, dimIdx int) error {
+func (s SNMP) validateConfig() error {
 	var err error
 	err = nil
-	if d.Name == "" {
-		err = appendError(err, fmt.Sprintf("invalid or missing value: charts[%d].dimension[%d].name;", chartIdx, dimIdx))
-	}
-	if d.OID == "" {
-		err = appendError(err, fmt.Sprintf("missing value: charts[%d].dimension[%d].oid;", chartIdx, dimIdx))
-	}
-	if d.Algorithm != nil {
-		if *d.Algorithm == "" ||
-			(*d.Algorithm != string(module.Incremental) &&
-				*d.Algorithm != string(module.PercentOfIncremental) &&
-				*d.Algorithm != string(module.PercentOfAbsolute)) {
-			err = appendError(err, fmt.Sprintf("invalid or missing value: charts[%d].dimension[%d].algorithm;", chartIdx, dimIdx))
+	if s.ChartInput == nil {
+		err = appendError(err, "charts missing from config")
+	} else {
+		for i, chartIn := range s.ChartInput {
+			if e := chartIn.validateConfig(i); e != nil {
+				err = appendError(err, e.Error())
+			}
 		}
 	}
-	if d.Multiplier != nil {
-		if *d.Multiplier == 0 {
-			err = appendError(err, fmt.Sprintf("integer set to 0: charts[%d].dimension[%d].multiplier;", chartIdx, dimIdx))
+
+	if s.Options != nil {
+		if e := s.Options.validateConfig(); e != nil {
+			err = appendError(err, e.Error())
+		}
+		if s.Options.Version == 3 {
+			if s.User == nil {
+				err = appendError(err, "SNMP v3 missing user credentials;")
+			}
+		} else {
+			if s.Community == nil {
+				err = appendError(err, "SNMP v1/2 missing community value;")
+			}
 		}
 	}
-	if d.Divisor != nil {
-		if *d.Divisor == 0 {
-			err = appendError(err, fmt.Sprintf("integer set to 0: charts[%d].dimension[%d].divisor;", chartIdx, dimIdx))
+
+	if s.User != nil {
+		if e := s.User.validateConfig(); e != nil {
+			err = appendError(err, e.Error())
 		}
 	}
+
+	if s.Config.Hostname == "" {
+		err = appendError(err, "missing value: name;")
+	}
+
+	return err
+}
+
+func (o Options) validateConfig() error {
+	var err error
+	err = nil
+	if o.Port <= 0 && o.Port > 65535 {
+		err = appendError(err, fmt.Sprintf("invalid range of value(%d): options.port;", o.Port))
+	}
+	if o.Version < 1 || o.Version > 3 {
+		err = appendError(err, fmt.Sprintf("invalid range of value(%d): options.versions;", o.Version))
+	}
+	if o.Retries < 1 || o.Retries > 100 {
+		err = appendError(err, fmt.Sprintf("invalid range of value(%d): options.retries;", o.Retries))
+	}
+	if o.Timeout < 1 {
+		err = appendError(err, fmt.Sprintf("invalid value(%d): options.timeout;", o.Timeout))
+	}
+	if o.MaxOIDs <= 0 {
+		err = appendError(err, fmt.Sprintf("invalid value(%d): max_request_size;", o.MaxOIDs))
+	}
+
 	return err
 }
 
@@ -99,65 +116,48 @@ func (c ChartsConfig) validateConfig(chartIdx int) error {
 	return err
 }
 
-func (o Options) validateConfig() error {
+func (d Dimension) validateConfig(chartIdx, dimIdx int) error {
 	var err error
 	err = nil
-	if o.Port <= 0 && o.Port > 65535 {
-		err = appendError(err, fmt.Sprintf("invalid range of value(%d): options.port;", o.Port))
+	if d.Name == "" {
+		err = appendError(err, fmt.Sprintf("invalid or missing value: charts[%d].dimension[%d].name;", chartIdx, dimIdx))
 	}
-	if o.Version < 1 || o.Version > 3 {
-		err = appendError(err, fmt.Sprintf("invalid range of value(%d): options.versions;", o.Version))
+	if d.OID == "" {
+		err = appendError(err, fmt.Sprintf("missing value: charts[%d].dimension[%d].oid;", chartIdx, dimIdx))
 	}
-	if o.Retries < 1 || o.Retries > 100 {
-		err = appendError(err, fmt.Sprintf("invalid range of value(%d): options.retries;", o.Retries))
+	if d.Algorithm != nil {
+		if *d.Algorithm == "" ||
+			(*d.Algorithm != string(module.Incremental) &&
+				*d.Algorithm != string(module.PercentOfIncremental) &&
+				*d.Algorithm != string(module.PercentOfAbsolute)) {
+			err = appendError(err, fmt.Sprintf("invalid or missing value: charts[%d].dimension[%d].algorithm;", chartIdx, dimIdx))
+		}
 	}
-	if o.Timeout < 1 {
-		err = appendError(err, fmt.Sprintf("invalid value(%d): options.timeout;", o.Timeout))
+	if d.Multiplier != nil {
+		if *d.Multiplier == 0 {
+			err = appendError(err, fmt.Sprintf("integer set to 0: charts[%d].dimension[%d].multiplier;", chartIdx, dimIdx))
+		}
 	}
-	if o.MaxOIDs <= 0 {
-		err = appendError(err, fmt.Sprintf("invalid value(%d): max_request_size;", o.MaxOIDs))
+	if d.Divisor != nil {
+		if *d.Divisor == 0 {
+			err = appendError(err, fmt.Sprintf("integer set to 0: charts[%d].dimension[%d].divisor;", chartIdx, dimIdx))
+		}
 	}
-
 	return err
 }
 
-func (s SNMP) validateConfig() error {
-	var err error
-	err = nil
-	if s.ChartInput == nil {
-		err = appendError(err, "charts missing from config")
+func appendError(err error, msg string) error {
+	var e error
+	if err == nil {
+		if msg == "" {
+			return nil
+		}
+		e = errors.New(msg)
 	} else {
-		for i, chartIn := range s.ChartInput {
-			if e := chartIn.validateConfig(i); e != nil {
-				err = appendError(err, e.Error())
-			}
+		if msg == "" {
+			return err
 		}
+		e = fmt.Errorf("%w %s", err, msg)
 	}
-
-	if s.Options != nil {
-		if e := s.Options.validateConfig(); e != nil {
-			err = appendError(err, e.Error())
-		}
-		if s.Options.Version == 3 {
-			if s.User == nil {
-				err = appendError(err, "SNMP v3 missing user credentials;")
-			}
-		} else {
-			if s.Community == nil {
-				err = appendError(err, "SNMP v1/2 missing community value;")
-			}
-		}
-	}
-
-	if s.User != nil {
-		if e := s.User.validateConfig(); e != nil {
-			err = appendError(err, e.Error())
-		}
-	}
-
-	if s.Config.Hostname == "" {
-		err = appendError(err, "missing value: name;")
-	}
-
-	return err
+	return e
 }
