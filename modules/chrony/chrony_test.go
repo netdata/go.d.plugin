dc13ce398f3caf77eafb0fee7ba388d839131cf6
boxjan <i@boxjan.li>
2022-05-16 01:00:21 +0800

    fix up chrony testcase assert type

--- modules/chrony/chrony_test.go
+++ modules/chrony/chrony_test.go
@@ -42,15 +42,15 @@ func TestChrony_Collect(t *testing.T) {
 	// should have something in result
 	assert.NotNil(t, mod.Collect())
 	// chrony should be running
-	assert.Equal(t, 1, ans["running"])
+	assert.EqualValues(t, 1, ans["running"])
 	// in most cases, the leap second status should be 0
-	assert.Equal(t, 0, ans["leap_status"])
+	assert.EqualValues(t, 0, ans["leap_status"])
 
 	// should collect source server
 	assert.True(t, mod.Charts().Has("source"))
 	// if chrony syncs upstream normally, the source should not be 0.0.0.0
 	assert.False(t, mod.Charts().Get("source").HasDim(net.IPv4zero.String()))
 	// if chrony syncs upstream normally, should at least one online source
-	assert.NotEqual(t, 0, ans["online_sources"])
+	assert.NotEqualValues(t, 0, ans["online_sources"])
 
 }
