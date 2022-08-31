c2a0047e0c24168799a26690ab8649e9fa3d53d7
Ilya Mashchenko <ilya@netdata.cloud>
2022-08-31 15:29:05 +0300

    chore(portcheck): some style fixes (#789)

--- modules/portcheck/charts.go
+++ modules/portcheck/charts.go
@@ -8,51 +8,50 @@ import (
 	"github.com/netdata/go.d.plugin/agent/module"
 )
 
-type (
-	// Charts is an alias for module.Charts
-	Charts = module.Charts
-	// Dims is an alias for module.Dims
-	Dims = module.Dims
-)
+var chartsTmpl = module.Charts{
+	checkStatusChartTmpl.Copy(),
+	checkInStateDurationChartTmpl.Copy(),
+	checkConnectionLatencyChartTmpl.Copy(),
+}
 
-var portCharts = Charts{
-	{
-		ID:    "port_%d_status",
-		Title: "TCP Check Status",
-		Units: "boolean",
-		Fam:   "port %d",
-		Ctx:   "portcheck.status",
-		Dims: Dims{
-			{ID: "port_%d_success", Name: "success"},
-			{ID: "port_%d_failed", Name: "failed"},
-			{ID: "port_%d_timeout", Name: "timeout"},
-		},
-	},
-	{
-		ID:    "port_%d_current_state_duration",
-		Title: "Current State Duration",
-		Units: "seconds",
-		Fam:   "port %d",
-		Ctx:   "portcheck.state_duration",
-		Dims: Dims{
-			{ID: "port_%d_current_state_duration", Name: "time"},
-		},
-	},
-	{
-		ID:    "port_%d_connection_latency",
-		Title: "TCP Connection Latency",
-		Units: "ms",
-		Fam:   "port %d",
-		Ctx:   "portcheck.latency",
-		Dims: Dims{
-			{ID: "port_%d_latency", Name: "time"},
-		},
+var checkStatusChartTmpl = module.Chart{
+	ID:    "port_%d_status",
+	Title: "TCP Check Status",
+	Units: "boolean",
+	Fam:   "port %d",
+	Ctx:   "portcheck.status",
+	Dims: module.Dims{
+		{ID: "port_%d_success", Name: "success"},
+		{ID: "port_%d_failed", Name: "failed"},
+		{ID: "port_%d_timeout", Name: "timeout"},
 	},
 }
 
-func newPortCharts(port int) *Charts {
-	cs := portCharts.Copy()
-	for _, chart := range *cs {
+var checkInStateDurationChartTmpl = module.Chart{
+	ID:    "port_%d_current_state_duration",
+	Title: "Current State Duration",
+	Units: "seconds",
+	Fam:   "port %d",
+	Ctx:   "portcheck.state_duration",
+	Dims: module.Dims{
+		{ID: "port_%d_current_state_duration", Name: "time"},
+	},
+}
+
+var checkConnectionLatencyChartTmpl = module.Chart{
+	ID:    "port_%d_connection_latency",
+	Title: "TCP Connection Latency",
+	Units: "ms",
+	Fam:   "port %d",
+	Ctx:   "portcheck.latency",
+	Dims: module.Dims{
+		{ID: "port_%d_latency", Name: "time"},
+	},
+}
+
+func newPortCharts(port int) *module.Charts {
+	charts := chartsTmpl.Copy()
+	for _, chart := range *charts {
 		chart.ID = fmt.Sprintf(chart.ID, port)
 		chart.Fam = fmt.Sprintf(chart.Fam, port)
 
@@ -60,5 +59,5 @@ func newPortCharts(port int) *Charts {
 			dim.ID = fmt.Sprintf(dim.ID, port)
 		}
 	}
-	return cs
+	return charts
 }
