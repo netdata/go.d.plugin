e19a210898c980bd2b4e95055ab4525f6870aecc
Brian Flanigan <brian.flanigan@salesforce.com>
2022-10-07 21:51:12 -0400

    charts

--- modules/zfs/charts.go
+++ modules/zfs/charts.go
@@ -1,6 +1,9 @@
 package zfs
 
-import "github.com/netdata/go.d.plugin/agent/module"
+import (
+	"github.com/netdata/go.d.plugin/agent/module"
+	"github.com/netdata/go.d.plugin/logger"
+)
 
 func (z *ZFS) createcharts() *module.Charts {
 	chart := &module.Chart{
@@ -8,23 +11,23 @@ func (z *ZFS) createcharts() *module.Charts {
 		Title: "ZFS Free Space Fragmentation",
 		Units: "percent",
 		Fam:   "zfs",
+		Ctx:   "zfs.fragmentation",
 	}
 
 	for _, v := range z.pools {
 		d := module.Dim{
-			ID:   v,
-			Name: v,
+			ID: v,
 		}
 		err := chart.AddDim(&d)
 		if err != nil {
-			panic("failed to add dim to chart")
+			logger.Panicf("failed to add dim to chart: %v\n", err)
 		}
 	}
 
 	charts := &module.Charts{}
 	err := charts.Add(chart)
 	if err != nil {
-		panic("failed to create module.Charts")
+		logger.Panicf("failed to create module.Charts: %v\n", err)
 	}
 
 	return charts
