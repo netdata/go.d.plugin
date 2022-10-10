21b616cee9acd09cfbe77a499d6389d944e4eec2
Thiago Marques <thiagoftsm@gmail.com>
2022-10-10 01:35:39 +0000

    cassandra_module: Add garbage collection metrics

--- modules/cassandra/metrics.go
+++ modules/cassandra/metrics.go
@@ -3,10 +3,12 @@
 package cassandra
 
 type metrics struct {
-	throughput *THROUGHPUT `stm:"org_apache_cassandra_metrics_clientrequest_oneminuterate"`
-	latency    *LATENCY    `stm:"org_apache_cassandra_metrics_clientrequest_count"`
-	cache      *CACHE      `stm:"org_apache_cassandra_metrics_cache_count"`
-	disk       *DISK       `stm:"org_apache_cassandra_metrics_table_count"`
+	throughput *THROUGHPUT               `stm:"org_apache_cassandra_metrics_clientrequest_oneminuterate"`
+	latency    *LATENCY                  `stm:"org_apache_cassandra_metrics_clientrequest_count"`
+	cache      *CACHE                    `stm:"org_apache_cassandra_metrics_cache_count"`
+	disk       *DISK                     `stm:"org_apache_cassandra_metrics_table_count"`
+	gcc        *GARBAGE_COLLECTION_COUNT `stm:"jvm_gc_collection_seconds_count"`
+	gct        *GARBAGE_COLLECTION_TIME  `stm:"jvm_gc_collection_seconds_sum"`
 }
 
 func (c metrics) hasThrouput() bool { return c.throughput != nil }
@@ -31,5 +33,13 @@ type (
 		used                 float64 `stm:"TotalDiskSpaceUsed"`
 		compaction_completed float64 `stm:"CompactionBytesWritten"`
 		compaction_queue     float64 `stm:"PendingCompactions"`
+	}
+	GARBAGE_COLLECTION_COUNT struct {
+		parNewCount    int64 `stm:"ParNew"`
+		markSweepCount int64 `stm:"ConcurrentMarkSweep"`
+	}
+	GARBAGE_COLLECTION_TIME struct {
+		parNewTime    int64 `stm:"ParNew"`
+		markSweepTime int64 `stm:"ConcurrentMarkSweep"`
 	}
 )
