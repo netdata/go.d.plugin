7f79e5a0fe404b81ea1d70639381f4c9801e2d74
ilyam8 <ilya@netdata.cloud>
2022-10-18 13:30:08 +0300

    cassandra various fixes

--- modules/cassandra/charts.go
+++ modules/cassandra/charts.go
@@ -5,76 +5,78 @@ package cassandra
 import "github.com/netdata/go.d.plugin/agent/module"
 
 const (
-	prioRequests = module.Priority + iota
+	prioRequestsRate = module.Priority + iota
 
 	prioLatency
-
-	prioDroppedMessages
-	prioRequestsTimeouts
-	prioRequestsUnavailables
-	prioRequestsFailures
-	prioStorageExceptions
 
 	prioCacheHitRatio
 	prioCacheHitRate
 	prioCacheSize
 
-	prioStorageLoad
+	prioStorageLiveDiskSpaceUsed
+	prioStorageTotalDiskSpaceUsed
 
-	prioThreadPoolsActiveTasks
-	prioThreadPoolsPendingTasks
-	prioThreadPoolsBlockedTasks
-	prioThreadPoolsCurrentlyBlockedTasks
+	prioCompactionCompletedTasksRate
+	prioCompactionPendingTasksCount
+	prioCompactionBytesCompactedRate
 
-	prioCompactionCompletedTasks
-	prioCompactionPendingTasks
-	prioCompactionBytesCompacted
+	prioThreadPoolsActiveTasksCount
+	prioThreadPoolsPendingTasksCount
+	prioThreadPoolsBlockedTasksRate
+	prioThreadPoolsCurrentlyBlockedTasksCount
 
 	prioJVMGCCount
 	prioJVMGCTime
+
+	prioDroppedMessagesOneMinuteRate
+	prioRequestsTimeoutsRate
+	prioRequestsUnavailablesRate
+	prioRequestsFailuresRate
+	prioStorageExceptionsRate
 )
 
 var baseCharts = module.Charts{
-	chartClientRequests.Copy(),
+	chartClientRequestsRate.Copy(),
 
 	chartClientRequestsLatency.Copy(),
-
-	chartDroppedMessages.Copy(),
-	chartClientRequestTimeouts.Copy(),
-	chartClientRequestUnavailables.Copy(),
-	chartClientRequestFailures.Copy(),
-	chartStorageExceptions.Copy(),
 
 	chartCacheHitRatio.Copy(),
 	chartCacheHitRate.Copy(),
 	chartCacheSize.Copy(),
 
-	chartStorageLoad.Copy(),
+	chartStorageLiveDiskSpaceUsed.Copy(),
+	chartStorageTotalDiskSpaceUsed.Copy(),
 
-	threadPoolsActiveTasks.Copy(),
-	threadPoolsPendingTasks.Copy(),
-	threadPoolsBlockedTasks.Copy(),
-	threadPoolsCurrentlyBlockedTasks.Copy(),
+	chartCompactionCompletedTasksRate.Copy(),
+	chartCompactionPendingTasksCount.Copy(),
+	chartCompactionBytesCompactedRate.Copy(),
 
-	chartCompactionCompletedTasks.Copy(),
-	chartCompactionPendingTasks.Copy(),
-	chartCompactionBytesCompacted.Copy(),
+	threadPoolsActiveTasksCount.Copy(),
+	threadPoolsPendingTasksCount.Copy(),
+	threadPoolsBlockedTasksRate.Copy(),
+	threadPoolsBlockedTasksCount.Copy(),
 
-	chartJVMGCCount.Copy(),
+	chartJVMGCRate.Copy(),
 	chartJVMGCTime.Copy(),
+
+	chartDroppedMessagesOneMinuteRate.Copy(),
+	chartClientRequestTimeoutsRate.Copy(),
+	chartClientRequestUnavailablesRate.Copy(),
+	chartClientRequestFailuresRate.Copy(),
+	chartStorageExceptionsRate.Copy(),
 }
 
 var (
-	chartClientRequests = module.Chart{
-		ID:       "client_requests",
-		Title:    "Client requests",
+	chartClientRequestsRate = module.Chart{
+		ID:       "client_requests_rate",
+		Title:    "Client requests rate",
 		Units:    "requests/s",
 		Fam:      "throughput",
-		Ctx:      "cassandra.client_requests",
-		Priority: prioRequests,
+		Ctx:      "cassandra.client_requests_rate",
+		Priority: prioRequestsRate,
 		Dims: module.Dims{
-			{ID: "client_request_latency_reads", Name: "reads", Algo: module.Incremental, Div: 1000},
-			{ID: "client_request_latency_writes", Name: "writes", Algo: module.Incremental, Mul: -1, Div: 1000},
+			{ID: "client_request_latency_reads", Name: "reads", Algo: module.Incremental},
+			{ID: "client_request_latency_writes", Name: "writes", Algo: module.Incremental, Mul: -1},
 		},
 	}
 )
@@ -88,69 +90,8 @@ var (
 		Ctx:      "cassandra.client_requests_latency",
 		Priority: prioLatency,
 		Dims: module.Dims{
-			{ID: "client_request_total_latency_reads", Name: "read", Algo: module.Incremental, Div: 1000},
-			{ID: "client_request_total_latency_writes", Name: "write", Algo: module.Incremental, Mul: -1, Div: 1000},
-		},
-	}
-)
-
-var (
-	chartDroppedMessages = module.Chart{
-		ID:       "dropped_messages_one_minute_rate",
-		Title:    "Dropped messages",
-		Units:    "messages/s",
-		Fam:      "errors",
-		Ctx:      "cassandra.dropped_messages_one_minute_rate",
-		Priority: prioDroppedMessages,
-		Dims: module.Dims{
-			{ID: "dropped_messages_one_minute", Name: "dropped", Div: 1000},
-		},
-	}
-	chartClientRequestTimeouts = module.Chart{
-		ID:       "client_requests_timeouts",
-		Title:    "Client requests timeouts",
-		Units:    "timeouts/s",
-		Fam:      "errors",
-		Ctx:      "cassandra.client_requests_timeouts",
-		Priority: prioRequestsTimeouts,
-		Dims: module.Dims{
-			{ID: "client_request_timeouts_reads", Name: "read", Algo: module.Incremental, Div: 1000},
-			{ID: "client_request_timeouts_writes", Name: "write", Algo: module.Incremental, Mul: -1, Div: 1000},
-		},
-	}
-	chartClientRequestUnavailables = module.Chart{
-		ID:       "client_requests_unavailables",
-		Title:    "Client requests unavailable exceptions",
-		Units:    "exceptions/s",
-		Fam:      "errors",
-		Ctx:      "cassandra.client_requests_unavailables",
-		Priority: prioRequestsUnavailables,
-		Dims: module.Dims{
-			{ID: "client_request_unavailables_reads", Name: "read", Algo: module.Incremental, Div: 1000},
-			{ID: "client_request_unavailables_writes", Name: "write", Algo: module.Incremental, Mul: -1, Div: 1000},
-		},
-	}
-	chartClientRequestFailures = module.Chart{
-		ID:       "client_requests_failures",
-		Title:    "Client requests failures",
-		Units:    "failures/s",
-		Fam:      "errors",
-		Ctx:      "cassandra.client_requests_failures",
-		Priority: prioRequestsFailures,
-		Dims: module.Dims{
-			{ID: "client_request_failures_reads", Name: "read", Algo: module.Incremental, Div: 1000},
-			{ID: "client_request_failures_writes", Name: "write", Algo: module.Incremental, Mul: -1, Div: 1000},
-		},
-	}
-	chartStorageExceptions = module.Chart{
-		ID:       "storage_exceptions",
-		Title:    "Storage exceptions",
-		Units:    "exceptions",
-		Fam:      "errors",
-		Ctx:      "cassandra.storage_exceptions",
-		Priority: prioStorageExceptions,
-		Dims: module.Dims{
-			{ID: "storage_exceptions", Name: "storage", Algo: module.Incremental},
+			{ID: "client_request_total_latency_reads", Name: "read", Algo: module.Incremental},
+			{ID: "client_request_total_latency_writes", Name: "write", Algo: module.Incremental, Mul: -1},
 		},
 	}
 )
@@ -176,8 +117,8 @@ var (
 		Priority: prioCacheHitRate,
 		Type:     module.Stacked,
 		Dims: module.Dims{
-			{ID: "cache_hits", Name: "hits", Algo: module.Incremental, Div: 1000},
-			{ID: "cache_misses", Name: "misses", Algo: module.Incremental, Div: 1000},
+			{ID: "cache_hits", Name: "hits", Algo: module.Incremental},
+			{ID: "cache_misses", Name: "misses", Algo: module.Incremental},
 		},
 	}
 	chartCacheSize = module.Chart{
@@ -194,96 +135,60 @@ var (
 )
 
 var (
-	chartStorageLoad = module.Chart{
-		ID:       "storage_load",
-		Title:    "Disk space used by live data on a node",
+	chartStorageLiveDiskSpaceUsed = module.Chart{
+		ID:       "storage_live_disk_space_used",
+		Title:    "Disk space used by live data",
 		Units:    "bytes",
 		Fam:      "disk usage",
-		Ctx:      "cassandra.storage_load",
-		Priority: prioStorageLoad,
+		Ctx:      "cassandra.storage_live_disk_space_used",
+		Priority: prioStorageLiveDiskSpaceUsed,
 		Dims: module.Dims{
 			{ID: "storage_load", Name: "used"},
 		},
 	}
+	chartStorageTotalDiskSpaceUsed = module.Chart{
+		ID:       "storage_total_disk_space_used",
+		Title:    "Total disk space used",
+		Units:    "bytes",
+		Fam:      "disk usage",
+		Ctx:      "cassandra.storage_tables_total_disk_space_used",
+		Priority: prioStorageTotalDiskSpaceUsed,
+		Dims: module.Dims{
+			{ID: "tables_total_disk_space_used", Name: "used"},
+		},
+	}
 )
 
 var (
-	threadPoolsActiveTasks = module.Chart{
-		ID:       "thread_pools_active_tasks",
-		Title:    "Active tasks",
-		Units:    "tasks",
-		Fam:      "thread pools",
-		Ctx:      "cassandra.thread_pools_active_tasks",
-		Priority: prioThreadPoolsActiveTasks,
-		Dims: module.Dims{
-			{ID: "thread_pools_active_tasks", Name: "active"},
-		},
-	}
-	threadPoolsPendingTasks = module.Chart{
-		ID:       "thread_pools_pending_tasks",
-		Title:    "Pending tasks",
-		Units:    "tasks",
-		Fam:      "thread pools",
-		Ctx:      "cassandra.thread_pools_pending_tasks",
-		Priority: prioThreadPoolsPendingTasks,
-		Dims: module.Dims{
-			{ID: "thread_pools_pending_tasks", Name: "pending"},
-		},
-	}
-	threadPoolsBlockedTasks = module.Chart{
-		ID:       "thread_pools_blocked_tasks",
-		Title:    "Blocked tasks",
+	chartCompactionCompletedTasksRate = module.Chart{
+		ID:       "compaction_completed_tasks_rate",
+		Title:    "Completed compactions rate",
 		Units:    "tasks/s",
-		Fam:      "thread pools",
-		Ctx:      "cassandra.thread_pools_blocked_tasks",
-		Priority: prioThreadPoolsBlockedTasks,
-		Dims: module.Dims{
-			{ID: "thread_pools_total_blocked_tasks", Name: "blocked", Algo: module.Incremental},
-		},
-	}
-	threadPoolsCurrentlyBlockedTasks = module.Chart{
-		ID:       "thread_pools_currently_blocked_tasks",
-		Title:    "Blocked tasks",
-		Units:    "tasks",
-		Fam:      "thread pools",
-		Ctx:      "cassandra.thread_pools_currently_blocked_tasks",
-		Priority: prioThreadPoolsCurrentlyBlockedTasks,
-		Dims: module.Dims{
-			{ID: "thread_pools_currently_blocked_tasks", Name: "blocked"},
-		},
-	}
-)
-
-var (
-	chartCompactionCompletedTasks = module.Chart{
-		ID:       "compaction_completed_tasks",
-		Title:    "Pending compactions",
-		Units:    "tasks",
 		Fam:      "compaction",
-		Ctx:      "cassandra.compaction_completed_tasks",
-		Priority: prioCompactionCompletedTasks,
+		Ctx:      "cassandra.compaction_completed_tasks_rate",
+		Priority: prioCompactionCompletedTasksRate,
 		Dims: module.Dims{
 			{ID: "compaction_completed_tasks", Name: "completed", Algo: module.Incremental},
 		},
 	}
-	chartCompactionPendingTasks = module.Chart{
-		ID:       "compaction_pending_tasks",
+	chartCompactionPendingTasksCount = module.Chart{
+		ID:       "compaction_pending_tasks_count",
 		Title:    "Pending compactions",
 		Units:    "tasks",
 		Fam:      "compaction",
-		Ctx:      "cassandra.compaction_pending_tasks",
-		Priority: prioCompactionPendingTasks,
+		Ctx:      "cassandra.compaction_pending_tasks_count",
+		Priority: prioCompactionPendingTasksCount,
 		Dims: module.Dims{
 			{ID: "compaction_pending_tasks", Name: "pending"},
 		},
 	}
-	chartCompactionBytesCompacted = module.Chart{
-		ID:       "compaction_compacted",
-		Title:    "Compaction",
+	chartCompactionBytesCompactedRate = module.Chart{
+		ID:       "compaction_compacted_rate",
+		Title:    "Compaction data rate",
 		Units:    "bytes/s",
 		Fam:      "compaction",
-		Ctx:      "cassandra.compaction_compacted",
-		Priority: prioCompactionBytesCompacted,
+		Ctx:      "cassandra.compaction_compacted_rate",
+		Priority: prioCompactionBytesCompactedRate,
 		Dims: module.Dims{
 			{ID: "compaction_bytes_compacted", Name: "compacted", Algo: module.Incremental},
 		},
@@ -291,16 +196,63 @@ var (
 )
 
 var (
-	chartJVMGCCount = module.Chart{
-		ID:       "jvm_gc_count",
-		Title:    "Garbage collections",
+	threadPoolsActiveTasksCount = module.Chart{
+		ID:       "thread_pools_active_tasks_count",
+		Title:    "Active tasks",
+		Units:    "tasks",
+		Fam:      "thread pools",
+		Ctx:      "cassandra.thread_pools_active_tasks_count",
+		Priority: prioThreadPoolsActiveTasksCount,
+		Dims: module.Dims{
+			{ID: "thread_pools_active_tasks", Name: "active"},
+		},
+	}
+	threadPoolsPendingTasksCount = module.Chart{
+		ID:       "thread_pools_pending_tasks_count",
+		Title:    "Pending tasks",
+		Units:    "tasks",
+		Fam:      "thread pools",
+		Ctx:      "cassandra.thread_pools_pending_tasks_count",
+		Priority: prioThreadPoolsPendingTasksCount,
+		Dims: module.Dims{
+			{ID: "thread_pools_pending_tasks", Name: "pending"},
+		},
+	}
+	threadPoolsBlockedTasksRate = module.Chart{
+		ID:       "thread_pools_blocked_tasks_rate",
+		Title:    "Blocked tasks rate",
+		Units:    "tasks/s",
+		Fam:      "thread pools",
+		Ctx:      "cassandra.thread_pools_blocked_tasks_rate",
+		Priority: prioThreadPoolsBlockedTasksRate,
+		Dims: module.Dims{
+			{ID: "thread_pools_total_blocked_tasks", Name: "blocked", Algo: module.Incremental},
+		},
+	}
+	threadPoolsBlockedTasksCount = module.Chart{
+		ID:       "thread_pools_blocked_tasks_count",
+		Title:    "Blocked tasks",
+		Units:    "tasks",
+		Fam:      "thread pools",
+		Ctx:      "cassandra.thread_pools_blocked_tasks_count",
+		Priority: prioThreadPoolsCurrentlyBlockedTasksCount,
+		Dims: module.Dims{
+			{ID: "thread_pools_currently_blocked_tasks", Name: "blocked"},
+		},
+	}
+)
+
+var (
+	chartJVMGCRate = module.Chart{
+		ID:       "jvm_gc_rate",
+		Title:    "Garbage collections rate",
 		Units:    "gc/s",
 		Fam:      "jvm gc",
-		Ctx:      "cassandra.jvm_gc_count",
+		Ctx:      "cassandra.jvm_gc_rate",
 		Priority: prioJVMGCCount,
 		Dims: module.Dims{
-			{ID: "jvm_gc_parnew_count", Name: "parnew", Algo: module.Incremental, Div: 1000},
-			{ID: "jvm_gc_cms_count", Name: "cms", Algo: module.Incremental, Div: 1000},
+			{ID: "jvm_gc_parnew_count", Name: "parnew", Algo: module.Incremental},
+			{ID: "jvm_gc_cms_count", Name: "cms", Algo: module.Incremental},
 		},
 	}
 	chartJVMGCTime = module.Chart{
@@ -313,6 +265,67 @@ var (
 		Dims: module.Dims{
 			{ID: "jvm_gc_parnew_time", Name: "parnew", Algo: module.Incremental, Div: 1000},
 			{ID: "jvm_gc_cms_time", Name: "cms", Algo: module.Incremental, Div: 1000},
+		},
+	}
+)
+
+var (
+	chartDroppedMessagesOneMinuteRate = module.Chart{
+		ID:       "dropped_messages_one_minute_rate",
+		Title:    "Dropped messages one minute rate",
+		Units:    "messages/s",
+		Fam:      "errors",
+		Ctx:      "cassandra.dropped_messages_one_minute_rate",
+		Priority: prioDroppedMessagesOneMinuteRate,
+		Dims: module.Dims{
+			{ID: "dropped_messages_one_minute", Name: "dropped", Div: 1000},
+		},
+	}
+	chartClientRequestTimeoutsRate = module.Chart{
+		ID:       "client_requests_timeouts_rate",
+		Title:    "Client requests timeouts rate",
+		Units:    "timeouts/s",
+		Fam:      "errors",
+		Ctx:      "cassandra.client_requests_timeouts_rate",
+		Priority: prioRequestsTimeoutsRate,
+		Dims: module.Dims{
+			{ID: "client_request_timeouts_reads", Name: "read", Algo: module.Incremental},
+			{ID: "client_request_timeouts_writes", Name: "write", Algo: module.Incremental, Mul: -1},
+		},
+	}
+	chartClientRequestUnavailablesRate = module.Chart{
+		ID:       "client_requests_unavailables_rate",
+		Title:    "Client requests unavailable exceptions rate",
+		Units:    "exceptions/s",
+		Fam:      "errors",
+		Ctx:      "cassandra.client_requests_unavailables_rate",
+		Priority: prioRequestsUnavailablesRate,
+		Dims: module.Dims{
+			{ID: "client_request_unavailables_reads", Name: "read", Algo: module.Incremental},
+			{ID: "client_request_unavailables_writes", Name: "write", Algo: module.Incremental, Mul: -1},
+		},
+	}
+	chartClientRequestFailuresRate = module.Chart{
+		ID:       "client_requests_failures_rate",
+		Title:    "Client requests failures rate",
+		Units:    "failures/s",
+		Fam:      "errors",
+		Ctx:      "cassandra.client_requests_failures_rate",
+		Priority: prioRequestsFailuresRate,
+		Dims: module.Dims{
+			{ID: "client_request_failures_reads", Name: "read", Algo: module.Incremental},
+			{ID: "client_request_failures_writes", Name: "write", Algo: module.Incremental, Mul: -1},
+		},
+	}
+	chartStorageExceptionsRate = module.Chart{
+		ID:       "storage_exceptions_rate",
+		Title:    "Storage exceptions rate",
+		Units:    "exceptions/s",
+		Fam:      "errors",
+		Ctx:      "cassandra.storage_exceptions_rate",
+		Priority: prioStorageExceptionsRate,
+		Dims: module.Dims{
+			{ID: "storage_exceptions", Name: "storage", Algo: module.Incremental},
 		},
 	}
 )
