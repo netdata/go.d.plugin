27d84cef056ef58d777f369597a05f515576d12a
Thiago Marques <thiagoftsm@gmail.com>
2022-10-28 23:54:14 +0000

    wmi_process: Add another missing metric

--- modules/wmi/wmi_test.go
+++ modules/wmi/wmi_test.go
@@ -710,7 +710,7 @@ func TestWMI_Collect(t *testing.T) {
 				"os_visible_memory_bytes":                                       4244140032000,
 				"os_visible_memory_used_bytes":                                  2306338816000,
 				"process_collection_duration":                                   1056,
-				"process_collection_success":                                    0,
+				"process_collection_success":                                    1,
 				"service_collection_duration":                                   0,
 				"service_collection_success":                                    1,
 				"system_boot_time":                                              0,
