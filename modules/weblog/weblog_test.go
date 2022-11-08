8d170b5d7e598e83267ca5b3c1ae9ae89a42d217
Thiago Marques <thiagoftsm@gmail.com>
2022-11-08 17:22:51 +0000

    web_log_iis: Add initial tests

--- modules/weblog/weblog_test.go
+++ modules/weblog/weblog_test.go
@@ -24,6 +24,7 @@ var (
 	testFullLog, _            = os.ReadFile("testdata/full.log")
 	testCustomLog, _          = os.ReadFile("testdata/custom.log")
 	testCustomTimeFieldLog, _ = os.ReadFile("testdata/custom_time_fields.log")
+	testIISLog, _             = os.ReadFile("testdata/u_ex221107.log")
 )
 
 func Test_readTestData(t *testing.T) {
@@ -31,6 +32,7 @@ func Test_readTestData(t *testing.T) {
 	assert.NotNil(t, testCommonLog)
 	assert.NotNil(t, testCustomLog)
 	assert.NotNil(t, testCustomTimeFieldLog)
+	assert.NotNil(t, testIISLog)
 }
 
 func TestNew(t *testing.T) {
@@ -556,6 +558,71 @@ func TestWebLog_Collect_CustomTimeFieldsLogs(t *testing.T) {
 	mx := weblog.Collect()
 	assert.Equal(t, expected, mx)
 	testCharts(t, weblog, mx)
+}
+
+func TestWebLog_IISLogs(t *testing.T) {
+	weblog := prepareWebLogCollectIISFields(t)
+
+	expected := map[string]int64{
+		"bytes_received":                    0,
+		"bytes_sent":                        0,
+		"req_http_scheme":                   0,
+		"req_https_scheme":                  0,
+		"req_ipv4":                          0,
+		"req_ipv6":                          0,
+		"req_proc_time_avg":                 0,
+		"req_proc_time_count":               0,
+		"req_proc_time_hist_bucket_1":       0,
+		"req_proc_time_hist_bucket_10":      0,
+		"req_proc_time_hist_bucket_11":      0,
+		"req_proc_time_hist_bucket_2":       0,
+		"req_proc_time_hist_bucket_3":       0,
+		"req_proc_time_hist_bucket_4":       0,
+		"req_proc_time_hist_bucket_5":       0,
+		"req_proc_time_hist_bucket_6":       0,
+		"req_proc_time_hist_bucket_7":       0,
+		"req_proc_time_hist_bucket_8":       0,
+		"req_proc_time_hist_bucket_9":       0,
+		"req_proc_time_hist_count":          0,
+		"req_proc_time_hist_sum":            0,
+		"req_proc_time_max":                 0,
+		"req_proc_time_min":                 0,
+		"req_proc_time_sum":                 0,
+		"req_type_bad":                      0,
+		"req_type_error":                    0,
+		"req_type_redirect":                 0,
+		"req_type_success":                  0,
+		"req_unmatched":                     72,
+		"requests":                          72,
+		"resp_1xx":                          0,
+		"resp_2xx":                          0,
+		"resp_3xx":                          0,
+		"resp_4xx":                          0,
+		"resp_5xx":                          0,
+		"uniq_ipv4":                         0,
+		"uniq_ipv6":                         0,
+		"upstream_resp_time_avg":            0,
+		"upstream_resp_time_count":          0,
+		"upstream_resp_time_hist_bucket_1":  0,
+		"upstream_resp_time_hist_bucket_10": 0,
+		"upstream_resp_time_hist_bucket_11": 0,
+		"upstream_resp_time_hist_bucket_2":  0,
+		"upstream_resp_time_hist_bucket_3":  0,
+		"upstream_resp_time_hist_bucket_4":  0,
+		"upstream_resp_time_hist_bucket_5":  0,
+		"upstream_resp_time_hist_bucket_6":  0,
+		"upstream_resp_time_hist_bucket_7":  0,
+		"upstream_resp_time_hist_bucket_8":  0,
+		"upstream_resp_time_hist_bucket_9":  0,
+		"upstream_resp_time_hist_count":     0,
+		"upstream_resp_time_hist_sum":       0,
+		"upstream_resp_time_max":            0,
+		"upstream_resp_time_min":            0,
+		"upstream_resp_time_sum":            0,
+	}
+
+	mx := weblog.Collect()
+	assert.Equal(t, expected, mx)
 }
 
 func testCharts(t *testing.T, w *WebLog, mx map[string]int64) {
@@ -1155,6 +1222,56 @@ func prepareWebLogCollectCustomTimeFields(t *testing.T) *WebLog {
 		Histogram:      nil,
 		GroupRespCodes: false,
 	}
+	weblog := New()
+	weblog.Config = cfg
+	require.True(t, weblog.Init())
+	require.True(t, weblog.Check())
+	defer weblog.Cleanup()
+
+	p, err := logs.NewCSVParser(weblog.Parser.CSV, bytes.NewReader(testCustomTimeFieldLog))
+	require.NoError(t, err)
+	weblog.parser = p
+	return weblog
+}
+
+func prepareWebLogCollectIISFields(t *testing.T) *WebLog {
+	t.Helper()
+	format := strings.Join([]string{
+		"-",               // date
+		"-",               // time
+		"$host",           // s-ip
+		"$request_method", // cs-method
+		"$request_uri",    // cs-uri-stem
+		"-",               // cs-uri-query
+		"$server_port",    // s-port
+		"-",               // cs-username
+		"$remote_addr",    // c-ip
+		"-",               // cs(User-Agent)
+		"-",               // cs(Referer)
+		"$status",         // sc-status
+		"-",               // sc-substatus
+		"-",               // sc-win32-status
+		"$request_time",   // time-taken
+	}, " ")
+	cfg := Config{
+		Parser: logs.ParserConfig{
+			LogType: logs.TypeCSV,
+			CSV: logs.CSVConfig{
+				// Users can define number of fields
+				FieldsPerRecord:  -1,
+				Delimiter:        " ",
+				TrimLeadingSpace: false,
+				Format:           format,
+				CheckField:       checkCSVFormatField,
+			},
+		},
+		Path:           "testdata/u_ex221107.log",
+		ExcludePath:    "",
+		URLPatterns:    nil,
+		Histogram:      nil,
+		GroupRespCodes: false,
+	}
+
 	weblog := New()
 	weblog.Config = cfg
 	require.True(t, weblog.Init())
