ed9b270cea75fc07f2ed5dc7e197aa52adcfb15b
ilyam8 <ilya@netdata.cloud>
2022-11-22 21:37:17 +0200

    fix(logstatsh): remove pipeline instance from family and add labels

--- modules/logstash/logstash.go
+++ modules/logstash/logstash.go
@@ -3,98 +3,74 @@
 package logstash
 
 import (
-	"errors"
+	"net/http"
 	"time"
 
-	"github.com/netdata/go.d.plugin/pkg/web"
-
 	"github.com/netdata/go.d.plugin/agent/module"
+	"github.com/netdata/go.d.plugin/pkg/web"
 )
 
 func init() {
-	creator := module.Creator{
+	module.Register("logstash", module.Creator{
 		Create: func() module.Module { return New() },
-	}
-
-	module.Register("logstash", creator)
+	})
 }
 
-// New creates Logstash with default values.
 func New() *Logstash {
-	config := Config{
-		HTTP: web.HTTP{
-			Request: web.Request{
-				URL: "http://localhost:9600",
-			},
-			Client: web.Client{
-				Timeout: web.Duration{Duration: time.Second},
+	return &Logstash{
+		Config: Config{
+			HTTP: web.HTTP{
+				Request: web.Request{
+					URL: "http://localhost:9600",
+				},
+				Client: web.Client{
+					Timeout: web.Duration{Duration: time.Second},
+				},
 			},
 		},
-	}
-	return &Logstash{
-		Config:             config,
-		charts:             charts.Copy(),
-		collectedPipelines: make(map[string]bool),
+		charts:    charts.Copy(),
+		pipelines: make(map[string]bool),
 	}
 }
 
-type (
-	// Config is the Logstash module configuration.
-	Config struct {
-		web.HTTP `yaml:",inline"`
-	}
-	Logstash struct {
-		module.Base
-		Config             `yaml:",inline"`
-		client             *client
-		charts             *Charts
-		collectedPipelines map[string]bool
-	}
-)
-
-func (l *Logstash) validateConfig() error {
-	if l.URL == "" {
-		return errors.New("URL not set")
-	}
-	return nil
+type Config struct {
+	web.HTTP `yaml:",inline"`
 }
 
-func (l *Logstash) createClient() error {
-	client, err := web.NewHTTPClient(l.Client)
-	if err != nil {
-		return err
-	}
-	l.client = newClient(client, l.Request)
-	return nil
+type Logstash struct {
+	module.Base
+	Config     `yaml:",inline"`
+	httpClient *http.Client
+	charts     *module.Charts
+	pipelines  map[string]bool
 }
 
-// Init makes initialization.
 func (l *Logstash) Init() bool {
-	if err := l.validateConfig(); err != nil {
-		l.Errorf("error on validating config: %v", err)
+	if l.URL == "" {
+		l.Error("config validation: 'url' cannot be empty")
 		return false
 	}
-	if err := l.createClient(); err != nil {
-		l.Errorf("error on creating client: %v", err)
+
+	httpClient, err := web.NewHTTPClient(l.Client)
+	if err != nil {
+		l.Errorf("init HTTP client: %v", err)
 		return false
 	}
+	l.httpClient = httpClient
 
 	l.Debugf("using URL %s", l.URL)
 	l.Debugf("using timeout: %s", l.Timeout.Duration)
 	return true
 }
 
-// Check makes check.
 func (l *Logstash) Check() bool {
 	return len(l.Collect()) > 0
 }
 
-// Charts creates Charts.
-func (l *Logstash) Charts() *Charts {
+func (l *Logstash) Charts() *module.Charts {
 	return l.charts
 }
 
-// Collect collects metrics.
 func (l *Logstash) Collect() map[string]int64 {
 	mx, err := l.collect()
 	if err != nil {
@@ -107,5 +83,8 @@ func (l *Logstash) Collect() map[string]int64 {
 	return mx
 }
 
-// Cleanup makes cleanup.
-func (Logstash) Cleanup() {}
+func (l *Logstash) Cleanup() {
+	if l.httpClient != nil {
+		l.httpClient.CloseIdleConnections()
+	}
+}
