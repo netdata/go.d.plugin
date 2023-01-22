b0ce8950af17cb832f037c6886d8461b92cf0602
Thiago Marques <thiagoftsm@gmail.com>
2023-01-22 23:14:09 +0000

    dotnet_metrics: Add collection

--- modules/wmi/collect_netframework.go
+++ modules/wmi/collect_netframework.go
@@ -25,6 +25,12 @@ const (
 	metricNetFrameworkCLRLoadingAssembliesLoaded  = "windows_netframework_clrloading_assemblies_loaded_total"
 	metricNetFrameworkCLRLoadingClassesLoaded     = "windows_netframework_clrloading_classes_loaded_total"
 	metricNetFrameworkCLRLoadingClassLoadFailure  = "windows_netframework_clrloading_class_load_failures_total"
+
+	metricNetFrameworkCLRLocksAndThreadsQueueLengthTotal      = "windows_netframework_clrlocksandthreads_queue_length_total"
+	metricNetFrameworkCLRLocksAndThreadsCurrentLogicalThreads = "windows_netframework_clrlocksandthreads_current_logical_threads"
+	metricNetFrameworkCLRLocksAndThreadsPhysicalThreads       = "windows_netframework_clrlocksandthreads_physical_threads_current"
+	metricNetFrameworkCLRLocksAndThreadsRecognizedThreads     = "windows_netframework_clrlocksandthreads_recognized_threads_total"
+	metricNetFrameworkCLRLocksAndThreadsContentions           = "windows_netframework_clrlocksandthreads_contentions_total"
 )
 
 func (w *WMI) collectNetFrameworkCLR(mx map[string]int64, pms prometheus.Series) {
@@ -151,6 +157,41 @@ func (w *WMI) collectNetFrameworkCLR(mx map[string]int64, pms prometheus.Series)
 		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
 			seen[name] = true
 			mx[px+"loading_"+name+"_class_load_failure"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRLocksAndThreadsQueueLengthTotal) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"lockandthreads_"+name+"_queue_length"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRLocksAndThreadsCurrentLogicalThreads) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"lockandthreads_"+name+"_current_logical_threads"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRLocksAndThreadsPhysicalThreads) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"lockandthreads_"+name+"_current_physical_threads"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRLocksAndThreadsRecognizedThreads) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"lockandthreads_"+name+"_recognized_threads"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRLocksAndThreadsContentions) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"lockandthreads_"+name+"_contentions"] += int64(pm.Value)
 		}
 	}
 
