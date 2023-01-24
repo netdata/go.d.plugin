0aa24b6e34764551171cbabc3f4c0a1670f75d5f
Thiago Marques <thiagoftsm@gmail.com>
2023-01-24 23:57:38 +0000

    dotnet_metrics: Adjust collection

--- modules/wmi/collect_netframework.go
+++ modules/wmi/collect_netframework.go
@@ -31,6 +31,19 @@ const (
 	metricNetFrameworkCLRLocksAndThreadsPhysicalThreads       = "windows_netframework_clrlocksandthreads_physical_threads_current"
 	metricNetFrameworkCLRLocksAndThreadsRecognizedThreads     = "windows_netframework_clrlocksandthreads_recognized_threads_total"
 	metricNetFrameworkCLRLocksAndThreadsContentions           = "windows_netframework_clrlocksandthreads_contentions_total"
+
+	metricNetFrameworkCLRMemoryAllocatedBytesTotal   = "windows_netframework_clrmemory_allocated_bytes_total"
+	metricNetFrameworkCLRMemoryFinalizationSurvivors = "windows_netframework_clrmemory_finalization_survivors"
+	metricNetFrameworkCLRMemoryHeapSizeBytes         = "windows_netframework_clrmemory_heap_size_bytes"
+	metricNetFrameworkCLRMemoryPromotedBytes         = "windows_netframework_clrmemory_promoted_bytes"
+	metricNetFrameworkCLRMemoryNumberGCHandles       = "windows_netframework_clrmemory_number_gc_handles"
+	metricNetFrameworkCLRMemoryCollectionsTotal      = "windows_netframework_clrmemory_collections_total"
+	metricNetFrameworkCLRMemoryInducedGCTotal        = "windows_netframework_clrmemory_induced_gc_total"
+	metricNetFrameworkCLRMemoryNumberPinnedObjects   = "windows_netframework_clrmemory_number_pinned_objects"
+	metricNetFrameworkCLRMemoryNumberSinkBlockInUse  = "windows_netframework_clrmemory_number_sink_blocksinuse"
+	metricNetFrameworkCLRMemoryCommittedBytes        = "windows_netframework_clrmemory_committed_bytes"
+	metricNetFrameworkCLRMemoryReservedBytes         = "windows_netframework_clrmemory_reserved_bytes"
+	metricNetFrameworkCLRMemoryGCTimePecent          = "windows_netframework_clrmemory_gc_time_percent"
 )
 
 func (w *WMI) collectNetFrameworkCLR(mx map[string]int64, pms prometheus.Series) {
@@ -195,12 +208,97 @@ func (w *WMI) collectNetFrameworkCLR(mx map[string]int64, pms prometheus.Series)
 		}
 	}
 
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryAllocatedBytesTotal) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_allocated_bytes_total"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryFinalizationSurvivors) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_finalization_survivors"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryHeapSizeBytes) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_heap_size"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryPromotedBytes) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_promoted"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryNumberGCHandles) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_gc_handles"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryCollectionsTotal) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_collection"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryInducedGCTotal) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_induced_gc_total"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryNumberPinnedObjects) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_pinned_objects"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryNumberSinkBlockInUse) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_sink_block_in_use"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryCommittedBytes) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_committed"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryReservedBytes) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_reserved"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRMemoryGCTimePecent) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"memory_"+name+"_gc_time"] += int64(pm.Value)
+		}
+	}
+
 	for proc := range seen {
 		if !w.cache.netFrameworkCLR[proc] {
 			w.cache.netFrameworkCLR[proc] = true
 			w.addProcessToNetFrameworkCharts(proc)
 		}
 	}
+
 	for proc := range w.cache.netFrameworkCLR {
 		if !seen[proc] {
 			delete(w.cache.netFrameworkCLR, proc)
