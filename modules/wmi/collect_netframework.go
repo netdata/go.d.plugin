8cfc2980ba66ad5331f23a4339ed47bde08953b0
Thiago Marques <thiagoftsm@gmail.com>
2023-01-25 12:46:34 +0000

    dotnet_metrics: Add collection

--- modules/wmi/collect_netframework.go
+++ modules/wmi/collect_netframework.go
@@ -51,6 +51,11 @@ const (
 	metricNetFrameworkCLRRemotingContextProxiesTotal       = "windows_netframework_clrremoting_context_proxies_total"
 	metricNetFrameworkCLRRemotingContexts                  = "windows_netframework_clrremoting_contexts"
 	metricNetFrameworkCLRRemotingRemoteCallsTotal          = "windows_netframework_clrremoting_remote_calls_total"
+
+	metricNetFrameworkCLRSecurityLinkTimeChecksTotal = "windows_netframework_clrsecurity_link_time_checks_total"
+	metricNetFrameworkCLRSecurityRTChecksTimePercent = "windows_netframework_clrsecurity_rt_checks_time_percent"
+	metricNetFrameworkCLRSecurityStackWalkDepth      = "windows_netframework_clrsecurity_stack_walk_depth"
+	metricNetFrameworkCLRSecurityRuntimeChecksTotal  = "windows_netframework_clrsecurity_runtime_checks_total"
 )
 
 func (w *WMI) collectNetFrameworkCLR(mx map[string]int64, pms prometheus.Series) {
@@ -338,6 +343,34 @@ func (w *WMI) collectNetFrameworkCLR(mx map[string]int64, pms prometheus.Series)
 		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
 			seen[name] = true
 			mx[px+"remoting_"+name+"_calls"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRSecurityLinkTimeChecksTotal) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"security_"+name+"_link_time_checks"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRSecurityRTChecksTimePercent) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"security_"+name+"_checks_time"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRSecurityStackWalkDepth) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"security_"+name+"_stack_walk_depth"] += int64(pm.Value)
+		}
+	}
+
+	for _, pm := range pms.FindByName(metricNetFrameworkCLRSecurityRuntimeChecksTotal) {
+		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
+			seen[name] = true
+			mx[px+"security_"+name+"_runtime_checks"] += int64(pm.Value)
 		}
 	}
 
