c56d798e26de9e1fdbc1c5482646353d5b64396d
Thiago Marques <thiagoftsm@gmail.com>
2023-02-02 00:34:21 +0000

    dotnet_metrics: Cleanup and prepare exceptions

--- modules/wmi/collect_netframework_clrmemory.go
+++ modules/wmi/collect_netframework_clrmemory.go
@@ -5,11 +5,6 @@ package wmi
 import "github.com/netdata/go.d.plugin/pkg/prometheus"
 
 const (
-	metricNetFrameworkCLRExceptionsThrown          = "windows_netframework_clrexceptions_exceptions_thrown_total"
-	metricNetFrameworkCLRExceptionsFilters         = "windows_netframework_clrexceptions_exceptions_filters_total"
-	metricNetFrameworkCLRExceptionsFinallys        = "windows_netframework_clrexceptions_exceptions_finallys_total"
-	metricNetFrameworkCLRExceptionsThrowCatchDepth = "windows_netframework_clrexceptions_throw_to_catch_depth_total"
-
 	metricNetFrameworkCLRInteropComCallableWrapper = "windows_netframework_clrinterop_com_callable_wrappers_total"
 	metricNetFrameworkCLRInteropMarshalling        = "windows_netframework_clrinterop_interop_marshalling_total"
 	metricNetFrameworkCLRInteropStubsCreated       = "windows_netframework_clrinterop_interop_stubs_created_total"
@@ -66,34 +61,6 @@ func (w *WMI) collectNetFrameworkCLR(mx map[string]int64, pms prometheus.Series)
 
 	seen := make(map[string]bool)
 	px := "netframework_clr"
-	for _, pm := range pms.FindByName(metricNetFrameworkCLRExceptionsThrown) {
-		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
-			seen[name] = true
-			mx[px+"exceptions_"+name+"_thrown"] += int64(pm.Value)
-		}
-	}
-
-	for _, pm := range pms.FindByName(metricNetFrameworkCLRExceptionsFilters) {
-		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
-			seen[name] = true
-			mx[px+"exceptions_"+name+"_filters"] += int64(pm.Value)
-		}
-	}
-
-	for _, pm := range pms.FindByName(metricNetFrameworkCLRExceptionsFinallys) {
-		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
-			seen[name] = true
-			mx[px+"exceptions_"+name+"_finallys"] += int64(pm.Value)
-		}
-	}
-
-	for _, pm := range pms.FindByName(metricNetFrameworkCLRExceptionsThrowCatchDepth) {
-		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
-			seen[name] = true
-			mx[px+"exceptions_"+name+"_throw_catch_depth"] += int64(pm.Value)
-		}
-	}
-
 	for _, pm := range pms.FindByName(metricNetFrameworkCLRInteropComCallableWrapper) {
 		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
 			seen[name] = true
