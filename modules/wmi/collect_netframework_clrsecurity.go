f16c4fafaa9488c8044aecd5fd48ebdd4a2caec0
Thiago Marques <thiagoftsm@gmail.com>
2023-02-02 03:27:15 +0000

    dotnet_metrics: Cleanup and prepare Security

--- modules/wmi/collect_netframework_clrsecurity.go
+++ modules/wmi/collect_netframework_clrsecurity.go
@@ -11,53 +11,53 @@ const (
 	metricNetFrameworkCLRSecurityRuntimeChecksTotal  = "windows_netframework_clrsecurity_runtime_checks_total"
 )
 
-func (w *WMI) collectNetFrameworkCLR(mx map[string]int64, pms prometheus.Series) {
+func (w *WMI) collectNetFrameworkCLRSecuriting(mx map[string]int64, pms prometheus.Series) {
 	if !w.cache.collection[collectorNetFrameworkCLRExceptions] {
 		w.cache.collection[collectorNetFrameworkCLRExceptions] = true
 		w.addNetFrameworkCRLExceptions()
 	}
 
 	seen := make(map[string]bool)
-	px := "netframework_clr"
+	px := "netframework_clrsecurity_"
 	for _, pm := range pms.FindByName(metricNetFrameworkCLRSecurityLinkTimeChecksTotal) {
 		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
 			seen[name] = true
-			mx[px+"security_"+name+"_link_time_checks"] += int64(pm.Value)
+			mx[px+name+"_link_time_checks"] += int64(pm.Value)
 		}
 	}
 
 	for _, pm := range pms.FindByName(metricNetFrameworkCLRSecurityRTChecksTimePercent) {
 		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
 			seen[name] = true
-			mx[px+"security_"+name+"_checks_time"] += int64(pm.Value)
+			mx[px+name+"_checks_time"] += int64(pm.Value)
 		}
 	}
 
 	for _, pm := range pms.FindByName(metricNetFrameworkCLRSecurityStackWalkDepth) {
 		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
 			seen[name] = true
-			mx[px+"security_"+name+"_stack_walk_depth"] += int64(pm.Value)
+			mx[px+name+"_stack_walk_depth"] += int64(pm.Value)
 		}
 	}
 
 	for _, pm := range pms.FindByName(metricNetFrameworkCLRSecurityRuntimeChecksTotal) {
 		if name := cleanProcessName(pm.Labels.Get("process")); name != "" {
 			seen[name] = true
-			mx[px+"security_"+name+"_runtime_checks"] += int64(pm.Value)
+			mx[px+name+"_runtime_checks"] += int64(pm.Value)
 		}
 	}
 
 	for proc := range seen {
-		if !w.cache.netFrameworkCLRExceptions[proc] {
-			w.cache.netFrameworkCLRExceptions[proc] = true
-			w.addProcessToNetFrameworkCharts(proc)
+		if !w.cache.netFrameworkCLRSecurity[proc] {
+			w.cache.netFrameworkCLRSecurity[proc] = true
+			w.addProcessToNetFrameworkSecurityCharts(proc)
 		}
 	}
 
-	for proc := range w.cache.netFrameworkCLRExceptions {
+	for proc := range w.cache.netFrameworkCLRSecurity {
 		if !seen[proc] {
-			delete(w.cache.netFrameworkCLRExceptions, proc)
-			w.removeProcessFromNetFrameworkCharts(proc)
+			delete(w.cache.netFrameworkCLRSecurity, proc)
+			w.removeProcessFromNetFrameworkSecuriyCharts(proc)
 		}
 	}
 }
