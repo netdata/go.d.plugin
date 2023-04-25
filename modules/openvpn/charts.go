5e32918316dc879a3c41284bdfa8257ebc4cfde9
ilyam8 <ilya@netdata.cloud>
2023-04-25 17:09:32 +0300

    update openvpn

--- modules/openvpn/charts.go
+++ modules/openvpn/charts.go
@@ -41,7 +41,7 @@ var userCharts = Charts{
 		ID:    "%s_user_traffic",
 		Title: "User Traffic",
 		Units: "kilobits/s",
-		Fam:   "user %s",
+		Fam:   "user traffic",
 		Ctx:   "openvpn.user_traffic",
 		Type:  module.Area,
 		Dims: Dims{
@@ -53,7 +53,7 @@ var userCharts = Charts{
 		ID:    "%s_user_connection_time",
 		Title: "User Connection Time",
 		Units: "seconds",
-		Fam:   "user %s",
+		Fam:   "user conn time",
 		Ctx:   "openvpn.user_connection_time",
 		Dims: Dims{
 			{ID: "%s_connection_time", Name: "time"},
--- modules/openvpn/collect.go
+++ modules/openvpn/collect.go
@@ -80,11 +80,9 @@ func (o *OpenVPN) addUserCharts(userName string) error {
 
 	for _, chart := range *cs {
 		chart.ID = fmt.Sprintf(chart.ID, userName)
-		chart.Fam = fmt.Sprintf(chart.Fam, userName)
 		chart.Labels = []module.Label{
 			{Key: "username", Value: userName},
 		}
-
 		for _, dim := range chart.Dims {
 			dim.ID = fmt.Sprintf(dim.ID, userName)
 		}
