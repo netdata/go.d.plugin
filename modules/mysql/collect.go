5ee7c969849788e102a3544d089635717eb963aa
chpfm <sysleg.fm@gmail.com>
2023-09-26 20:01:25 +0300

    Do not stop collecting mysql statistics because of timeout (#1346)

--- modules/mysql/collect.go
+++ modules/mysql/collect.go
@@ -5,6 +5,7 @@ package mysql
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -68,17 +69,15 @@ func (m *MySQL) collect() (map[string]int64, error) {
 	// TODO: perhaps make a decisions based on privileges? (SHOW GRANTS FOR CURRENT_USER();)
 	if m.doSlaveStatus {
 		if err := m.collectSlaveStatus(mx); err != nil {
-			m.Errorf("error on collecting slave status: %v", err)
-			// TODO: shouldn't disable on any error
-			m.doSlaveStatus = false
+			m.Warningf("error on collecting slave status: %v", err)
+			m.doSlaveStatus = errors.Is(err, context.DeadlineExceeded)
 		}
 	}
 
 	if m.doUserStatistics {
 		if err := m.collectUserStatistics(mx); err != nil {
-			m.Errorf("error on collecting user statistics: %v", err)
-			// TODO: shouldn't disable on any error
-			m.doUserStatistics = false
+			m.Warningf("error on collecting user statistics: %v", err)
+			m.doUserStatistics = errors.Is(err, context.DeadlineExceeded)
 		}
 	}
 
