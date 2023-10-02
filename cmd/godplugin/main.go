5eff66e3670242cf8a98ecef62cd03e74faab929
ilyam8 <ilya@netdata.cloud>
2023-10-02 16:01:41 +0300

    respect NETDATA_LOG_SEVERITY_LEVEL

--- cmd/godplugin/main.go
+++ cmd/godplugin/main.go
@@ -21,13 +21,14 @@ import (
 )
 
 var (
-	cd, _     = os.Getwd()
-	name      = "go.d"
-	userDir   = os.Getenv("NETDATA_USER_CONFIG_DIR")
-	stockDir  = os.Getenv("NETDATA_STOCK_CONFIG_DIR")
-	varLibDir = os.Getenv("NETDATA_LIB_DIR")
-	lockDir   = os.Getenv("NETDATA_LOCK_DIR")
-	watchPath = os.Getenv("NETDATA_PLUGINS_GOD_WATCH_PATH")
+	cd, _               = os.Getwd()
+	name                = "go.d"
+	userDir             = os.Getenv("NETDATA_USER_CONFIG_DIR")
+	stockDir            = os.Getenv("NETDATA_STOCK_CONFIG_DIR")
+	varLibDir           = os.Getenv("NETDATA_LIB_DIR")
+	lockDir             = os.Getenv("NETDATA_LOCK_DIR")
+	watchPath           = os.Getenv("NETDATA_PLUGINS_GOD_WATCH_PATH")
+	envLogSeverityLevel = os.Getenv("NETDATA_LOG_SEVERITY_LEVEL")
 
 	version = "unknown"
 )
@@ -94,6 +95,10 @@ func main() {
 	if opts.Version {
 		fmt.Printf("go.d.plugin, version: %s\n", version)
 		return
+	}
+
+	if envLogSeverityLevel != "" {
+		logger.SetSeverityByName(envLogSeverityLevel)
 	}
 
 	if opts.Debug {
