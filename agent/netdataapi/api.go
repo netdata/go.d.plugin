d9eb24b3caec85567449f7b72552a28d90496037
ilyam8 <ilya@netdata.cloud>
2023-10-07 19:09:03 +0300

    ndapi: improve perfomance of most used calls

--- agent/netdataapi/api.go
+++ agent/netdataapi/api.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"strconv"
 )
 
 type (
@@ -14,6 +15,14 @@ type (
 	API struct {
 		io.Writer
 	}
+)
+
+const quotes = "' '"
+
+var (
+	end          = []byte("END\n\n")
+	clabelCommit = []byte("CLABEL_COMMIT\n\n")
+	newLine      = []byte("\n")
 )
 
 func New(w io.Writer) *API { return &API{w} }
@@ -33,8 +42,19 @@ func (a *API) CHART(
 	options string,
 	plugin string,
 	module string) error {
-	_, err := fmt.Fprintf(a, "CHART '%s.%s' '%s' '%s' '%s' '%s' '%s' '%s' '%d' '%d' '%s' '%s' '%s'\n",
-		typeID, ID, name, title, units, family, context, chartType, priority, updateEvery, options, plugin, module)
+	_, err := a.Write([]byte("CHART " + "'" +
+		typeID + "." + ID + quotes +
+		name + quotes +
+		title + quotes +
+		units + quotes +
+		family + quotes +
+		context + quotes +
+		chartType + quotes +
+		strconv.Itoa(priority) + quotes +
+		strconv.Itoa(updateEvery) + quotes +
+		options + quotes +
+		plugin + quotes +
+		module + "'\n"))
 	return err
 }
 
@@ -46,72 +66,74 @@ func (a *API) DIMENSION(
 	multiplier int,
 	divisor int,
 	options string) error {
-	_, err := fmt.Fprintf(a, "DIMENSION '%s' '%s' '%s' '%d' '%d' '%s'\n",
-		ID, name, algorithm, multiplier, divisor, options)
+	_, err := a.Write([]byte("DIMENSION '" +
+		ID + quotes +
+		name + quotes +
+		algorithm + quotes +
+		strconv.Itoa(multiplier) + quotes +
+		strconv.Itoa(divisor) + quotes +
+		options + "'\n"))
 	return err
 }
 
 // CLABEL adds or update a label to the chart.
 func (a *API) CLABEL(key, value string, source int) error {
-	_, err := fmt.Fprintf(a, "CLABEL '%s' '%s' '%d'\n", key, value, source)
+	_, err := a.Write([]byte("CLABEL '" +
+		key + quotes +
+		value + quotes +
+		strconv.Itoa(source) + "'\n"))
 	return err
 }
 
 // CLABELCOMMIT adds labels to the chart. Should be called after one or more CLABEL.
 func (a *API) CLABELCOMMIT() error {
-	_, err := fmt.Fprint(a, "CLABEL_COMMIT\n")
+	_, err := a.Write(clabelCommit)
 	return err
 }
 
 // BEGIN initializes data collection for a chart.
 func (a *API) BEGIN(typeID string, ID string, msSince int) (err error) {
 	if msSince > 0 {
-		_, err = fmt.Fprintf(a, "BEGIN '%s.%s' %d\n", typeID, ID, msSince)
+		_, err = a.Write([]byte("BEGIN " + "'" + typeID + "." + ID + "' " + strconv.Itoa(msSince) + "\n"))
 	} else {
-		_, err = fmt.Fprintf(a, "BEGIN '%s.%s'\n", typeID, ID)
+		_, err = a.Write([]byte("BEGIN " + "'" + typeID + "." + ID + "'\n"))
 	}
 	return err
 }
 
 // SET sets the value of a dimension for the initialized chart.
 func (a *API) SET(ID string, value int64) error {
-	_, err := fmt.Fprintf(a, "SET '%s' = %d\n", ID, value)
+	_, err := a.Write([]byte("SET '" + ID + "' = " + strconv.FormatInt(value, 10) + "\n"))
 	return err
 }
 
 // SETEMPTY sets the empty value of a dimension for the initialized chart.
 func (a *API) SETEMPTY(ID string) error {
-	_, err := fmt.Fprintf(a, "SET '%s' = \n", ID)
+	_, err := a.Write([]byte("SET '" + ID + "' = \n"))
 	return err
 }
 
 // VARIABLE sets the value of a CHART scope variable for the initialized chart.
 func (a *API) VARIABLE(ID string, value int64) error {
-	_, err := fmt.Fprintf(a, "VARIABLE CHART '%s' = %d\n", ID, value)
+	_, err := a.Write([]byte("VARIABLE CHART '" + ID + "' = " + strconv.FormatInt(value, 10) + "\n"))
 	return err
 }
 
 // END completes data collection for the initialized chart.
 func (a *API) END() error {
-	_, err := fmt.Fprintf(a, "END\n\n")
-	return err
-}
-
-// FLUSH ignores the last collected values.
-func (a *API) FLUSH() error {
-	_, err := fmt.Fprintf(a, "FLUSH\n")
+	_, err := a.Write(end)
 	return err
 }
 
 // DISABLE disables this plugin. This will prevent Netdata from restarting the plugin.
 func (a *API) DISABLE() error {
-	_, err := fmt.Fprintf(a, "DISABLE\n")
+	_, err := a.Write([]byte("DISABLE\n"))
 	return err
 }
 
 // EMPTYLINE writes an empty line.
 func (a *API) EMPTYLINE() error {
-	_, err := fmt.Fprintf(a, "\n")
+	_, err := a.Write(newLine)
 	return err
 }
 
@@ -143,12 +165,12 @@ func (a *API) HOSTDEFINEEND() error {
 }
 
 func (a *API) HOST(guid string) error {
-	_, err := fmt.Fprintf(a, "HOST '%s'\n\n", guid)
+	_, err := a.Write([]byte("HOST " + "'" + guid + "'" + "\n\n"))
 	return err
 }
 
 func (a *API) DynCfgEnable(pluginName string) error {
-	_, err := fmt.Fprintf(a, "DYNCFG_ENABLE '%s'\n\n", pluginName)
+	_, err := a.Write([]byte("DYNCFG_ENABLE '" + pluginName + "'\n\n"))
 	return err
 }
 
@@ -168,17 +190,17 @@ func (a *API) DynCfgReportJobStatus(moduleName, jobName, status, reason string) 
 }
 
 func (a *API) FunctionResultSuccess(uid, contentType, payload string) error {
-	return a.functionResult(uid, contentType, payload, 1)
+	return a.functionResult(uid, contentType, payload, "1")
 }
 
 func (a *API) FunctionResultReject(uid, contentType, payload string) error {
-	return a.functionResult(uid, contentType, payload, 0)
+	return a.functionResult(uid, contentType, payload, "0")
 }
 
-func (a *API) functionResult(uid, contentType, payload string, code int) error {
+func (a *API) functionResult(uid, contentType, payload, code string) error {
 	var buf bytes.Buffer
 
-	buf.WriteString(fmt.Sprintf("FUNCTION_RESULT_BEGIN %s %d %s 0\n", uid, code, contentType))
+	buf.WriteString("FUNCTION_RESULT_BEGIN " + uid + " " + code + " " + contentType + " 0\n")
 	if payload != "" {
 		buf.WriteString(payload + "\n")
 	}
--- agent/netdataapi/api_test.go
+++ agent/netdataapi/api_test.go
@@ -10,10 +10,10 @@ import (
 )
 
 func TestAPI_CHART(t *testing.T) {
-	b := &bytes.Buffer{}
-	netdataAPI := API{Writer: b}
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
 
-	_ = netdataAPI.CHART(
+	_ = a.CHART(
 		"",
 		"id",
 		"name",
@@ -25,22 +25,22 @@ func TestAPI_CHART(t *testing.T) {
 		1,
 		1,
 		"",
-		"orchestrator",
+		"plugin",
 		"module",
 	)
 
 	assert.Equal(
 		t,
-		"CHART '.id' 'name' 'title' 'units' 'family' 'context' 'line' '1' '1' '' 'orchestrator' 'module'\n",
-		b.String(),
+		"CHART '.id' 'name' 'title' 'units' 'family' 'context' 'line' '1' '1' '' 'plugin' 'module'\n",
+		buf.String(),
 	)
 }
 
 func TestAPI_DIMENSION(t *testing.T) {
-	b := &bytes.Buffer{}
-	netdataAPI := API{Writer: b}
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
 
-	_ = netdataAPI.DIMENSION(
+	_ = a.DIMENSION(
 		"id",
 		"name",
 		"absolute",
@@ -52,15 +52,15 @@ func TestAPI_DIMENSION(t *testing.T) {
 	assert.Equal(
 		t,
 		"DIMENSION 'id' 'name' 'absolute' '1' '1' ''\n",
-		b.String(),
+		buf.String(),
 	)
 }
 
 func TestAPI_BEGIN(t *testing.T) {
-	b := &bytes.Buffer{}
-	netdataAPI := API{Writer: b}
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
 
-	_ = netdataAPI.BEGIN(
+	_ = a.BEGIN(
 		"typeID",
 		"id",
 		0,
@@ -69,12 +69,12 @@ func TestAPI_BEGIN(t *testing.T) {
 	assert.Equal(
 		t,
 		"BEGIN 'typeID.id'\n",
-		b.String(),
+		buf.String(),
 	)
 
-	b.Reset()
+	buf.Reset()
 
-	_ = netdataAPI.BEGIN(
+	_ = a.BEGIN(
 		"typeID",
 		"id",
 		1,
@@ -83,71 +83,265 @@ func TestAPI_BEGIN(t *testing.T) {
 	assert.Equal(
 		t,
 		"BEGIN 'typeID.id' 1\n",
-		b.String(),
+		buf.String(),
 	)
 }
 
 func TestAPI_SET(t *testing.T) {
-	b := &bytes.Buffer{}
-	netdataAPI := API{Writer: b}
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
 
-	_ = netdataAPI.SET("id", 100)
+	_ = a.SET("id", 100)
 
 	assert.Equal(
 		t,
 		"SET 'id' = 100\n",
-		b.String(),
+		buf.String(),
 	)
 }
 
 func TestAPI_SETEMPTY(t *testing.T) {
-	b := &bytes.Buffer{}
-	netdataAPI := API{Writer: b}
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
 
-	_ = netdataAPI.SETEMPTY("id")
+	_ = a.SETEMPTY("id")
 
 	assert.Equal(
 		t,
 		"SET 'id' = \n",
-		b.String(),
+		buf.String(),
 	)
 }
 
 func TestAPI_VARIABLE(t *testing.T) {
-	b := &bytes.Buffer{}
-	netdataAPI := API{Writer: b}
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
 
-	_ = netdataAPI.VARIABLE("id", 100)
+	_ = a.VARIABLE("id", 100)
 
 	assert.Equal(
 		t,
 		"VARIABLE CHART 'id' = 100\n",
-		b.String(),
+		buf.String(),
 	)
 }
 
 func TestAPI_END(t *testing.T) {
-	b := &bytes.Buffer{}
-	netdataAPI := API{Writer: b}
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
 
-	_ = netdataAPI.END()
+	_ = a.END()
 
 	assert.Equal(
 		t,
 		"END\n\n",
-		b.String(),
+		buf.String(),
 	)
 }
 
-func TestAPI_FLUSH(t *testing.T) {
-	b := &bytes.Buffer{}
-	netdataAPI := API{Writer: b}
+func TestAPI_CLABEL(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
 
-	_ = netdataAPI.FLUSH()
+	_ = a.CLABEL("key", "value", 1)
 
 	assert.Equal(
 		t,
-		"FLUSH\n",
-		b.String(),
+		"CLABEL 'key' 'value' '1'\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_CLABELCOMMIT(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.CLABELCOMMIT()
+
+	assert.Equal(
+		t,
+		"CLABEL_COMMIT\n\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_DISABLE(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.DISABLE()
+
+	assert.Equal(
+		t,
+		"DISABLE\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_EMPTYLINE(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.EMPTYLINE()
+
+	assert.Equal(
+		t,
+		"\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_HOST(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.HOST("guid")
+
+	assert.Equal(
+		t,
+		"HOST 'guid'\n\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_HOSTDEFINE(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.HOSTDEFINE("guid", "hostname")
+
+	assert.Equal(
+		t,
+		"HOST_DEFINE 'guid' 'hostname'\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_HOSTLABEL(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.HOSTLABEL("name", "value")
+
+	assert.Equal(
+		t,
+		"HOST_LABEL 'name' 'value'\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_HOSTDEFINEEND(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.HOSTDEFINEEND()
+
+	assert.Equal(
+		t,
+		"HOST_DEFINE_END\n\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_HOSTINFO(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.HOSTINFO("guid", "hostname", map[string]string{"label1": "value1"})
+
+	assert.Equal(
+		t,
+		`HOST_DEFINE 'guid' 'hostname'
+HOST_LABEL 'label1' 'value1'
+HOST_DEFINE_END
+
+`,
+		buf.String(),
+	)
+}
+
+func TestAPI_DynCfgEnable(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.DynCfgEnable("plugin")
+
+	assert.Equal(
+		t,
+		"DYNCFG_ENABLE 'plugin'\n\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_DyncCfgRegisterModule(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.DyncCfgRegisterModule("module")
+
+	assert.Equal(
+		t,
+		"DYNCFG_REGISTER_MODULE 'module' job_array\n\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_DynCfgRegisterJob(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.DynCfgRegisterJob("module", "job", "type")
+
+	assert.Equal(
+		t,
+		"DYNCFG_REGISTER_JOB 'module' 'job' 'type' 0\n\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_DynCfgReportJobStatus(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.DynCfgReportJobStatus("module", "job", "status", "reason")
+
+	assert.Equal(
+		t,
+		"REPORT_JOB_STATUS 'module' 'job' 'status' 0 'reason'\n\n",
+		buf.String(),
+	)
+}
+
+func TestAPI_FunctionResultSuccess(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.FunctionResultSuccess("uid", "contentType", "payload")
+
+	assert.Equal(
+		t,
+		`FUNCTION_RESULT_BEGIN uid 1 contentType 0
+payload
+FUNCTION_RESULT_END
+
+`,
+		buf.String(),
+	)
+}
+
+func TestAPI_FunctionResultReject(t *testing.T) {
+	buf := &bytes.Buffer{}
+	a := API{Writer: buf}
+
+	_ = a.FunctionResultReject("uid", "contentType", "payload")
+
+	assert.Equal(
+		t,
+		`FUNCTION_RESULT_BEGIN uid 0 contentType 0
+payload
+FUNCTION_RESULT_END
+
+`,
+		buf.String(),
 	)
 }
